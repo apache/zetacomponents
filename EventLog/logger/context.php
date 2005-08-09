a444e81a996e668bf214c1a807364ff32a3a0660
rb:ez.no <dev-null@apache.org>
2005-08-09 09:02:04 +0000

    - Changed package name (again).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@967393 13f79535-47bb-0310-9956-ffa450edef68

---
+++ EventLog/logger/context.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Stores and checks the eventTypeContexts and eventSourceContexts.
+ * 
+ *
+ * @package Logger
+ * @copyright Copyright (C) 2005 eZ systems as. All rights reserved.
+ * @license LGPL {@link http://www.gnu.org/copyleft/lesser.html}
+ * @version //autogentag//
+**/
+class ezcLoggerContext
+{
+
+    private $typeContexts;
+
+    private $sourceContexts;
+
+    /**
+     * Remove all contexts. 
+    **/
+    public function reset()
+    {
+    }
+
+    /**
+     * Set the context for an eventType. These events are hardcoded. 
+     *
+     * @param int $eventTypeMask    
+     *      Bit pattern for the eventType assigned to a context. 
+     * @param array $context
+     *      Array with strings.
+    **/
+    private function setEventTypeContext( $eventTypeMask, $context )
+    {
+    }
+
+    /**
+     * Set the context for an eventType. 
+     *
+     * @param int $eventSource Bit pattern for the eventType assigned to a 
+     *                         context. 
+     * @param array $context   Array with strings.
+    **/
+    public function setSourceContext( $eventSource, $context )
+    {
+    }
+
+    /**
+     * Check the context.
+     * @return bool The value true if the context is correct, otherwise false.
+     */
+    public function getContext( $eventType, $eventSource ) 
+    {
+    }
+
+}
+
+
+?>
