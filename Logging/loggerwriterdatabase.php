bbf3730785c8908fcaf1d6f5c74f1b5e42c2584b
rb:ez.no <dev-null@apache.org>
2005-08-09 08:23:20 +0000

    - Changed package name.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@967389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Logging/loggerwriterdatabase.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Writes the log messages to a file
+ * 
+ *
+ * @package Logger
+ * @copyright Copyright (C) 2005 eZ systems as. All rights reserved.
+ * @license LGPL {@link http://www.gnu.org/copyleft/lesser.html}
+ * @version //autogentag//
+**/
+abstract class LoggerWriterUnix extends LoggerWriterFile
+{
+    /**
+     * Write the logEntries to a file in the unix style.
+     * 
+     * @param string group 
+     *      Group of the log data.
+     * @param array logEntries  
+     *      An array with strings specifying each column to be written.
+     */
+    public function writeLogEntry( $group, $logData )
+    {
+    }
+
+    /**
+     * Connect with the database
+    **/
+    public function connect()
+    {
+    }
+}
+
+
+?>
