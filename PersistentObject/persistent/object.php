9c355aab7138fb2241df76fbf4e5011ae7e3bbd3
fh:ez.no <dev-null@apache.org>
2005-08-10 07:22:04 +0000

    - additional cleanup. Now make the various methods have consistent parameters and split the methods that return rows instead of objects
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@967409 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/persistent/object.php
+++ PersistentObject/persistent/object.php
@@ -28,6 +28,7 @@
  *    }
  * }
  * <code>
+ * @todo Explain exactly what we mean with a row (simply an array with the field values)
  * @todo Map of property names --> Database Names
  * @todo is the definition of set and getters here necessary if you do this in the derived property classes?
  * @todo attribute and setAttribute is no longer necessary because the values can be fetched directly using the
@@ -50,10 +51,8 @@ abstract class eczPersistentObject
     /**
      * Initializes the object with the row \a $row. It will try to set
      * each field taken from the database row. Calls fill to do the job.
-     * If the parameter \a $row is an integer it will try to fetch it
-     * from the database using it as the unique ID.
      */
-    public function __construct( $row )
+    public function __construct( $id = -1 )
     {
     }
 
@@ -70,50 +69,58 @@ abstract class eczPersistentObject
      *
      *
      * The definition array is an associative array consists of these keys:
-     * - fields - an associative array of fields which defines which database
-     *            field (the key) is to fetched and how they map to object
-     *            member variables (the value). In order to support all the
-     *            databases this field must fulfill the following requirements:
      * - keys - an array of fields which is used for uniquely identifying the object in the table.
-     * - function_attributes - an associative array of attributes which maps to member functions, used for fetching data with functions.
-     * - set_functions - an associative array of attributes which maps to member functions, used for setting data with functions.
-     * - increment_key - the field which is incremented on table inserts.
+     * - function_attributes - an associative array of attributes which maps
+     *                         to member functions, used for fetching data with functions.
+     * - set_functions - an associative array of attributes which maps to member
+     *                   functions, used for setting data with functions.
+     * - increment_key - the field which is incremented on table inserts. When
+     *                   you store a new object this field is set automatically.
      * - class_name - the classname which is used for instantiating new objecs when fetching from the
      *                database.
      * - sort - an associative array which defines the default sorting of lists, the key is the table field while the value
      *          is the sorting method which is either \c asc or \c desc.
      * - name - the name of the database table
+     * - fields - an associative array of fields which defines which database
+     *            field (the key) is to fetched and how they map to object
+     *            member variables (the value). In order to support all the
+     *            databases this field must fulfill the following requirements:
+     *            an associative array of fields that define which database
+     *            field (the key) that a value is mapped from. The value
+     *            of this entry is an array explained below.
+     * Each field itself is specified through an associative array.
+     * The following fields are accepted:
+     * - property - the name of the property that stores the value
+     * - phptype - the type of the value in PHP.
+     * - dbtype - the type of the value in the database.
+     * - default - the default value for this field. Note: this is automatically
+     *             set to 'null' for fields marked as increment_key.
+     * - visibility - the visibility of the field. Can be 'public' (default) and 'private'.
+     * - required - sets if the field is required or not. You will not be able to store
+     *              objects where required fields have not been set.
      *
      * Example:
      * <code>
-     * function definition()
-     * {
-     *    return array( "fields" => array( "id" => "ID",
-     *                                     "version" => "Version",
-     *                                     "name" => "Name" ),
-     *                  "keys" => array( "id", "version" ),
-     *                  "function_attributes" => array( "current" => "currentVersion",
-     *                                                  "class_name" => "className" ),
-     *                  "increment_key" => "id",
-     *                  "class_name" => "eZContentClass",
-     *                  "sort" => array( "id" => "asc" ),
-     *                  "name" => "ezcontentclass" );
-     * }
      * </code>
+     *
      * @return array The definition for the object. This function is abstract
      *         and must be implemented in derivatives.
      */
     protected abstract function definition();
 
+    /**
+     * Returns a new persistent object based the $row data.
+     */
+    public static function constructFromRow( $row )
+    {
+    }
 
     /**
-     * Fills this object with the data from the $row.
+     * Returns PHP objects out of the database rows \a $rows.
      *
-     * Matching of the data to the field is done through the object definition.
-     * @param array $row
-     * @return void
+     * @return object
      */
-    public function fillFromRow( $row )
+    public static function constructFromRows( $rows )
     {
     }
 
@@ -133,89 +140,11 @@ abstract class eczPersistentObject
      * @param array $custom_fields
      * @return mixed Returns either an array or an object depending on $asObject
      */
-    public static function &fetchObject( $def, $field_filters, $conds, $asObject = true, $grouping = null, $custom_fields = null )
+    public static function fetchObject( $def, $field_filters, $conds, $asObject = true, $grouping = null, $custom_fields = null )
     {
     }
 
-    /**
-     * Deletes this object from the database
-     *
-     * By default it will use all the fields and their values as the condition
-     * for the delete. This can be overrided through the $conditions variable.
-     * It uses removeObject to do the real job and passes the object defintion,
-     * conditions and extra conditions \a $extraConditions to this function.
-     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
-     * the calls within a db transaction; thus within db->begin and db->commit.
-     *
-     * @param array $conditions An associative array of keys and values that
-     *        will be used as conditions for the delete.
-     * @param array @extraConditions Extra conditions that replace or append
-     *        the existing conditions.
-     * @return void
-     */
-    public function delete( $conditions = null, $extraConditions = null )
-    {
-    }
-
-    /**
-     * Deletes an object from the table defined in \a $def with conditions \a $conditions
-     * and extra conditions \a $extraConditions. The extra conditions will either be
-     * appended to the existing conditions or overwrite existing fields.
-     * Uses conditionText() to create the condition SQL.
-     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
-     * the calls within a db transaction; thus within db->begin and db->commit.
-     *
-     * @param array $def The definition of the object type to delete.
-     * @param array $conditions An associative array of keys and values that
-     *        will be used as conditions for the delete.
-     * @param array @extraConditions Extra conditions that replace or append
-     *        the existing conditions.
-     * @return void
-     */
-    public static function deleteObject( &$def, $conditions = null, $extraConditions = null )
-    {
-    }
-
-    /**
-     * Stores this object to the database if the data is considered dirty.
-     *
-     * This method uses storeObject() to do the actual
-     * job and passes \a $fieldFilters to it.
-     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
-     * the calls within a db transaction; thus within db->begin and db->commit.
-     * @param array fieldFilters If specified only certain fields will be stored.
-     * @return void
-     */
-    public function store( $fieldFilters = null )
-    {
-        eZPersistentObject::storeObject( $this, $fieldFilters );
-    }
-
-    /**
-     * Stores the persistent object $obj to the database.
-     *
-     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
-     * the calls within a db transaction; thus within db->begin and db->commit.
-     *
-     * @param object @obj
-     * @param fieldFilters If specified only certain fields will be stored.
-     * @return void
-     */
-    private function storeObject( $obj, $fieldFilters = null )
-    {
-    }
-
-    /**
-     * Returns an SQL sentence from the conditions \a $conditions and row data \a $row.
-     * If \a $row is empty (null) it uses the condition data instead of row data.
-     * @return string
-     */
-    protected function conditionTextByRowSql( $conditions, $row = null )
-    {
-    }
-
-
-    /**
+        /**
      * Returns a list of objects fetched from the database    Creates an SQL query out of the different parameters and returns an array with the result.
      *     If \a $asObject is true the array contains objects otherwise a db row.
      *
@@ -298,68 +227,69 @@ abstract class eczPersistentObject
     }
 
     /**
-     * Returns PHP objects out of the database rows \a $rows.
-     * Each object is created from class \$ class_name and is passed
-     * as a row array as parameter.
+     * Deletes this object from the database
      *
-     * @param object $asObject If \c true then objects will be created,
-     *                      if not it just returns \a $rows as it is.
-     * @return object
-     */
-    protected static function &objectsFromRows( $rows, $class_name )
-    {
-    }
-
-    /**
-     * Returns an update SQL call to make rows $id1 and $id2 change places.
-     */
-    protected function swapRowsSql( $table, &$keys, &$order_id, &$rows, $id1, $id2 )
-    {
-    }
-
-    /**
-     * Returns an order value which can be used for new items in table, for instance placement.
-     * Uses \a $def, \a $orderField and \a $conditions to figure out the currently maximum order value
-     * and returns one that is larger.
-     * @param array $def
-     * @param string $orderField
-     * @param array $conditions
-     * @return int
-     */
-    protected function nextObjectOrder( &$def, $orderField, $conditions )
-    {
-    }
-
-    /**
-     * Moves a row in a database table. \a $def is the object definition.
-     * Uses \a $orderField to determine the order of objects in a table, usually this
-     * is a placement of some kind. It uses this order field to figure out how move
-     * the row, the row is either swapped with another row which is either above or
-     * below according to whether \a $down is true or false, or it is swapped
-     * with the first item or the last item depending on whether this row is first or last.
-     * Uses \a $conditions to figure out unique rows.
-     * \sa swapRow
+     * By default it will use all the fields and their values as the condition
+     * for the delete. This can be overrided through the $conditions variable.
+     * It uses removeObject to do the real job and passes the object defintion,
+     * conditions and extra conditions \a $extraConditions to this function.
      * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
      * the calls within a db transaction; thus within db->begin and db->commit.
      *
-     * @todo Is this thing really necessary?!?
+     * @param array $conditions An associative array of keys and values that
+     *        will be used as conditions for the delete.
+     * @param array @extraConditions Extra conditions that replace or append
+     *        the existing conditions.
      * @return void
      */
-    protected function reorderObject( &$def,
-                            /*! Associative array with one element, the key is the order id and values is order value. */
-                            $orderField,
-                            $conditions,
-                            $down = true )
+    public function delete( $conditions = null, $extraConditions = null )
     {
     }
 
     /**
-     * Returns the $array of strings properly escaped for the current database
+     * Deletes an object from the table defined in \a $def with conditions \a $conditions
+     * and extra conditions \a $extraConditions. The extra conditions will either be
+     * appended to the existing conditions or overwrite existing fields.
+     * Uses conditionText() to create the condition SQL.
+     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
+     * the calls within a db transaction; thus within db->begin and db->commit.
      *
-     * @param array $array
-     * @return array
+     * @param array $def The definition of the object type to delete.
+     * @param array $conditions An associative array of keys and values that
+     *        will be used as conditions for the delete.
+     * @param array @extraConditions Extra conditions that replace or append
+     *        the existing conditions.
+     * @return void
      */
-    private function &escapeArray( &$array )
+    public static function deleteObject( $def, $conditions = null, $extraConditions = null )
+    {
+    }
+
+    /**
+     * Stores this object to the database if the data is considered dirty.
+     *
+     * This method uses storeObject() to do the actual
+     * job and passes \a $fieldFilters to it.
+     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
+     * the calls within a db transaction; thus within db->begin and db->commit.
+     * @param array fieldFilters If specified only certain fields will be stored.
+     * @return void
+     */
+    public function store( $fieldFilters = null )
+    {
+    }
+
+    /**
+     * Stores the persistent object $obj to the database.
+     *
+     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
+     * the calls within a db transaction; thus within db->begin and db->commit.
+     *
+     * @param object @obj
+     * @param fieldFilters If specified only certain fields will be stored.
+     * @return void
+     */
+    public function storeObject( $obj, $fieldFilters = null )
     {
     }
 
@@ -378,6 +308,54 @@ abstract class eczPersistentObject
      * @return void
      */
     protected static function storeObjectList( $parameters )
+    {
+    }
+
+    /**
+     * Returns an SQL sentence from the conditions \a $conditions and row data \a $row.
+     * If \a $row is empty (null) it uses the condition data instead of row data.
+     * @return string
+     */
+    protected function conditionTextByRowSql( $conditions, $row = null )
+    {
+    }
+
+    /**
+     * Returns an update SQL call to make rows $id1 and $id2 change places.
+     */
+    protected function swapRowsSql( $table, $keys, $order_id, $rows, $id1, $id2 )
+    {
+    }
+
+    /**
+     * Returns an order value which can be used for new items in table, for instance placement.
+     * Uses \a $def, \a $orderField and \a $conditions to figure out the currently maximum order value
+     * and returns one that is larger.
+     * @param array $def
+     * @param string $orderField
+     * @param array $conditions
+     * @return int
+     */
+    protected function nextObjectOrder( $def, $orderField, $conditions )
+    {
+    }
+
+    /**
+     * Moves a row in a database table. \a $def is the object definition.
+     * Uses \a $orderField to determine the order of objects in a table, usually this
+     * is a placement of some kind. It uses this order field to figure out how move
+     * the row, the row is either swapped with another row which is either above or
+     * below according to whether \a $down is true or false, or it is swapped
+     * with the first item or the last item depending on whether this row is first or last.
+     * Uses \a $conditions to figure out unique rows.
+     * \sa swapRow
+     * \note Transaction unsafe. If you call several transaction unsafe methods you must enclose
+     * the calls within a db transaction; thus within db->begin and db->commit.
+     *
+     * @todo Is this thing really necessary?!?
+     * @return void
+     */
+    protected function reorderObject( $def, $orderField, $conditions, $down = true )
     {
     }
 
@@ -417,7 +395,28 @@ abstract class eczPersistentObject
      * @param boolean @hasDirtyData
      * @return void
      */
-    protected function setHasDirtyData( $hasDirtyData )
+    protected function setDirtyData( $dirtyData )
+    {
+    }
+
+    /**
+     * Fills this object with the data from the $row.
+     *
+     * Matching of the data to the field is done through the object definition.
+     * @param array $row
+     * @return void
+     */
+    private function fillFromRow( $row )
+    {
+    }
+
+    /**
+     * Returns the $array of strings properly escaped for the current database
+     *
+     * @param array $array
+     * @return array
+     */
+    private function escapeArray( $array )
     {
     }
 }
