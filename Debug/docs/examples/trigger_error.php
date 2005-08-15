cee26dc3b26820bdb1e2bb4add054fe58d445274
rb:ez.no <dev-null@apache.org>
2005-08-15 12:17:49 +0000

    - trigger_error example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@967460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Debug/docs/examples/trigger_error.php
@@ -0,0 +1,105 @@
+<?php
+
+// This example demonstrates how the Debugger and Log can be used in
+// combination with the trigger_error.
+
+function errorHandler($code, $msg, $file, $line)
+{
+    if ( /*debug = */ true && $code == E_USER_NOTICE )
+    {
+        // ezcDebug is auto_included, therefore when debug is disabled this class is not loaded at all.
+
+        // Func: START_TIMER, STOP_TIMER, or LOG.
+        // Msg: Message without the 'encoded parameters'.
+        // source: Paynet, example, etc.
+        // category: Template, SQL, etc.
+
+        $dm = new ezcDebugMessage($msg);
+        $dm->setDefaultSource("Paynet")
+
+        if( $dm->isLog()) 
+        {
+            ezcDebug::getInstance()->log($dm->message, $dm->verbosity, $dm->source, 
+                                         $dm->category, array("file" => $file, "line" => $line));
+        }
+        elseif( $parsedMsg->isStartTimer())
+        {
+            $ezcDebug->getInstance()->startTimer( $dm->name, $dm->source, $dm->category );
+        }
+        else( $dm->isStopTimer())
+        {
+            $ezcDebug->getInstance()->stopTimer( $dm->name );
+        }
+    }
+    else
+    {
+        // E_USER_WARNING, E_USER_ERROR
+        $m = new ezcMessage($msg);
+        $m->setDefaultSource("Paynet")
+
+        // Severity is translated: E_USER_WARNING => WARNING, E_USER_ERROR => ERROR.
+        ezcLog::getInstance()->log($m->message, $m->severity, $m->source, $m->category, 
+                                    array("file" => $file, "line" => $line));
+    }
+}
+
+// Set the errorHandler.
+set_error_handler( "errorHandler" );
+
+////////////// Possible Debug Log messages: //////////////////////////////////
+
+// [ Log, source, category ] Verbosity: Msg
+trigger_error( "[Log, Paynet, Templates ] 1: Loading header template. ");
+
+// [ source, category ] Verbosity: Msg
+trigger_error( "[Paynet, Templates ] 2: Loading main template. ");
+
+// [ category ] Verbosity: Msg
+// Source is specified in the error_handler
+trigger_error( "[ SQL ] 1: Last query: $query ");
+
+// [ category ] Verbosity: Msg
+// Use the default verbosity
+trigger_error( "[ SQL ] Last query: $query ");
+
+// Default category and verbosity
+trigger_error( "5 + 5 = 10");
+
+// Timers:
+// [Timer_start, Src, Group] name 
+trigger_error("[ TIMER_START, Paynet, template_timers ] Loading header template.");
+
+// [Timer_start, Group/Category] name 
+trigger_error("[ TIMER_START, sql_timers ] Start_transaction");
+
+// [Timer_stop] name 
+trigger_error("[ TIMER_STOP ] Start_transaction");
+
+trigger_error("[ TIMER_STOP ] Loading header template.");
+
+
+
+////////////// Log messages: //////////////////////////////////
+
+// Warnings and Errors are the same as Debug, except for the severity:
+// [ Src, Category ] msg
+trigger_error( "[paynet, template] Couldn't load the template: Header", E_USER_WARNING); 
+
+// Example error:
+trigger_error( "[template] Couldn't produce any output", E_USER_ERROR); 
+
+
+////////////// Audit trails: //////////////////////////////////
+
+Log::getInstance()->log("Added new user: $user", SUCCES_AUDIT, "paynet", "users");
+
+Log::getInstance()->log("Couldn't delete user: $user", FAILED_AUDIT, "paynet", "users");
+
+
+
+////////////// Print debug output: //////////////////////////////////
+
+print (ezcDebug::getInstance()->getOutput());
+
+
+?>
