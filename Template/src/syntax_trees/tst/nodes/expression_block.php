834667eaa09e13cea8a297b1f8b416708289fbc5
amos:ez.no <dev-null@apache.org>
2006-03-16 10:39:21 +0000

    - Removed isOutput() method which are no longer needed.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@969721 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/syntax_trees/tst/nodes/expression_block.php
+++ Template/src/syntax_trees/tst/nodes/expression_block.php
@@ -90,40 +90,5 @@ class ezcTemplateExpressionBlockTstNode extends ezcTemplateBlockTstNode
     {
         return $this->startCursor->column;
     }
-
-    /**
-     * @return true if the block is considered an output block.
-     */
-    public function isOutput()
-    {
-        if ( $this->expressionRoot instanceof ezcTemplateAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateConcatAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateDivisionAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateMinusAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateModuloAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplateMultiplicationAssignmentOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplatePlusAssignmentOperatorTstNode )
-            return false;
-
-        if ( $this->expressionRoot instanceof ezcTemplatePostIncrementOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplatePreIncrementOperatorTstNode )
-            return false;
-
-        if ( $this->expressionRoot instanceof ezcTemplatePostDecrementOperatorTstNode )
-            return false;
-        if ( $this->expressionRoot instanceof ezcTemplatePreDecrementOperatorTstNode )
-            return false;
-
-        return true;
-    }
 }
 ?>
