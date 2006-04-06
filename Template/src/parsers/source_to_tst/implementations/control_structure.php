8dd29b9ea1c8b002026ac026105e655963e989e9
rb:ez.no <dev-null@apache.org>
2006-04-06 11:48:36 +0000

    - Disallowed 'for' and 'do .. while', and added tests for it. - Updated the TODO list.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@969824 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/parsers/source_to_tst/implementations/control_structure.php
+++ Template/src/parsers/source_to_tst/implementations/control_structure.php
@@ -65,7 +65,7 @@ class ezcTemplateControlStructureSourceToTstParser extends ezcTemplateSourceToTs
         // Check if any control structure names are used.
         // Note: The code inside the (?:) brace ensures that the next character
         // is not an alphabetical character ie. a word boundary
-        $matches = $cursor->pregMatchComplete( "#^(foreach|for|while|if|elseif|else|switch|case|include|embed|break|continue|skip|delimiter|once|def|undef|set)(?:[^a-zA-Z])#i" );
+        $matches = $cursor->pregMatchComplete( "#^(foreach|while|if|elseif|else|switch|case|include|embed|break|continue|skip|delimiter|once|def|undef|set)(?:[^a-zA-Z])#i" );
 
         if ( $matches === false )
             return false;
