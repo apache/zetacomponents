f48aff04e77ee22c5aac6359c15b6d32dc8b48fc
dr:ez.no <dev-null@apache.org>
2006-04-20 07:00:57 +0000

    - Fixed grammar.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@969927 13f79535-47bb-0310-9956-ffa450edef68

--- SystemInformation/docs/tutorial_getinfo.php
+++ SystemInformation/docs/tutorial_getinfo.php
@@ -1,9 +1,8 @@
 <?php
-
-include( "tutorial_autoload.php" );
+require_once "tutorial_autoload.php";
 
 $info = ezcSystemInfo::getInstance();
-echo( $info->cpuType . "\n" );
-echo( $info->cpuSpeed . "\n" );
+echo $info->cpuType, "\n";
+echo $info->cpuSpeed, "\n";
 
 ?>
