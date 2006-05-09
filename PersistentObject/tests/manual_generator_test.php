7cd9a2ee9f03f4d69b48cd5a8333bb976bf7a9a4
fh:ez.no <dev-null@apache.org>
2006-05-09 13:57:38 +0000

    - added missing tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970074 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/tests/manual_generator_test.php
+++ PersistentObject/tests/manual_generator_test.php
@@ -1,9 +1,97 @@
 <?php
-class ezcPersistentManualGeneratorTest
+/**
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package PersistentObject
+ * @subpackage Tests
+ */
+
+require_once "data/manual_generator_test.php";
+
+/**
+ * Tests the code manager.
+ *
+ * @package PersistentObject
+ * @subpackage Tests
+ */
+class ezcManualGeneratorTest extends ezcTestCase
 {
-    static public function suite()
+    private $session = null;
+    private $hasTables = false;
+
+    public function setUp()
     {
-        return new ezcTestSuite( 'ezcPersistentManualGeneratorTest' );
+        $db = ezcDbInstance::get();
+        PersistentTestObject::setupTable();
+        PersistentTestObject::insertCleanData();
+//        PersistentTestObject::saveSqlSchemas();
+        $this->session = new ezcPersistentSession( ezcDbInstance::get(),
+                                                   new ezcPersistentCodeManager( dirname( __FILE__ ) . "/data/" ) );
+    }
+
+    public function tearDown()
+    {
+        PersistentTestObject::cleanup();
+    }
+
+    public static function suite()
+    {
+        return new ezcTestSuite( 'ezcManualGeneratorTest' );
+    }
+
+    // test no id error
+    public function testErrorID()
+    {
+        $object = new ManualGeneratorTest();
+        $object->id = null;
+        $object->varchar = 'Finland';
+        $object->integer = 42;
+        $object->decimal = 1.42;
+        $object->text = "Finland has Nokia!";
+        try
+        {
+            $this->session->save( $object );
+        }catch( ezcPersistentIdentifierGenerationException $e ){ return; }
+        $this->fail( "Did not get exception when saving with null id." );
+    }
+
+    // test save single
+    public function testSaveValid()
+    {
+        $object = new ManualGeneratorTest();
+        $object->id = 42;
+        $object->varchar = 'Finland';
+        $object->integer = 42;
+        $object->decimal = 1.42;
+        $object->text = "Finland has Nokia!";
+        $this->session->save( $object );
+
+        $this->assertEquals( 42, $object->id );
+        $object2 = $this->session->loadIfExists( 'PersistentTestObject', 42 );
+        $this->assertEquals( 'Finland', $object2->varchar );
+        $this->assertEquals( 42, (int)$object2->integer );
+        $this->assertEquals( 1.42, (float)$object2->decimal );
+        $this->assertEquals( 'Finland has Nokia!', $object2->text );
+
+    }
+
+    // test save already stored
+    public function testSaveAlreadyPersistent()
+    {
+        $object = new ManualGeneratorTest();
+        $object->id = 42;
+        $object->varchar = 'Finland';
+        $object->integer = 42;
+        $object->decimal = 1.42;
+        $object->text = "Finland has Nokia!";
+        $this->session->save( $object );
+        try{
+            $this->session->save( $object );
+        }catch( Exception $e ){ return; }
+        $this->fail( "Did not get exception when saving object twice.." );
     }
 }
+
 ?>
