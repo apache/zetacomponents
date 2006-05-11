4ea562575216b7293a72dc532a2e19463a186874
ts:ez.no <dev-null@apache.org>
2006-05-11 15:04:06 +0000

    - Fix underscore in function to class mapping.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970108 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/functions/function_to_class.php
+++ Template/src/functions/function_to_class.php
@@ -12,7 +12,7 @@ return array
 ( 
 
 "/^str_.*/" => "ezcTemplateStringFunctions",
-"/^(base64|url).*/" => "ezcTemplateWebFunctions",
+"/^(base64_|url_).*/" => "ezcTemplateWebFunctions",
 "/^array_.*/" => "ezcTemplateArrayFunctions",
 "/^hash_.*/" => "ezcTemplateArrayFunctions",
 "/^preg_.*/" => "ezcTemplateRegExpFunctions",
