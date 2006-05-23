6113b45a15a94ea7a0c887646e4ce68697681d9e
sa:ez.no <dev-null@apache.org>
2006-05-23 12:04:00 +0000

    - Explanation and examples about autoloading of external class repositories   added to tutorial.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970185 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Base/docs/repos/autoloads/my_autoload.php
@@ -0,0 +1,7 @@
+<?php
+    return array (
+        'erMyClass1' => 'Me/myclass1.php',
+        'erMyClass2' => 'Me/myclass2.php',
+    );
+?>
+                  
---
+++ Base/docs/repos/autoloads/your_autoload.php
@@ -0,0 +1,7 @@
+<?php
+      return array (
+           'erYourClass1' => 'You/yourclass1.php',
+           'erYourClass2' => 'You/yourclass2.php',
+       );
+?>
+                     
