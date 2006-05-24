b695da1d6e4a8ff1c666d982b73437e171404e37
rb:ez.no <dev-null@apache.org>
2006-05-24 10:11:13 +0000

    - Added typehints to the string functions. - Documented the str_wrap function. - Added a typehint test case for one string function.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970197 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/functions/string_functions.php
+++ Template/src/functions/string_functions.php
@@ -23,7 +23,7 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
             // str_replace( $sl, $index, $len, $sr )
             // substr( $sl, 0, $index ) . $sr . substr( $sl, $index + $len );
             case "str_replace": 
-                return array( array("%left", "%index", "%length", "%right"), 
+                return array( ezcTemplateAstNode::TYPE_VALUE, array("%left", "%index", "%length", "%right"), 
                    self::concat( 
                        self::functionCall( "substr", array( "%left", self::value( 0 ), "%index" ) ),
                        self::concat( 
@@ -40,7 +40,7 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
             // str_remove( $s, $index, $len ) 
             // substr( $s, 0, $index ) . substr( $s, $index + $len );
              case "str_remove": 
-                 return array( array("%string", "%index", "%length"), 
+                 return array( ezcTemplateAstNode::TYPE_VALUE,  array("%string", "%index", "%length"), 
                    self::concat( 
                        self::functionCall( "substr", array( "%string", self::value( 0 ), "%index" ) ),
                        self::functionCall( "substr", array( "%string", array("ezcTemplateAdditionOperatorAstNode", array( "%index", "%length" ) ) ) 
@@ -51,7 +51,7 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
             // string str_chop( $s, $len ) ( QString::chop ):
             // substr( $s, 0, strlen( $string ) - $len );
              case "str_chop": 
-                return array( array("%string", "%length"), 
+                return array( ezcTemplateAstNode::TYPE_VALUE,  array("%string", "%length"), 
                        self::functionCall( "substr", array( 
                            "%string", 
                            self::value( 0 ), 
@@ -61,37 +61,38 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
                        
             // string str_chop_front( $s, $len )
             // substr( $s, $len );
-             case "str_chop_front": return array( array("%string", "%length"), self::functionCall( "substr", array( "%string", "%length" ) ) );
+             case "str_chop_front": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%length"), self::functionCall( "substr", array( "%string", "%length" ) ) );
 
-             case "str_append": return array( array("%left", "%right"), self::concat( "%left", "%right" ) );
+             case "str_append": return array( ezcTemplateAstNode::TYPE_VALUE, array("%left", "%right"), self::concat( "%left", "%right" ) );
 
-             case "str_prepend": return array( array("%left", "%right"), self::concat( "%right", "%left" ) );
+             case "str_prepend": return array( ezcTemplateAstNode::TYPE_VALUE, array("%left", "%right"), self::concat( "%right", "%left" ) );
 
             // str_compare( $sl, $sr )
             // strcmp( $sl, $sr );
-            case "str_compare": return array( array( "%left", "%right"), self::functionCall( "strcmp", array( "%left", "%right" ) ) );
+            case "str_compare": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%left", "%right"), self::functionCall( "strcmp", array( "%left", "%right" ) ) );
 
             // str_nat_compare( $sl, $sr )
             // strnatcmp( $sl, $sr );
-            case "str_nat_compare": return array( array( "%left", "%right"), self::functionCall( "strnatcmp", array( "%left", "%right" ) ) );
+            case "str_nat_compare": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%left", "%right"), self::functionCall( "strnatcmp", array( "%left", "%right" ) ) );
 
             //str_contains( $sl, $sr ) ( QString::compare )::
             // strpos( $sl, $sr ) !== false 
-            case "str_contains": return array( array( "%left", "%right"), 
+            case "str_contains": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%left", "%right"), 
                 array( "ezcTemplateNotIdenticalOperatorAstNode", 
                 array( self::functionCall( "strpos", array( "%left", "%right" ) ), self::value( false ) ) ) );
 
             // str_len( $s )
             // strlen( $s )
-            case "str_len": return array( array("%string"), self::functionCall( "strlen", array( "%string" ) ) );
+            case "str_len": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), self::functionCall( "strlen", array( "%string" ) ) );
 
             // str_left( $s, $len )
             // substr( $s, 0, $len )
-            case "str_left": return array( array("%string", "%length"), self::functionCall( "substr", array( "%string", self::value(0), "%length" ) ) );
+            case "str_left": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%length"), self::functionCall( "substr", array( "%string", self::value(0), "%length" ) ) );
 
             // str_starts_with( $sl, $sr )
             // strpos( $sl, $sr ) === 0
-            case "str_starts_with": return array( 
+            case "str_starts_with": return array(
+                ezcTemplateAstNode::TYPE_VALUE,  
                 array("%haystack", "%needle"), 
                 array( "ezcTemplateIdenticalOperatorAstNode", array( 
                     self::functionCall( "strpos", array( "%haystack", "%needle" ) ),
@@ -100,12 +101,13 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
 
             // str_right( $s, $len )
             // substr( $s, -$len )
-            case "str_right": return array( array("%string", "%length"), 
+            case "str_right": return array(ezcTemplateAstNode::TYPE_VALUE,  array("%string", "%length"), 
                 self::functionCall( "substr", array( "%string", array( "ezcTemplateArithmeticNegationOperatorAstNode",  array("%length") ) ) ) );
 
             // str_ends_with( $sl, $sr )
             // strrpos( $sl, $sr ) === ( strlen( $sl ) - strlen( $sr) )
-            case "str_ends_with": return array( 
+            case "str_ends_with": return array(
+                ezcTemplateAstNode::TYPE_VALUE,  
                 array("%haystack", "%needle"), 
                 array( "ezcTemplateIdenticalOperatorAstNode", array( 
                     self::functionCall( "strrpos", array( "%haystack", "%needle" ) ),
@@ -116,81 +118,81 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
 
             // str_mid( $s, $index, $len )
             // substr( $s, $index, $len )
-            case "str_mid": return array( array("%string", "%index", "%length"), 
+            case "str_mid": return array(ezcTemplateAstNode::TYPE_VALUE,  array("%string", "%index", "%length"), 
                 self::functionCall( "substr", array( "%string", "%index", "%length") ) );
 
             // str_at( $s, $index )
             // substr( $s, $index, 1 )
-            case "str_at": return array( array("%string", "%index"), 
+            case "str_at": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%index"), 
                 self::functionCall( "substr", array( "%string", "%index", self::value(1) ) ) );
 
             // str_fill( $s, $len )
             // str_repeat( $s, $len )
-            case "str_fill": return array( array("%string", "%length"), 
+            case "str_fill": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%length"), 
                 self::functionCall( "str_repeat", array( "%string", "%length" ) ) );
 
             // str_index_of( $sl, $sr [, $index ] )
             // strpos( $sl, $sr [, $index ] )
-            case "str_index_of": return array( array("%haystack", "%needle", "[%index]"), 
+            case "str_index_of": return array( ezcTemplateAstNode::TYPE_VALUE, array("%haystack", "%needle", "[%index]"), 
                 self::functionCall( "strpos", array( "%haystack", "%needle", "[%index]" ) ) );
             
             // str_last_index( $sl, $sr [, $index] )
             // strrpos( $sl, $sr [, $index ] )
-            case "str_index_of": return array( array("%haystack", "%needle", "[%index]"), 
+            case "str_index_of": return array( ezcTemplateAstNode::TYPE_VALUE, array("%haystack", "%needle", "[%index]"), 
                 self::functionCall( "strpos", array( "%haystack", "%needle", "[%index]" ) ) );
              
             // str_is_empty( $s )
             // strlen( $s ) === 0
-            case "str_is_empty": return array( array("%string"), 
+            case "str_is_empty": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), 
                 array( "ezcTemplateIdenticalOperatorAstNode", array( 
                     self::functionCall( "strlen", array( "%string" ) ),
                     self::value( 0 ) ) ) );
              
             // str_pad_left( $s, $len, $fill )
             // str_pad( $s, $len, $fill, STR_PAD_LEFT )
-            case "str_pad_left": return array( array("%string", "%length", "%fill"), 
+            case "str_pad_left": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%length", "%fill"), 
                     self::functionCall( "str_pad", array( "%string", "%length", "%fill", self::constant( "STR_PAD_LEFT" ) ) ) );
              
             // str_pad_right( $s, $len, $fill ) ( QString::rightJustified() )::
             // str_pad( $s, $len, $fill, STR_PAD_RIGHT )
-            case "str_pad_right": return array( array("%string", "%length", "%fill"), 
+            case "str_pad_right": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%length", "%fill"), 
                     self::functionCall( "str_pad", array( "%string", "%length", "%fill", self::constant( "STR_PAD_RIGHT" ) ) ) );
              
             // str_number( $num, $decimals, $point, $sep )
             // number_format( $num, $decimals, $point, $sep )
-            case "str_number": return array( array("%number", "%decimals", "%point", "%separator"), 
+            case "str_number": return array( ezcTemplateAstNode::TYPE_VALUE, array("%number", "%decimals", "%point", "%separator"), 
                     self::functionCall( "number_format", array( "%number", "%decimals", "%point", "%separator") ) );
              
             // str_trim( $s [, $chars ] )
             // trim( $s [, $chars] )
-            case "str_trim": return array( array("%string", "[%chars]"), 
+            case "str_trim": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "[%chars]"), 
                     self::functionCall( "trim", array( "%string", "[%chars]") ) );
              
             // str_trim_left( $s [, $chars] )
             // ltrim( $s [, $chars] )
-            case "str_trim_left": return array( array("%string", "[%chars]"), 
+            case "str_trim_left": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "[%chars]"), 
                     self::functionCall( "ltrim", array( "%string", "[%chars]") ) );
              
             // str_trim_right( $s [, $chars] )
             // rtrim( $s, [$chars] )
-            case "str_trim_right": return array( array("%string", "[%chars]"), 
+            case "str_trim_right": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "[%chars]"), 
                     self::functionCall( "rtrim", array( "%string", "[%chars]") ) );
              
             // str_simplified( $s )
             // trim( preg_replace( "/(\n|\t|\r\n|\s)+/", " ", $s ) )
-            case "str_simplify": return array( array("%string"), 
+            case "str_simplify": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), 
                     self::functionCall( "trim", array(
                         self::functionCall( "preg_replace", array( self::constant('"/(\n|\t|\r\n|\s)+/"'), self::value(" "), "%string") )
                     ) ) );
              
             // str_split( $s, $sep[, $max] )
             // explode( $s, $sep, $max )
-            case "str_split": return array( array("%string", "%separator", "[%max]"), 
+            case "str_split": return array( ezcTemplateAstNode::TYPE_VALUE | ezcTemplateAstNode::TYPE_ARRAY, array("%string", "%separator", "[%max]"), 
                     self::functionCall( "explode", array( "%separator", "%string", "[%max]") ) );
              
             // str_join( $s_list, $sep )
             // join( $sList, $sep )
-            case "str_join": return array( array("%list", "%separator"), 
+            case "str_join": return array( ezcTemplateAstNode::TYPE_VALUE, array("%list", "%separator"), 
                     self::functionCall( "join", array( "%separator", "%list") ) );
              
             // str_printf( $format [...] )
@@ -203,7 +205,7 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
             
             // str_ord( $c )
             // chr( $c )
-            case "str_ord": return array( array("%char"), 
+            case "str_ord": return array( ezcTemplateAstNode::TYPE_VALUE, array("%char"), 
                     self::functionCall( "chr", array( "%char") ) );
             
             // str_ord_list( $s )::
@@ -212,27 +214,27 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
              
             // str_upper( $s )
             // strtoupper( $s )
-            case "str_upper": return array( array("%string"), 
+            case "str_upper": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), 
                     self::functionCall( "strtoupper", array( "%string") ) );
             
             // str_lower( $s )
             // strtolower( $s )
-            case "str_lower": return array( array("%string"), 
+            case "str_lower": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), 
                     self::functionCall( "strtolower", array( "%string") ) );
              
             // str_capitalize( $s )::
             // ucfirst( $s )
-            case "str_capitalize": return array( array("%string"), 
+            case "str_capitalize": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string"), 
                     self::functionCall( "ucfirst", array( "%string") ) );
              
             // str_find_replace( $s, $find, $replace, $count )::
             // str_replace( $s, $replace, $find, $count )
-            case "str_find_replace": return array( array("%string", "%find", "%replace", "[%count]"), 
+            case "str_find_replace": return array( ezcTemplateAstNode::TYPE_VALUE, array("%string", "%find", "%replace", "[%count]"), 
                     self::functionCall( "str_replace", array( "%find", "%replace", "%string", "[%count]") ) );
              
             // str_reverse( $s )::
             // strrev( $s )
-            case "str_reverse": return array( array( "%string" ), 
+            case "str_reverse": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%string" ), 
                     self::functionCall( "strrev", array( "%string" ) ) );
              
             // str_section( $s, $sep, $start, $end = -1 )
@@ -241,17 +243,17 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
 
              // str_char_count( $s )::
             // strlen( $s )
-            case "str_char_count": return array( array( "%string" ), 
+            case "str_char_count": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%string" ), 
                     self::functionCall( "strlen", array( "%string" ) ) );
              
             // str_word_count( $s [, $wordsep] )
             // str_word_count( $s, 0 [, $wordsep] )
-            case "str_word_count": return array( array( "%string", "[%wordsep]" ), 
+            case "str_word_count": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%string", "[%wordsep]" ), 
                     self::functionCall( "str_word_count", array( "%string", self::value(0), "[%wordsep]" ) ) );
  
             // - *string* str_paragraph_count( $s )::
             // Code.
-            case "str_paragraph_count": return array( array( "%string" ), 
+            case "str_paragraph_count": return array( ezcTemplateAstNode::TYPE_VALUE, array( "%string" ), 
                     self::functionCall( "ezcTemplateString::str_paragraph_count", array( "%string" ) ) );
  
            // 
@@ -285,8 +287,8 @@ class ezcTemplateStringFunctions extends ezcTemplateFunctions
             
             // str_wrap( $s, $width, $break, $cut )
             // wordwrap( $s, $width, $break, $cut )
-            case "str_wrap": return array( array( "%string", "%width", "%break", "%cut" ), 
-                    self::functionCall( "wordwrap", array( "%string", "%width", "%break", "%cut" ) ) );
+            case "str_wrap": return array( array( "%string", "%width", "%break", "[%cut]" ), 
+                    self::functionCall( "wordwrap", array( "%string", "%width", "%break", "[%cut]" ) ) );
 
             // 
             // - *string* str_wrap_indent::
