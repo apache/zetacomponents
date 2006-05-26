d00b31d8ce14c0202d327bb3f99b6c5017e3683f
dr:ez.no <dev-null@apache.org>
2006-05-26 07:13:59 +0000

    - Fixed spelling/grammar.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970217 13f79535-47bb-0310-9956-ffa450edef68

--- Base/docs/tutorial_example_01.php
+++ Base/docs/tutorial_example_01.php
@@ -1,10 +1,9 @@
 <?php
 require_once 'tutorial_autoload.php';
 
-  ezcBase::addClassRepository( './repos', 'autoloads' );
-  $myVar1 = new erMyClass2();
-  $myVar1->toString();
-  $yourVar1 = new erYourClass1();
-  $yourVar1->toString();
-
+ezcBase::addClassRepository( './repos', 'autoloads' );
+$myVar1 = new erMyClass2();
+$myVar1->toString();
+$yourVar1 = new erYourClass1();
+$yourVar1->toString();
 ?>
