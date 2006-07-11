8ad03dd79f249196804c48b000ae524caa64a042
kn:ez.no <dev-null@apache.org>
2006-07-11 09:02:10 +0000

    - Moved image assertions to own test case - Modified parameter order to be the same as in assertEquals - Added assertImageSame() method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970480 13f79535-47bb-0310-9956-ffa450edef68

---
+++ UnitTest/src/test/image_case.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once 'PHPUnit2/Framework/TestCase.php';
+
+require_once 'PHPUnit2/Util/Filter.php';
+PHPUnit2_Util_Filter::addFileToFilter( __FILE__ );
+
+abstract class ezcImageTestCase extends ezcTestCase
+{
+    /**
+     * Asserts that the compared images are same
+     *
+     * Uses the compare binary of the imagemagick package to compare to images
+     * and will fail if the difference between two images is higher then zero.
+     * 
+     * @param string $image New image
+     * @param string $expectedImage Image to compare with
+     * @param string $message Message to append to the fail message
+     * @access public
+     * @return void
+     */
+    public function assertImageSame( $expectedImage, $image, $message = '' )
+    {
+        $constraint = new ezcTestConstraintSimilarImage( $expectedImage );
+
+        if ( ! $constraint->evaluate( $image ) ) {
+            self::failConstraint( $constraint, $image, $message );
+        }
+    }
+
+    /**
+     * Asserts that the compared images are similar
+     *
+     * Uses the compare binary of the imagemagick package to compare to images
+     * and will fail if the difference between two images is higher then the
+     * defined value.
+     *
+     * See http://www.imagemagick.org/script/compare.php for details. The 
+     * difference is logarithmical scaled.
+     * 
+     * @param string $image New image
+     * @param string $expectedImage Image to compare with
+     * @param string $message Message to append to the fail message
+     * @param int $maxDifference Maximum difference between images
+     * @access public
+     * @return void
+     */
+    public function assertImageSimilar( $expectedImage, $image, $message = '', $maxDifference = 0 )
+    {
+        $constraint = new ezcTestConstraintSimilarImage( $expectedImage, $maxDifference );
+
+        if ( ! $constraint->evaluate( $image ) ) {
+            self::failConstraint( $constraint, $image, $message );
+        }
+    }
+}
+?>
--- UnitTest/src/test_autoload.php
+++ UnitTest/src/test_autoload.php
@@ -4,6 +4,7 @@
 return array (
 	"ezcTestRunner" => "UnitTest/test/runner.php",
 	"ezcTestCase" => "UnitTest/test/case.php",
+	"ezcImageTestCase" => "UnitTest/test/image_case.php",
 	"ezcTestSuite" => "UnitTest/test/suite.php",
 	"ezcTestPrinter" => "UnitTest/test/printer.php",
 
