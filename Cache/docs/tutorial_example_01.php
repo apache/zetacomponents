6d78dc7f560b1ecc2dd0b717ff22252e02ccdff7
ts:ez.no <dev-null@apache.org>
2006-07-18 10:51:31 +0000

    - Add a second cache item in the 1 example, to avoid missunderstandings. # Thanks RB!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970494 13f79535-47bb-0310-9956-ffa450edef68

--- Cache/docs/tutorial_example_01.php
+++ Cache/docs/tutorial_example_01.php
@@ -9,15 +9,22 @@ $options = array(
 ezcCacheManager::createCache( 'simple', '/tmp/cache/plain', 'ezcCacheStorageFilePlain', $options );
 
 $myId = 'unique_id_1';
+$mySecondId = 'id_2';
 
 $cache = ezcCacheManager::getCache( 'simple' );
 
-if ( ( $data = $cache->restore( $myId ) ) === false )
+if ( ( $dataOfFirstItem = $cache->restore( $myId ) ) === false )
 {
-    $data = "Plain cache stored on " . date( 'Y-m-d, H:i:s' );
-    $cache->store( $myId, $data );
+    $dataOfFirstItem = "Plain cache stored on " . date( 'Y-m-d, H:i:s' );
+    $cache->store( $myId, $dataOfFirstItem );
 }
 
-var_dump( $data );
+if ( ( $dataOfSecondItem = $cache->restore( $mySecondId ) ) === false )
+{
+    $dataOfSecondItem = "Plain cache 2 stored on " . date( 'Y-m-d, H:i:s' );
+    $cache->store( $mySecondId, $dataOfSecondItem );
+}
+
+var_dump( $dataOfFirstItem, $dataOfSecondItem );
 
 ?>
