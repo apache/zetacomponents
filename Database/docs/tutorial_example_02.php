22fba7f7bfaadb710893b1e1d756b9cd7f66b7df
rb:ez.no <dev-null@apache.org>
2006-08-08 15:00:47 +0000

    - Prevent some confusion with the result of the query.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970560 13f79535-47bb-0310-9956-ffa450edef68

--- Database/docs/tutorial_example_02.php
+++ Database/docs/tutorial_example_02.php
@@ -4,4 +4,9 @@ $db = ezcDbInstance::get();
 
 $rows = $db->query( 'SELECT * FROM quotes' );
 
+// Iterate over the rows and print the information from each result.
+foreach( $rows as $row )
+{
+    print_r( $row );
+}
 ?>
