f67072bcd68b9a996cd9a16d717ddc7eb64addfa
sb:ez.no <dev-null@apache.org>
2006-10-05 11:19:22 +0000

    Leftover.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970963 13f79535-47bb-0310-9956-ffa450edef68

--- UnitTest/src/test_autoload.php
+++ UnitTest/src/test_autoload.php
@@ -3,7 +3,6 @@ return array (
 	"ezcTestRunner" => "UnitTest/test/runner.php",
 	"ezcTestCase" => "UnitTest/test/case.php",
 	"ezcImageTestCase" => "UnitTest/test/image_case.php",
-	"PHPUnit_Framework_TestSuite" => "UnitTest/test/suite.php",
 	"ezcTestPrinter" => "UnitTest/test/printer.php",
 
 	"ezcTestConstraintSimilarImage" => "UnitTest/constraint/image.php",
