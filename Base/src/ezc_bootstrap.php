2dfc94f1eecb22a5deeab7bcfd213eb4e9239b27
dr:ez.no <dev-null@apache.org>
2006-10-10 12:56:43 +0000

    - Implemented FR #8753: Added the 'Base/ezc_bootstrap.php' file which sets up   the autoload environment for you to facilitate an easier way of starting to   use the eZ components.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@970991 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Base/src/ezc_bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+$dir = dirname( __FILE__ );
+$dirParts = split( '/', $dir );
+
+if ( $dirParts[count( $dirParts ) - 1] === 'src' )
+{
+    require 'Base/src/base.php'; // svn, bundle
+}
+else if ( $dirParts[count( $dirParts ) - 2] === 'ezc' )
+{
+    require 'ezc/Base/base.php'; // pear
+}
+else
+{
+    die( "Your environment isn't properly set-up. Please refer to the eZ components documentation at http://components.ez.no/doc ." );
+}
+
+function __autoload( $className )
+{
+	ezcBase::autoload( $className );
+}
+?>
