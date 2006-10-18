09ab82dbab8e3ddf15b268ea056b646e4e3eb132
ts:ez.no <dev-null@apache.org>
2006-10-18 18:03:23 +0000

    - Added some more tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971079 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/tests/persistent_session_instance_test.php
+++ PersistentObject/tests/persistent_session_instance_test.php
@@ -31,6 +31,16 @@ class ezcPersistentSessionInstanceTest extends ezcTestCase
         }
     }
 
+    public function testWithoutIdentifierInvalid()
+    {
+        try
+        {
+            ezcPersistentSessionInstance::get();
+            $this->fail( "Getting a non existent instance did not fail." );
+        }
+        catch ( ezcPersistentSessionNotFoundException $e ) {}
+    }
+
     public function testGetWithIdentifierValid()
     {
         $manager = new ezcPersistentCodeManager( dirname( __FILE__ ) . "/data/" );
@@ -48,20 +58,38 @@ class ezcPersistentSessionInstanceTest extends ezcTestCase
     public function testChooseDefault()
     {
 
-        $manager = new ezcPersistentCodeManager( dirname( __FILE__ ) . "/data/" );
-        $session1 = new ezcPersistentSession( ezcDbInstance::get(), $manager );
-        $manager2 = clone( $manager );
-        $manager2->a = "something";
+        $manager1 = new ezcPersistentCodeManager( dirname( __FILE__ ) . "/data/" );
+        $session1 = new ezcPersistentSession( ezcDbInstance::get(), $manager1 );
+        $manager2 = clone( $manager1 );
         $session2 = new ezcPersistentSession( ezcDbInstance::get(), $manager2 );
 
         ezcPersistentSessionInstance::set( $session1 );
         ezcPersistentSessionInstance::set( $session2, 'secondary' );
         ezcPersistentSessionInstance::chooseDefault( 'secondary' );
-        $this->assertEquals( true, isset( ezcPersistentSessionInstance::get()->definitionManager->a ) );
+        $this->assertSame( $manager2, ezcPersistentSessionInstance::get()->definitionManager );
     }
 
     public function testWithIdentifierInvalid()
     {
+        try
+        {
+            ezcPersistentSessionInstance::get( "NoSuchInstance" );
+            $this->fail( "Getting a non existent instance did not fail." );
+        }
+        catch ( ezcPersistentSessionNotFoundException $e ) {}
+    }
+
+    public function testWith2IdentifiersInvalid()
+    {
+        $manager1 = new ezcPersistentCodeManager( dirname( __FILE__ ) . "/data/" );
+        $session1 = new ezcPersistentSession( ezcDbInstance::get(), $manager1 );
+        
+        $manager2 = clone( $manager1 );
+        $session2 = new ezcPersistentSession( ezcDbInstance::get(), $manager2 );
+        
+        ezcPersistentSessionInstance::set( $session1, 'first' );
+        ezcPersistentSessionInstance::set( $session2, 'secondary' );
+
         try
         {
             ezcPersistentSessionInstance::get( "NoSuchInstance" );
