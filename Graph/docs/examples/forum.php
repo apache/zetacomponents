07723c6319e1c32ac4d245b3c89a3baa83c0f56e
kn:ez.no <dev-null@apache.org>
2006-10-26 14:33:16 +0000

    - Added example for ez newsletter
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971167 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Graph/docs/examples/forum.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'Base/src/base.php';
+function __autoload( $className )
+{
+        ezcBase::autoload( $className );
+}
+
+// Create the graph
+$graph = new ezcGraphPieChart();
+$graph->palette = new ezcGraphPaletteEzBlue();
+$graph->legend = false;
+
+// Use fdb font for ming driver
+$graph->options->font = dirname( __FILE__ ) . '/../../tests/data/fdb_font.fdb';
+
+// Add the data and hilight norwegian data set
+$graph->data['week'] = new ezcGraphArrayDataSet( array(
+    'Claudia Kosny' => 45,
+    'Lukasz Serwatka' => 35,
+    'Kristof Coomans' => 25,
+    'David Jones' => 23,
+    'Xavier Dutoit' => 20,
+    'sangib das' => 14,
+    'Mark Marsiglio' => 10,
+    'mark hayhurst' => 10,
+    'Paul Borgermans' => 10,
+    'Nabil Alimi' => 9,
+) );
+
+// Set graph title
+$graph->title = '10 most active users on forum in last week';
+
+// Use 3d renderer, and beautify it
+$graph->renderer = new ezcGraphRenderer3d();
+$graph->renderer->options->pieChartShadowSize = 12;
+$graph->renderer->options->pieChartGleam = .5;
+$graph->renderer->options->dataBorder = false;
+$graph->renderer->options->pieChartHeight = 16;
+$graph->renderer->options->legendSymbolGleam = .5;
+$graph->renderer->options->pieChartOffset = 100;
+$graph->renderer->options->pieChartSymbolColor = '#88888888';
+
+$graph->driver = new ezcGraphMingDriver();
+
+// Output the graph with std SVG driver
+$graph->render( 500, 200, 'forum.swf' );
+
+?>
