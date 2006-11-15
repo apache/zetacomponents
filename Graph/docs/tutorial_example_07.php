1104d7042ecd23fc22f68c42ba017a563de17741
kn:ez.no <dev-null@apache.org>
2006-11-15 13:01:03 +0000

    - Added element reference documentation to tutorial
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971239 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Graph/docs/tutorial_example_07.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'tutorial_autoload.php';
+
+$graph = new ezcGraphPieChart();
+$graph->palette = new ezcGraphPaletteEz();
+$graph->title = 'Access statistics';
+
+$graph->data['Access statistics'] = new ezcGraphArrayDataSet( array(
+    'Mozilla' => 19113,
+    'Explorer' => 10917,
+    'Opera' => 1464,
+    'Safari' => 652,
+    'Konqueror' => 474,
+) );
+
+$graph->render( 400, 200, 'tutorial_example_07.svg' );
+
+// Get element references from renderer
+$elements = $graph->renderer->getElementReferences();
+
+// Add links to charts
+$dom = new DOMDocument();
+$dom->load( 'tutorial_example_07.svg' );
+$xpath = new DomXPath( $dom );
+
+// Link chart elements
+foreach( $elements['data']['Access statistics'] as $objectName => $ids )
+{
+    foreach( $ids as $id )
+    {
+        echo "Link: $id\n";
+        $element = $xpath->query( '//*[@id = \'' . $id . '\']' )->item( 0 );
+
+        $element->setAttribute( 'style', $element->getAttribute( 'style' ) . ' cursor: pointer;' );
+        $element->setAttribute( 'onclick', 'top.location = \'/detailedData.php?browser=' . $objectName . '\'' );
+    }
+}
+
+// Link legend elements
+foreach( $elements['legend'] as $objectName => $ids )
+{
+    foreach( $ids as $id )
+    {
+        echo "Link: $id\n";
+        $element = $xpath->query( '//*[@id = \'' . $id . '\']' )->item( 0 );
+
+        $element->setAttribute( 'style', $element->getAttribute( 'style' ) . ' cursor: pointer;' );
+        $element->setAttribute( 'onclick', 'top.location = \'/detailedData.php?browser=' . $objectName . '\'' );
+    }
+}
+
+$dom->save( 'tutorial_example_07.svg' );
+
+?>
---
+++ Graph/docs/tutorial_example_08.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'tutorial_autoload.php';
+
+$graph = new ezcGraphPieChart();
+$graph->palette = new ezcGraphPaletteEzGreen();
+$graph->title = 'Access statistics';
+
+$graph->driver = new ezcGraphGdDriver();
+$graph->options->font = 'tutorial_font.ttf';
+
+$graph->data['Access statistics'] = new ezcGraphArrayDataSet( array(
+    'Mozilla' => 19113,
+    'Explorer' => 10917,
+    'Opera' => 1464,
+    'Safari' => 652,
+    'Konqueror' => 474,
+) );
+
+$graph->render( 400, 200, 'tutorial_example_08.png' );
+
+$elements = $graph->renderer->getElementReferences();
+
+?>
+<html>
+    <head><title>Image map example</title></head>
+    <body>
+        <map 
+            name="ezcGraphPieChartMap">
+<?php
+    foreach ( $elements['legend'] as $objectName => $polygones )
+    {
+        foreach ( $polygones as $shape => $polygone )
+        {
+            $coordinateString = '';
+            foreach( $polygone as $coordinate )
+            {
+                $coordinateString .= sprintf( '%d,%d,', $coordinate->x, $coordinate->y );
+            }
+
+            printf( "<area shape=\"poly\" coords=\"%s\" href=\"/detailedData.php?browser=%s\" alt=\"%s: %s\" title=\"%s: %s\" />\n",
+                substr( $coordinateString, 0, -1 ),
+                $objectName,
+                $shape, $objectName,
+                $shape, $objectName
+            );
+        }
+    }
+?>
+        </map>
+        <img
+            src="tutorial_example_08.png"
+            width="400" height="200"
+            usemap="#ezcGraphPieChartMap"
+    </body>
+</html>
