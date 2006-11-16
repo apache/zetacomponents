c5f96123a8beee99fb346aa46ffd101e2499c568
ts:ez.no <dev-null@apache.org>
2006-11-16 10:21:24 +0000

    - Added interface for watermark filter.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971251 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ImageConversion/src/interfaces/watermark.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * File containing the ezcImageWatermarkFilters interface.
+ *
+ * @package ImageConversion
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @filesource
+ */
+
+/**
+ * This interface has to implemented by ezcImageFilters classes to
+ * support watermark filters.
+ *
+ * @see ezcImageHandler
+ * @see ezcImageTransformation
+ * @see ezcImageFiltersInterface
+ *
+ * @package ImageConversion
+ */
+interface ezcImageWatermarkFilters
+{
+    /**
+     * Watermark filter.
+     * Places a watermark on the image. The file to use as the watermark image
+     * is given as $image. The $posX, $posY and $size values are given in
+     * percent, related to the destination image. A $size value of 10 will make
+     * the watermark appear in 10% of the destination image size.
+     * $posX = $posY = 10 will make the watermark appear in the top left corner
+     * of the destination image, 10% of its size away from its borders. If
+     * $size is ommitted, the watermark image will not be resized.
+     *
+     * @param string $image The image file to use as the watermark
+     * @param int $posX     X position in the destination image in percent.
+     * @param int $posY     Y position in the destination image in percent.
+     * @param int $size     Percentage size of the watermark.
+     * @return void
+     *
+     * @throws ezcImageInvalidReferenceException
+     *         If no valid resource for the active reference could be found.
+     * @throws ezcImageFilterFailedException
+     *         If the operation performed by the the filter failed.
+     * @throws ezcBaseValueException
+     *         If a submitted parameter was out of range or type.
+     */
+    function watermarkPercent( $image, $posX, $posY, $size = false );
+
+    /**
+     * Watermark filter.
+     * Places a watermark on the image. The file to use as the watermark image
+     * is given as $image. The $posX, $posY and $size values are given in
+     * pixel. The watermark appear at $posX, $posY in the destination image
+     * with a size of $size pixel. If $size is ommitted, the watermark image
+     * will not be resized.
+     *
+     * @param string $image The image file to use as the watermark
+     * @param int $posX     X position in the destination image in pixel.
+     * @param int $posY     Y position in the destination image in pixel.
+     * @param int $size     Pixel size of the watermark.
+     * @return void
+     *
+     * @throws ezcImageInvalidReferenceException
+     *         If no valid resource for the active reference could be found.
+     * @throws ezcImageFilterFailedException
+     *         If the operation performed by the the filter failed.
+     * @throws ezcBaseValueException
+     *         If a submitted parameter was out of range or type.
+     */
+    function watermarkAbsolute( $image, $posX, $posY, $width = false, $height = false );
+}
+?>
