e15a663fd76590b57dc7ecfe11342f0ac77a76e5
dr:ez.no <dev-null@apache.org>
2006-11-20 10:37:16 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971295 13f79535-47bb-0310-9956-ffa450edef68

---
+++ UserInput/tests/manual-tests/test5.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package UserInput
+ * @subpackage Tests
+ */
+?>
+<html>
+<head><title>Test 3</title></head>
+<body>
+<?php
+ini_set( 'include_path', '/home/httpd/ezcomponents/trunk:.' );
+require_once 'Base/src/base.php';
+
+function __autoload( $className )
+{
+    ezcBase::autoload( $className );
+}
+
+if ( ezcInputForm::hasPostData() )
+{
+?>
+<table border="1" align="center">
+    <tr><th>Expected</th><th>Result</th></tr>
+    <tr>
+        <td>
+            <pre>
+<?php
+    echo "Valid properties:\n";
+    var_dump( array( 'test1', 'test3' ) );
+
+    echo "Invalid properties:\n";
+    var_dump( array( 'test2', 'test4' ) );
+
+    echo "Required properties:\n";
+    var_dump( array( 'test1') );
+
+    echo "Optional properties:\n";
+    var_dump( array( 'test2', 'test3', 'test4' ) );
+
+    echo "Values:\n";
+    var_dump( 3.14 );
+
+    try
+    {
+        throw new ezcInputFormNoValidDataException( "test2" );
+    }
+    catch ( ezcInputFormException $e )
+    {
+        echo $e->getCode(), ': ', $e->getMessage();
+    }
+
+    var_dump( 42.4 );
+?>
+        </td>
+        <td>
+            <pre>
+<?php
+    $def = array(
+        'test1' => new ezcInputFormDefinitionElement( ezcInputFormDefinitionElement::REQUIRED, 'boolean' ),
+        'test2' => new ezcInputFormDefinitionElement( ezcInputFormDefinitionElement::OPTIONAL, 'boolean' ),
+    );
+    $form = new ezcInputForm( INPUT_POST, $def );
+
+    echo "Valid properties:\n";
+    var_dump( $form->getValidProperties() );
+
+    echo "Invalid properties:\n";
+    var_dump( $form->getInvalidProperties() );
+
+    echo "Required properties:\n";
+    var_dump( $form->getRequiredProperties() );
+
+    echo "Optional properties:\n";
+    var_dump( $form->getOptionalProperties() );
+
+    echo "Values:\n";
+    var_dump( $form->test1 );
+
+    try
+    {
+        var_dump( $form->test2 );
+    }
+    catch ( ezcInputFormException $e )
+    {
+        echo $e->getCode(), ': ', $e->getMessage();
+    }
+?>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
+<?php
+}
+else
+{
+?>
+<form method="post" align="center">
+<input type="checkbox" name="test1" checked="yes"/>
+<input type="checkbox" name="test2"/>
+<input type="submit" name="submit" value="Go!"/>
+</form>
+</body>
+</html>
+<?php
+}
+?>
---
+++ UserInput/tests/manual-tests/test5b.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package UserInput
+ * @subpackage Tests
+ */
+?>
+<html>
+<head><title>Test 3</title></head>
+<body>
+<?php
+var_dump(filter_list());
+$cb1 = filter_input( INPUT_GET, 'test1', filter_id( 'boolean' ), FILTER_NULL_ON_FAILURE );
+$cb2 = filter_input( INPUT_GET, 'test2', filter_id( 'boolean' ) );
+$int1 = filter_input( INPUT_GET, 'test3', filter_id( 'int' ), FILTER_NULL_ON_FAILURE );
+$int2 = filter_input( INPUT_GET, 'test4', filter_id( 'int' ), array( 'flags' => FILTER_NULL_ON_FAILURE, 'options' => array ('max_range' => 42 ) ) );
+
+if ($cb1 && $cb2) {
+    echo "both are checked";
+}
+
+var_dump( $cb1, $cb2, $int1, $int2 );
+?>
+<form method="get" align="center">
+<input type="checkbox" name="test1" checked="yes"/>
+<input type="checkbox" name="test2"/>
+<input type="text" name="test3"/>
+<input type="text" name="test4"/>
+<input type="submit" name="submit" value="Go!"/>
+</form>
+</body>
+</html>
+<?php
+?>
