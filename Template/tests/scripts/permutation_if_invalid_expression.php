881af6daf75506199bd40ee3d1619cc91238a0ad
amos:ez.no <dev-null@apache.org>
2006-12-06 09:19:53 +0000

    - New permutation generator for modifying expressions.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971503 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Template/tests/scripts/permutation_if_invalid_expression.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once dirname( __FILE__ ) . "/permutation.php";
+
+// Writes to: regression_tests/if/incorrect/modifying_expression_*.in
+
+$binOperators = alt( "+",
+                     "-",
+                     "*",
+                     "/",
+                     "%",
+
+                     "==",
+                     "===",
+                     "!=",
+                     "!==",
+                     "<",
+                     "<=",
+                     ">",
+                     ">=",
+
+                     "&&",
+                     "||",
+
+                     "." );
+
+
+$modifyingOperators = alt( "=",
+                           "+=",
+                           "-=",
+                           "*=",
+                           "/=", // this is handled in a separate test.
+                           "%=",
+                           ".="
+                           );
+
+$mod = perm( "\$foo ",
+             $modifyingOperators,
+             " 5"
+             );
+
+$binRhs = alt( perm( " ( \$bar ",
+                     $modifyingOperators,
+                     " 5 )" ),
+               perm( " \$bar++" ),
+               perm( " \$bar--" ),
+               perm( " ++\$bar" ),
+               perm( " --\$bar" )
+               );
+
+
+$bin = perm( "\$foo ",
+             "+ ",
+             alt( clone $binRhs,
+                  perm( "( ",
+                        clone $binRhs,
+                        " )" ) )
+             );
+
+$ops = alt( $mod,
+            $bin );
+
+$main = alt( $ops,
+             perm( "( ", $ops, " )" )
+             );
+$list = perm( "%num%:",
+              "{if ",
+              $main,
+              "}",
+              "this should never appear",
+              "{/if}\n"
+              );
+
+$dir = dirname( __FILE__ ) . "/../regression_tests/";
+
+$a = app( "", $argv );
+
+$i = 1;
+$top = "{var \$foo = 42, \$bar = 101}\n";
+
+do
+{
+    $num = sprintf( "%04d", $i );
+    $str = $list->generate();
+    $str = str_replace( "%num%", $num, $str );
+    $fileIn  = $dir . "if/incorrect/modifying_expression_" . $num . ".in";
+    $a->store( "{* file: " . "if/incorrect/modifying_expression_" . $num . ".in" . " *}\n" . $top . $str,
+               $fileIn );
+    ++$i;
+} while( $list->increase() );
+
+?>
