ef4edc5a9248eb42d099148235bdd2f2cd10aa28
kn:ez.no <dev-null@apache.org>
2006-12-07 14:47:30 +0000

    - Added example from PHP Magazine columne
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971521 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Graph/docs/examples/php_magazine.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'Base/src/base.php';
+function __autoload( $className )
+{
+    ezcBase::autoload( $className );
+}
+
+// Create the graph
+$graph = new ezcGraphPieChart();
+
+$graph->palette = new ezcGraphPaletteEzRed();
+
+// Add the data and hilight norwegian data set
+$graph->data['articles'] = new ezcGraphArrayDataSet( array(
+    'English' => 1300000,
+    'Germany' => 452000,
+    'Netherlands' => 217000,
+    'Norway' => 70000,
+) );
+$graph->data['articles']->highlight['Germany'] = true;
+
+// Set graph title
+$graph->title = 'Wikipedia articles by country';
+
+// Modify pie chart label to only show amount and percent
+$graph->options->label = '%2$d (%3$.1f%%)';
+
+// Use 3d renderer, and beautify it
+$graph->renderer = new ezcGraphRenderer3d();
+
+$graph->renderer->options->pieChartShadowSize = 12;
+$graph->renderer->options->pieChartGleam = .5;
+$graph->renderer->options->dataBorder = false;
+
+$graph->renderer->options->pieChartHeight = 8;
+$graph->renderer->options->pieChartRotation = .8;
+$graph->renderer->options->pieChartOffset = 190;
+
+$graph->renderer->options->legendSymbolGleam = .5;
+
+// Output the graph with std SVG driver
+$graph->render( 400, 200, 'wikipedia.svg' );
+
+?>
