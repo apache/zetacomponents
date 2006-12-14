5896381d4762d7f0ab32a97b3c9198777f9613b1
as:ez.no <dev-null@apache.org>
2006-12-14 09:57:43 +0000

    - Added examples to the docblocks of custom block and function definitions.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971582 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/structs/custom_block_definition.php
+++ Template/src/structs/custom_block_definition.php
@@ -11,6 +11,54 @@
 /**
  * Contains the definition of a custom block.
  *
+ * Example of use: create a link custom block.
+ *
+ * 1. Create a class which implements ezcTemplateCustomBlock and which
+ * will be included in your application (with the autoloading mechanism).
+ * <code>
+ * class htmlBlocks implements ezcTemplateCustomBlock
+ * {
+ *     public static function getCustomBlockDefinition( $name )
+ *     {
+ *         switch ( $name )
+ *         {
+ *             case "link":
+ *                 $def = new ezcTemplateCustomBlockDefinition();
+ *                 $def->class = __CLASS__;
+ *                 $def->method = 'link';
+ *                 $def->hasCloseTag = false;
+ *                 $def->startExpressionName = 'from';
+ *                 $def->requiredParameters = array( 'title' );
+ *                 $def->optionalParameters = array( 'from', 'to' );
+ *                 return $def;
+ *         }
+ *         return false;
+ *     }
+ *
+ *     public static function link( $parameters )
+ *     {
+ *         $title = "";
+ *         if( isset( $parameters['title'] ) )
+ *         {
+ *             $title = "title=\"{$parameters['title']}\"";
+ *         }
+ *         return "<" . "a href=\"{$parameters['to']}\" {$title}>{$parameters['from']}</a>";
+ *     }
+ * }
+ * </code>
+ *
+ * 2. Assign the class to the Template configuration in your application.
+ * <code>
+ * $config = ezcTemplateConfiguration::getInstance();
+ * $config->addExtension( "htmlBlocks" );
+ * </code>
+ *
+ * 3. Use the custom block in the template.
+ * <code>
+ * {link "Google" to "http://www.google.com" title "Google search engine"}
+ * </code>
+ * The generated html code for this will be a hyperlink.
+ *
  * @package Template
  * @version //autogen//
  */
--- Template/src/structs/custom_function_definition.php
+++ Template/src/structs/custom_function_definition.php
@@ -11,6 +11,48 @@
 /**
  * Contains the definition of a custom function.
  *
+ * Example of use: create a function to hide a mail address.
+ *
+ * 1. Create a class which implements ezcTemplateCustomFunction and which
+ * will be included in your application (with the autoloading mechanism).
+ * <code>
+ * class htmlFunctions implements ezcTemplateCustomFunction
+ * {
+ *     public static function getCustomFunctionDefinition( $name )
+ *     {
+ *         switch ($name )
+ *         {
+ *             case "hide_mail":
+ *                 $def = new ezcTemplateCustomFunctionDefinition();
+ *                 $def->class = __CLASS__;
+ *                 $def->method = "hide_mail";
+ *                 $def->parameters = array( "mailAddress" );
+ *                 return $def;
+ *         }
+ *         return false;
+ *     }
+ *
+ *     public static function hide_mail( $mailAddress )
+ *     {
+ *         $old = array( '@', '.' );
+ *         $new = array( ' at ', ' dot ' );
+ *         return  str_replace( $old, $new, $mailAddress );
+ *     }
+ * }
+ * </code>
+ *
+ * 2. Assign the class to the Template configuration in your application.
+ * <code>
+ * $config = ezcTemplateConfiguration::getInstance();
+ * $config->addExtension( "htmlFunctions" );
+ * </code>
+ *
+ * 3. Use the custom function in the template.
+ * <code>
+ * {hide_mail( "john.doe@example.com" )}
+ * </code>
+ * The generated html code for this will be: john dot doe at example dot com
+ *
  * @package Template
  * @version //autogen//
  */
