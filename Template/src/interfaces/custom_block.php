8f99c5081d0e25177add2e0b7f755d170d269db9
amos:ez.no <dev-null@apache.org>
2006-12-18 10:40:49 +0000

    - More docblock fixes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971687 13f79535-47bb-0310-9956-ffa450edef68

--- Template/src/interfaces/custom_block.php
+++ Template/src/interfaces/custom_block.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * File containing the ezcTemplateCustomBlock class
+ *
+ * @package Template
+ * @version //autogen//
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+/**
+ * Interface for classes which provides custom blocks to the template engine.
+ * The classes must implement this interface and then return a
+ * ezcTemplateCustomBlockDefinition object from the method
+ * getCustomBlockDefinition().
+ *
+ * @package Template
+ * @version //autogen//
+ */
 interface ezcTemplateCustomBlock
 {
     public static function getCustomBlockDefinition( $name );
