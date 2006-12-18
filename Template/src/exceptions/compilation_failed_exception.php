3659a7f74b0c23413fda1db9d37a2c366abc798c
amos:ez.no <dev-null@apache.org>
2006-12-18 10:06:40 +0000

    - Added missing exception files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971683 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Template/src/exceptions/compilation_failed_exception.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * File containing the ezcTemplateCompilationFailedException class
+ *
+ * @package Template
+ * @version //autogen//
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ *
+ * @package Template
+ * @version //autogen//
+ */
+class ezcTemplateCompilationFailedException extends ezcTemplateException
+{
+    /**
+     * Creates a exception for failed compilations, error message is
+     * specified by caller.
+     */
+    public function __construct( $msg )
+    {
+        parent::__construct( $msg );
+    }
+}
+?>
