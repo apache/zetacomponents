46eabd0f4bc104489d28881a642b0a97d5c922e0
ts:ez.no <dev-null@apache.org>
2006-12-19 16:25:10 +0000

    - Use PATH_SEPARATOR here, to make it Windows compatible.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971719 13f79535-47bb-0310-9956-ffa450edef68

--- UnitTest/src/runtests.php
+++ UnitTest/src/runtests.php
@@ -1,7 +1,9 @@
 <?php
 // All errors must be reported
 error_reporting( E_ALL | E_STRICT );
-ini_set( 'include_path', getcwd(). ':' . dirname( __FILE__ ) . '/../..:' . ini_get( 'include_path' ) );
+
+ini_set( 'include_path', getcwd(). PATH_SEPARATOR . dirname( __FILE__ ) . '/../..' . PATH_SEPARATOR . ini_get( 'include_path' ) );
+
 require_once("Base/src/base.php");
 
 function __autoload( $className )
