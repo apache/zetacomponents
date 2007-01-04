c547c0420dbea5bf979d51cde36dda86e00af4f6
kn:ez.no <dev-null@apache.org>
2007-01-04 11:08:14 +0000

    - Resolved task #9910: Add "renderToOutput" example to Graph tutorial.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971777 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Graph/docs/tutorial_example_30.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'tutorial_autoload.php';
+
+$graph = new ezcGraphPieChart();
+$graph->title = 'Access statistics';
+
+$graph->data['Access statistics'] = new ezcGraphArrayDataSet( array(
+    'Mozilla' => 19113,
+    'Explorer' => 10917,
+    'Opera' => 1464,
+    'Safari' => 652,
+    'Konqueror' => 474,
+) );
+$graph->data['Access statistics']->highlight['Opera'] = true;
+
+$graph->renderToOutput( 400, 150 );
+
+?>
