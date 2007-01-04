b43b5000a2273f2325b31a66c6a5b1802caeea1f
kn:ez.no <dev-null@apache.org>
2007-01-04 11:49:45 +0000

    - Added numeric dataset example to tutorial
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971779 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Graph/docs/tutorial_example_31.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'tutorial_autoload.php';
+
+$graph = new ezcGraphLineChart();
+$graph->title = 'Sinus';
+$graph->legend->position = ezcGraph::BOTTOM;
+
+$graph->xAxis = new ezcGraphChartElementNumericAxis();
+
+$graph->data['sinus'] = new ezcGraphNumericDataSet( 
+    -360, // Start value
+    360,  // End value
+    create_function(
+        '$x',
+        'return sin( deg2rad( $x ) );'
+    )
+);
+
+$graph->data['sinus']->resolution = 120;
+
+$graph->render( 400, 150, 'tutorial_example_31.svg' );
+
+?>
