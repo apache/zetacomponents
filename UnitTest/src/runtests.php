997de2f8cf1ae5fc44892753e740770c62af29d9
dr:ez.no <dev-null@apache.org>
2007-01-15 15:56:05 +0000

    - Warn users about not having their error reporting set, instead of silently   setting it for them.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971821 13f79535-47bb-0310-9956-ffa450edef68

--- UnitTest/src/runtests.php
+++ UnitTest/src/runtests.php
@@ -1,6 +1,11 @@
 <?php
 // All errors must be reported
-error_reporting( E_ALL | E_STRICT );
+$currentErrorLevel = error_reporting();
+if ( $currentErrorLevel != ( E_ALL | E_STRICT ) )
+{
+    echo "Your error reporting setting is not E_ALL | E_STRICT, please change\nthis in your php.ini.\n";
+    die();
+}
 
 ini_set( 'include_path', getcwd(). PATH_SEPARATOR . dirname( __FILE__ ) . '/../..' . PATH_SEPARATOR . ini_get( 'include_path' ) );
 
