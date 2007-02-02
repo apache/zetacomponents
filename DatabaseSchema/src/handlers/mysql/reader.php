85c44b0c1f5cca4b427b6fc6c716f77c801203a6
fh:ez.no <dev-null@apache.org>
2007-02-02 10:49:36 +0000

    - undo unintional commit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@971935 13f79535-47bb-0310-9956-ffa450edef68

--- DatabaseSchema/src/handlers/mysql/reader.php
+++ DatabaseSchema/src/handlers/mysql/reader.php
@@ -122,7 +122,6 @@ class ezcDbSchemaMysqlReader implements ezcDbSchemaDbReader
     {
         $fields = array();
 
-        var_dump( "DESCRIBE $tableName" );
         $resultArray = $this->db->query( "DESCRIBE $tableName" );
         $resultArray->setFetchMode( PDO::FETCH_ASSOC );
 
