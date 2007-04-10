d515ab946a8bd6f8a9584f9eaaafa0a5890de4ef
rb:ez.no <dev-null@apache.org>
2007-04-10 14:09:05 +0000

    - Forgot to add this file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972155 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Template/tests/override.php
@@ -0,0 +1,45 @@
+<?php
+
+ezcTestRunner::addFileToFilter( __FILE__ );
+
+class OverrideCustomFunction implements ezcTemplateCustomFunction
+{
+    public static function getCustomFunctionDefinition( $name )
+    {
+        if ( $name == "override" )
+        {
+            $def = new ezcTemplateCustomFunctionDefinition();
+            $def->class = __CLASS__;
+            $def->method = "override";
+
+            return $def;
+        }
+    }
+
+    public static function override()
+    {
+        return new OverrideLocation( "override_test" ); // Refer to override_test.ezt
+    }
+}
+
+
+class OverrideLocation implements ezcTemplateLocationInterface
+{
+    private $keys = array();
+    private $path;
+
+    
+    public function __construct( $path )
+    {
+        $this->path = $path;
+    }
+
+    public function getPath()
+    {
+        return $this->path . ".ezt";
+    }
+}
+
+
+
+?>
