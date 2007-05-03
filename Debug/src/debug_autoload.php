4b8ee7c9ad91cff9d4176cefe59cba8278611a15
dr:ez.no <dev-null@apache.org>
2007-05-03 12:06:27 +0000

    - Update autoload file for Debug, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972305 13f79535-47bb-0310-9956-ffa450edef68

--- Debug/src/debug_autoload.php
+++ Debug/src/debug_autoload.php
@@ -1,26 +1,23 @@
 <?php
 /**
- * File containing the autoload structure.
+ * Autoloader definition for the Debug component.
  *
- * @package Debug
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Debug
  */
 
 return array(
-    'ezcDebug'             => 'Debug/debug.php',
-    'ezcDebugMemoryWriter' => 'Debug/writers/memory_writer.php',
-    'ezcDebugStructure'    => 'Debug/structs/debug_structure.php',
-    'ezcDebugTimer'        => 'Debug/debug_timer.php',
-
-    'ezcDebugOutputFormatter'     => 'Debug/interfaces/formatter.php',
-    'ezcDebugHtmlFormatter' => 'Debug/formatters/html_formatter.php',
-
-    'ezcDebugTimerStruct'      => 'Debug/structs/timer.php',
-    'ezcDebugSwitchTimerStruct'      => 'Debug/structs/switch_timer.php',
-
-    'ezcDebugMessage'      => 'Debug/debug_message.php'
+    'ezcDebugOutputFormatter'   => 'Debug/interfaces/formatter.php',
+    'ezcDebug'                  => 'Debug/debug.php',
+    'ezcDebugHtmlFormatter'     => 'Debug/formatters/html_formatter.php',
+    'ezcDebugMemoryWriter'      => 'Debug/writers/memory_writer.php',
+    'ezcDebugMessage'           => 'Debug/debug_message.php',
+    'ezcDebugStructure'         => 'Debug/structs/debug_structure.php',
+    'ezcDebugSwitchTimerStruct' => 'Debug/structs/switch_timer.php',
+    'ezcDebugTimer'             => 'Debug/debug_timer.php',
+    'ezcDebugTimerStruct'       => 'Debug/structs/timer.php',
 );
-
 ?>
