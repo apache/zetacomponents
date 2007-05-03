1e6f57b16b1b0553f443d3d6b00c2419c23bd351
dr:ez.no <dev-null@apache.org>
2007-05-03 12:08:21 +0000

    - Update autoload file for Database, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972323 13f79535-47bb-0310-9956-ffa450edef68

--- Database/src/query_autoload.php
+++ Database/src/query_autoload.php
@@ -1,30 +1,30 @@
 <?php
 /**
- * Autoloading helper.
+ * Autoloader definition for the Database component.
  *
- * @package Database
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
- * @ignore
+ * @version //autogentag//
+ * @filesource
+ * @package Database
  */
 
 return array(
-    'ezcQuery' => 'Database/sqlabstraction/query.php',
-    'ezcQueryException' => 'Database/exceptions/query_exception.php',
-    'ezcQueryInvalidException' => 'Database/exceptions/query/invalid.php',
+    'ezcQueryException'              => 'Database/exceptions/query_exception.php',
+    'ezcQueryInvalidException'       => 'Database/exceptions/query/invalid.php',
     'ezcQueryVariableParameterException' => 'Database/exceptions/query/variable_parameter.php',
-    'ezcQuerySelect' => 'Database/sqlabstraction/query_select.php',
-    'ezcQuerySubSelect' => 'Database/sqlabstraction/query_subselect.php',    
-    'ezcQuerySelectOracle' => 'Database/sqlabstraction/implementations/query_select_oracle.php',
-    'ezcQuerySelectSqlite' => 'Database/sqlabstraction/implementations/query_select_sqlite.php',
-    'ezcQueryExpression' => 'Database/sqlabstraction/expression.php',
-    'ezcQueryExpressionOracle' => 'Database/sqlabstraction/implementations/expression_oracle.php',
-    'ezcQueryExpressionPgsql' => 'Database/sqlabstraction/implementations/expression_pgsql.php',
-    'ezcQueryExpressionSqlite' => 'Database/sqlabstraction/implementations/expression_sqlite.php',
-    'ezcQuerySqliteFunctions' => 'Database/sqlabstraction/implementations/query_sqlite_function_implementations.php',
-    'ezcQueryInsert' => 'Database/sqlabstraction/query_insert.php',
-    'ezcQueryUpdate' => 'Database/sqlabstraction/query_update.php',
-    'ezcQueryDelete' => 'Database/sqlabstraction/query_delete.php',
+    'ezcQuery'                       => 'Database/sqlabstraction/query.php',
+    'ezcQueryExpression'             => 'Database/sqlabstraction/expression.php',
+    'ezcQuerySelect'                 => 'Database/sqlabstraction/query_select.php',
+    'ezcQueryDelete'                 => 'Database/sqlabstraction/query_delete.php',
+    'ezcQueryExpressionOracle'       => 'Database/sqlabstraction/implementations/expression_oracle.php',
+    'ezcQueryExpressionPgsql'        => 'Database/sqlabstraction/implementations/expression_pgsql.php',
+    'ezcQueryExpressionSqlite'       => 'Database/sqlabstraction/implementations/expression_sqlite.php',
+    'ezcQueryInsert'                 => 'Database/sqlabstraction/query_insert.php',
+    'ezcQuerySelectOracle'           => 'Database/sqlabstraction/implementations/query_select_oracle.php',
+    'ezcQuerySelectSqlite'           => 'Database/sqlabstraction/implementations/query_select_sqlite.php',
+    'ezcQuerySqliteFunctions'        => 'Database/sqlabstraction/implementations/query_sqlite_function_implementations.php',
+    'ezcQuerySubSelect'              => 'Database/sqlabstraction/query_subselect.php',
+    'ezcQueryUpdate'                 => 'Database/sqlabstraction/query_update.php',
 );
 ?>
