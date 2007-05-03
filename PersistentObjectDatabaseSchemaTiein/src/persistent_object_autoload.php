6f2511dc393c4d1be469c46eb667554547e42ca3
dr:ez.no <dev-null@apache.org>
2007-05-03 12:08:15 +0000

    - Update autoload file for PersistentObjectDatabaseSchemaTiein, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972322 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObjectDatabaseSchemaTiein/src/persistent_object_autoload.php
+++ PersistentObjectDatabaseSchemaTiein/src/persistent_object_autoload.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Autoloader definition for the PersistentObjectDatabaseSchemaTiein component.
+ *
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package PersistentObjectDatabaseSchemaTiein
+ */
 
 return array(
-   'ezcPersistentObjectSchemaGenerator' => 'PersistentObjectDatabaseSchemaTiein/generator.php',
+    'ezcPersistentObjectSchemaGenerator' => 'PersistentObjectDatabaseSchemaTiein/generator.php',
 );
-
 ?>
