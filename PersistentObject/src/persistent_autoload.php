0049f81ed13888d62c4c0383530b842a86a6722a
dr:ez.no <dev-null@apache.org>
2007-05-03 12:06:39 +0000

    - Update autoload file for PersistentObject, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972307 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/src/persistent_autoload.php
+++ PersistentObject/src/persistent_autoload.php
@@ -1,51 +1,49 @@
 <?php
 /**
+ * Autoloader definition for the PersistentObject component.
+ *
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @version //autogentag//
  * @filesource
  * @package PersistentObject
  */
+
 return array(
-    'ezcPersistentSession'                              => 'PersistentObject/persistent_session.php',
-    'ezcPersistentSessionInstance'                      => 'PersistentObject/persistent_session_instance.php',
-    'ezcPersistentSessionNotFoundException'             => 'PersistentObject/exceptions/session_not_found.php',
-    'ezcPersistentFindIterator'                         => 'PersistentObject/find_iterator.php',
-    'ezcPersistentStateTransformer'                     => 'PersistentObject/internal/state_transformer.php',
-    
-    'ezcPersistentObjectException'                      => 'PersistentObject/exceptions/persistent_object_exception.php',
-    'ezcPersistentObjectNotPersistentException'         => 'PersistentObject/exceptions/not_persistent.php',
-    'ezcPersistentObjectAlreadyPersistentException'     => 'PersistentObject/exceptions/already_persistent.php',
-    'ezcPersistentQueryException'                       => 'PersistentObject/exceptions/query_exception.php',
-    
-    'ezcPersistentIdentifierGenerator'                  => 'PersistentObject/interfaces/identifier_generator.php',
-    'ezcPersistentSequenceGenerator'                    => 'PersistentObject/generators/sequence_generator.php',
-    'ezcPersistentManualGenerator'                      => 'PersistentObject/generators/manual_generator.php',
-    'ezcPersistentNativeGenerator'                      => 'PersistentObject/generators/native_generator.php',
-    'ezcPersistentIdentifierGenerationException'        => 'PersistentObject/exceptions/identifier_generation.php',
-
-    'ezcPersistentObjectDefinition'                     => 'PersistentObject/structs/persistent_object_definition.php',
-    'ezcPersistentObjectProperty'                       => 'PersistentObject/structs/persistent_object_property.php',
-    'ezcPersistentObjectIdProperty'                     => 'PersistentObject/structs/persistent_object_id_property.php',
-    'ezcPersistentGeneratorDefinition'                  => 'PersistentObject/structs/generator_definition.php',
-    
-    'ezcPersistentRelation'                             => 'PersistentObject/interfaces/relation.php',
-    'ezcPersistentOneToOneRelation'                     => 'PersistentObject/relations/one_to_one.php',
-    'ezcPersistentOneToManyRelation'                    => 'PersistentObject/relations/one_to_many.php',
-    'ezcPersistentManyToOneRelation'                    => 'PersistentObject/relations/many_to_one.php',
-    'ezcPersistentManyToManyRelation'                   => 'PersistentObject/relations/many_to_many.php',
-    'ezcPersistentSingleTableMap'                       => 'PersistentObject/structs/single_table_map.php',
-    'ezcPersistentDoubleTableMap'                       => 'PersistentObject/structs/double_table_map.php',
-    'ezcPersistentRelationNotFoundException'            => 'PersistentObject/exceptions/relation_not_found.php',
-    'ezcPersistentRelationInvalidException'            => 'PersistentObject/exceptions/relation_invalid.php',
+    'ezcPersistentObjectException'                        => 'PersistentObject/exceptions/persistent_object_exception.php',
+    'ezcPersistentDefinitionMissingIdPropertyException'   => 'PersistentObject/exceptions/definition_missing_id_property.php',
+    'ezcPersistentDefinitionNotFoundException'            => 'PersistentObject/exceptions/definition_not_found.php',
+    'ezcPersistentIdentifierGenerationException'          => 'PersistentObject/exceptions/identifier_generation.php',
+    'ezcPersistentObjectAlreadyPersistentException'       => 'PersistentObject/exceptions/already_persistent.php',
+    'ezcPersistentObjectNotPersistentException'           => 'PersistentObject/exceptions/not_persistent.php',
+    'ezcPersistentQueryException'                         => 'PersistentObject/exceptions/query_exception.php',
+    'ezcPersistentRelatedObjectNotFoundException'         => 'PersistentObject/exceptions/related_object_not_found.php',
+    'ezcPersistentRelationInvalidException'               => 'PersistentObject/exceptions/relation_invalid.php',
+    'ezcPersistentRelationNotFoundException'              => 'PersistentObject/exceptions/relation_not_found.php',
     'ezcPersistentRelationOperationNotSupportedException' => 'PersistentObject/exceptions/relation_operation_not_supported.php',
-    'ezcPersistentRelatedObjectNotFoundException'       => 'PersistentObject/exceptions/related_object_not_found.php',
-
-    'ezcPersistentDefinitionManager'                    => 'PersistentObject/interfaces/definition_manager.php',
-    'ezcPersistentMultiManager'                         => 'PersistentObject/managers/multi_manager.php',
-    'ezcPersistentCodeManager'                          => 'PersistentObject/managers/code_manager.php',
-    'ezcPersistentCacheManager'                         => 'PersistentObject/managers/cache_manager.php',
-    'ezcPersistentDefinitionNotFoundException'          => 'PersistentObject/exceptions/definition_not_found.php',
-    'ezcPersistentDefinitionMissingIdPropertyException' => 'PersistentObject/exceptions/definition_missing_id_property.php',
+    'ezcPersistentSessionNotFoundException'               => 'PersistentObject/exceptions/session_not_found.php',
+    'ezcPersistentDefinitionManager'                      => 'PersistentObject/interfaces/definition_manager.php',
+    'ezcPersistentIdentifierGenerator'                    => 'PersistentObject/interfaces/identifier_generator.php',
+    'ezcPersistentRelation'                               => 'PersistentObject/interfaces/relation.php',
+    'ezcPersistentCacheManager'                           => 'PersistentObject/managers/cache_manager.php',
+    'ezcPersistentCodeManager'                            => 'PersistentObject/managers/code_manager.php',
+    'ezcPersistentDoubleTableMap'                         => 'PersistentObject/structs/double_table_map.php',
+    'ezcPersistentFindIterator'                           => 'PersistentObject/find_iterator.php',
+    'ezcPersistentGeneratorDefinition'                    => 'PersistentObject/structs/generator_definition.php',
+    'ezcPersistentManualGenerator'                        => 'PersistentObject/generators/manual_generator.php',
+    'ezcPersistentManyToManyRelation'                     => 'PersistentObject/relations/many_to_many.php',
+    'ezcPersistentManyToOneRelation'                      => 'PersistentObject/relations/many_to_one.php',
+    'ezcPersistentMultiManager'                           => 'PersistentObject/managers/multi_manager.php',
+    'ezcPersistentNativeGenerator'                        => 'PersistentObject/generators/native_generator.php',
+    'ezcPersistentObjectDefinition'                       => 'PersistentObject/structs/persistent_object_definition.php',
+    'ezcPersistentObjectIdProperty'                       => 'PersistentObject/structs/persistent_object_id_property.php',
+    'ezcPersistentObjectProperty'                         => 'PersistentObject/structs/persistent_object_property.php',
+    'ezcPersistentOneToManyRelation'                      => 'PersistentObject/relations/one_to_many.php',
+    'ezcPersistentOneToOneRelation'                       => 'PersistentObject/relations/one_to_one.php',
+    'ezcPersistentSequenceGenerator'                      => 'PersistentObject/generators/sequence_generator.php',
+    'ezcPersistentSession'                                => 'PersistentObject/persistent_session.php',
+    'ezcPersistentSessionInstance'                        => 'PersistentObject/persistent_session_instance.php',
+    'ezcPersistentSingleTableMap'                         => 'PersistentObject/structs/single_table_map.php',
+    'ezcPersistentStateTransformer'                       => 'PersistentObject/internal/state_transformer.php',
 );
 ?>
