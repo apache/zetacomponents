5fc71f7356b3204444a81054e0a333af745baaa7
dr:ez.no <dev-null@apache.org>
2007-05-03 12:08:09 +0000

    - Update autoload file for PhpGenerator, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972321 13f79535-47bb-0310-9956-ffa450edef68

--- PhpGenerator/src/php_generator_autoload.php
+++ PhpGenerator/src/php_generator_autoload.php
@@ -2,16 +2,18 @@
 /**
  * Autoloader definition for the PhpGenerator component.
  *
- * @package PhpGenerator
- * @version //autogen//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package PhpGenerator
  */
+
 return array(
-    'ezcPhpGeneratorReturnData'    => 'PhpGenerator/structs/php_generator_return_data.php',
-    'ezcPhpGeneratorParameter'     => 'PhpGenerator/structs/php_generator_parameter.php',
-    'ezcPhpGenerator'              => 'PhpGenerator/php_generator.php',
     'ezcPhpGeneratorException'     => 'PhpGenerator/exceptions/php_generator_exception.php',
-    'ezcPhpGeneratorFlowException' => 'PhpGenerator/exceptions/flow_exception.php'
+    'ezcPhpGeneratorFlowException' => 'PhpGenerator/exceptions/flow_exception.php',
+    'ezcPhpGenerator'              => 'PhpGenerator/php_generator.php',
+    'ezcPhpGeneratorParameter'     => 'PhpGenerator/structs/php_generator_parameter.php',
+    'ezcPhpGeneratorReturnData'    => 'PhpGenerator/structs/php_generator_return_data.php',
 );
 ?>
