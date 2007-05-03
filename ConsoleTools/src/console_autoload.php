e5f164bb42a9921bcd03ebdb6ba38ca38a07d6f1
dr:ez.no <dev-null@apache.org>
2007-05-03 12:07:21 +0000

    - Update autoload file for ConsoleTools, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972314 13f79535-47bb-0310-9956-ffa450edef68

--- ConsoleTools/src/console_autoload.php
+++ ConsoleTools/src/console_autoload.php
@@ -1,71 +1,61 @@
 <?php
 /**
- * Autoload map for ConsoleTools package. 
+ * Autoloader definition for the ConsoleTools component.
  *
- * @package ConsoleTools
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package ConsoleTools
  */
 
-return array (
-    'ezcConsoleOutput'                              => 'ConsoleTools/output.php',
-    'ezcConsoleOutputOptions'                       => 'ConsoleTools/options/output.php',
-    'ezcConsoleOutputFormats'                       => 'ConsoleTools/structs/output_formats.php',
-    'ezcConsoleOutputFormat'                        => 'ConsoleTools/structs/output_format.php',
-    'ezcConsoleInput'                               => 'ConsoleTools/input.php',
-    'ezcConsoleOption'                              => 'ConsoleTools/input/option.php',
-    'ezcConsoleOptionRule'                          => 'ConsoleTools/structs/option_rule.php',
-    'ezcConsoleTable'                               => 'ConsoleTools/table.php',
-    'ezcConsoleTableRow'                            => 'ConsoleTools/table/row.php',
-    'ezcConsoleTableCell'                           => 'ConsoleTools/table/cell.php',
-    'ezcConsoleTableOptions'                        => 'ConsoleTools/options/table.php',
-    'ezcConsoleProgressbar'                         => 'ConsoleTools/progressbar.php',
-    'ezcConsoleProgressbarOptions'                  => 'ConsoleTools/options/progressbar.php',
-    'ezcConsoleProgressMonitor'                     => 'ConsoleTools/progressmonitor.php',
-    'ezcConsoleProgressMonitorOptions'              => 'ConsoleTools/options/progressmonitor.php',
-    'ezcConsoleStatusbar'                           => 'ConsoleTools/statusbar.php',
-    'ezcConsoleStatusbarOptions'                    => 'ConsoleTools/options/statusbar.php',
-
-    'ezcConsoleException'                           => 'ConsoleTools/exceptions/exception.php',
-    'ezcConsoleNoPositionStoredException'           => 'ConsoleTools/exceptions/no_position_stored.php',
-    'ezcConsoleInvalidOutputTargetException'        => 'ConsoleTools/exceptions/invalid_output_target.php',
-
-    'ezcConsoleOptionAlreadyRegisteredException'    => 'ConsoleTools/exceptions/option_already_registered.php',
-    'ezcConsoleOptionNoAliasException'              => 'ConsoleTools/exceptions/option_no_alias.php',
-    'ezcConsoleOptionStringNotWellformedException'  => 'ConsoleTools/exceptions/option_string_not_wellformed.php',
-    'ezcConsoleInvalidOptionNameException'          => 'ConsoleTools/exceptions/invalid_option_name.php',
-    
-    'ezcConsoleOptionException'                     => 'ConsoleTools/exceptions/option.php',
-    
-    'ezcConsoleOptionArgumentsViolationException'   => 'ConsoleTools/exceptions/option_arguments_violation.php',
-    'ezcConsoleOptionDependencyViolationException'  => 'ConsoleTools/exceptions/option_dependency_violation.php',
-    'ezcConsoleOptionExclusionViolationException'   => 'ConsoleTools/exceptions/option_exclusion_violation.php',
-    'ezcConsoleOptionMandatoryViolationException'   => 'ConsoleTools/exceptions/option_mandatory_violation.php',
-    'ezcConsoleOptionTypeViolationException'        => 'ConsoleTools/exceptions/option_type_violation.php',
-    'ezcConsoleOptionMissingValueException'         => 'ConsoleTools/exceptions/option_missing_value.php',
-    'ezcConsoleOptionNotExistsException'            => 'ConsoleTools/exceptions/option_not_exists.php',
-    'ezcConsoleOptionTooManyValuesException'        => 'ConsoleTools/exceptions/option_too_many_values.php',
-
-    'ezcConsoleDialog'                              => 'ConsoleTools/interfaces/dialog.php',
-    'ezcConsoleDialogViewer'                        => 'ConsoleTools/dialog_viewer.php',
-    'ezcConsoleQuestionDialog'                      => 'ConsoleTools/dialog/question_dialog.php',
-    'ezcConsoleMenuDialog'                          => 'ConsoleTools/dialog/menu_dialog.php',
-    
-    'ezcConsoleDialogValidator'                     => 'ConsoleTools/interfaces/dialog_validator.php',
-    'ezcConsoleQuestionDialogValidator'             => 'ConsoleTools/interfaces/question_dialog_validator.php',
-    'ezcConsoleMenuDialogValidator'                 => 'ConsoleTools/interfaces/menu_dialog_validator.php',
-    'ezcConsoleQuestionDialogCollectionValidator'   => 'ConsoleTools/dialog/validators/question_dialog_collection.php',
-    'ezcConsoleQuestionDialogTypeValidator'         => 'ConsoleTools/dialog/validators/question_dialog_type.php',
-    'ezcConsoleMenuDialogDefaultValidator'          => 'ConsoleTools/dialog/validators/menu_dialog_default.php',
-
-    
-    'ezcConsoleDialogOptions'                       => 'ConsoleTools/options/dialog.php',
-    'ezcConsoleQuestionDialogOptions'               => 'ConsoleTools/options/question_dialog.php',
-    'ezcConsoleMenuDialogOptions'                   => 'ConsoleTools/options/menu_dialog.php',
-
-    'ezcConsoleNoValidDialogResultException'        => 'ConsoleTools/exceptions/no_valid_dialog_result.php',
+return array(
+    'ezcConsoleException'                          => 'ConsoleTools/exceptions/exception.php',
+    'ezcConsoleOptionException'                    => 'ConsoleTools/exceptions/option.php',
+    'ezcConsoleInvalidOptionNameException'         => 'ConsoleTools/exceptions/invalid_option_name.php',
+    'ezcConsoleInvalidOutputTargetException'       => 'ConsoleTools/exceptions/invalid_output_target.php',
+    'ezcConsoleNoPositionStoredException'          => 'ConsoleTools/exceptions/no_position_stored.php',
+    'ezcConsoleNoValidDialogResultException'       => 'ConsoleTools/exceptions/no_valid_dialog_result.php',
+    'ezcConsoleOptionAlreadyRegisteredException'   => 'ConsoleTools/exceptions/option_already_registered.php',
+    'ezcConsoleOptionArgumentsViolationException'  => 'ConsoleTools/exceptions/option_arguments_violation.php',
+    'ezcConsoleOptionDependencyViolationException' => 'ConsoleTools/exceptions/option_dependency_violation.php',
+    'ezcConsoleOptionExclusionViolationException'  => 'ConsoleTools/exceptions/option_exclusion_violation.php',
+    'ezcConsoleOptionMandatoryViolationException'  => 'ConsoleTools/exceptions/option_mandatory_violation.php',
+    'ezcConsoleOptionMissingValueException'        => 'ConsoleTools/exceptions/option_missing_value.php',
+    'ezcConsoleOptionNoAliasException'             => 'ConsoleTools/exceptions/option_no_alias.php',
+    'ezcConsoleOptionNotExistsException'           => 'ConsoleTools/exceptions/option_not_exists.php',
+    'ezcConsoleOptionStringNotWellformedException' => 'ConsoleTools/exceptions/option_string_not_wellformed.php',
+    'ezcConsoleOptionTooManyValuesException'       => 'ConsoleTools/exceptions/option_too_many_values.php',
+    'ezcConsoleOptionTypeViolationException'       => 'ConsoleTools/exceptions/option_type_violation.php',
+    'ezcConsoleDialogValidator'                    => 'ConsoleTools/interfaces/dialog_validator.php',
+    'ezcConsoleQuestionDialogValidator'            => 'ConsoleTools/interfaces/question_dialog_validator.php',
+    'ezcConsoleDialog'                             => 'ConsoleTools/interfaces/dialog.php',
+    'ezcConsoleDialogOptions'                      => 'ConsoleTools/options/dialog.php',
+    'ezcConsoleMenuDialogValidator'                => 'ConsoleTools/interfaces/menu_dialog_validator.php',
+    'ezcConsoleDialogViewer'                       => 'ConsoleTools/dialog_viewer.php',
+    'ezcConsoleInput'                              => 'ConsoleTools/input.php',
+    'ezcConsoleMenuDialog'                         => 'ConsoleTools/dialog/menu_dialog.php',
+    'ezcConsoleMenuDialogDefaultValidator'         => 'ConsoleTools/dialog/validators/menu_dialog_default.php',
+    'ezcConsoleMenuDialogOptions'                  => 'ConsoleTools/options/menu_dialog.php',
+    'ezcConsoleOption'                             => 'ConsoleTools/input/option.php',
+    'ezcConsoleOptionRule'                         => 'ConsoleTools/structs/option_rule.php',
+    'ezcConsoleOutput'                             => 'ConsoleTools/output.php',
+    'ezcConsoleOutputFormat'                       => 'ConsoleTools/structs/output_format.php',
+    'ezcConsoleOutputFormats'                      => 'ConsoleTools/structs/output_formats.php',
+    'ezcConsoleOutputOptions'                      => 'ConsoleTools/options/output.php',
+    'ezcConsoleProgressMonitor'                    => 'ConsoleTools/progressmonitor.php',
+    'ezcConsoleProgressMonitorOptions'             => 'ConsoleTools/options/progressmonitor.php',
+    'ezcConsoleProgressbar'                        => 'ConsoleTools/progressbar.php',
+    'ezcConsoleProgressbarOptions'                 => 'ConsoleTools/options/progressbar.php',
+    'ezcConsoleQuestionDialog'                     => 'ConsoleTools/dialog/question_dialog.php',
+    'ezcConsoleQuestionDialogCollectionValidator'  => 'ConsoleTools/dialog/validators/question_dialog_collection.php',
+    'ezcConsoleQuestionDialogOptions'              => 'ConsoleTools/options/question_dialog.php',
+    'ezcConsoleQuestionDialogTypeValidator'        => 'ConsoleTools/dialog/validators/question_dialog_type.php',
+    'ezcConsoleStatusbar'                          => 'ConsoleTools/statusbar.php',
+    'ezcConsoleStatusbarOptions'                   => 'ConsoleTools/options/statusbar.php',
+    'ezcConsoleTable'                              => 'ConsoleTools/table.php',
+    'ezcConsoleTableCell'                          => 'ConsoleTools/table/cell.php',
+    'ezcConsoleTableOptions'                       => 'ConsoleTools/options/table.php',
+    'ezcConsoleTableRow'                           => 'ConsoleTools/table/row.php',
 );
-
 ?>
