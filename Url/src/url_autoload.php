73f3a04093c07dfc86a35a927fd34ed11e375ab0
dr:ez.no <dev-null@apache.org>
2007-05-03 12:04:57 +0000

    - Update autoload file for Url
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972299 13f79535-47bb-0310-9956-ffa450edef68

--- Url/src/url_autoload.php
+++ Url/src/url_autoload.php
@@ -2,19 +2,20 @@
 /**
  * Autoloader definition for the Url component.
  *
- * @package Url
- * @version //autogen//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Url
  */
-return array(
-    'ezcUrl'                => 'Url/url.php',
-    'ezcUrlConfiguration'   => 'Url/url_configuration.php',
-    'ezcUrlCreator'         => 'Url/url_creator.php',
 
+return array(
     'ezcUrlException'                 => 'Url/exceptions/url_exception.php',
-    'ezcUrlNotRegisteredException'    => 'Url/exceptions/url_not_registered_exception.php',
+    'ezcUrlInvalidParameterException' => 'Url/exceptions/url_invalid_parameter_exception.php',
     'ezcUrlNoConfigurationException'  => 'Url/exceptions/url_no_configuration_exception.php',
-    'ezcUrlInvalidParameterException' => 'Url/exceptions/url_invalid_parameter_exception.php'
+    'ezcUrlNotRegisteredException'    => 'Url/exceptions/url_not_registered_exception.php',
+    'ezcUrl'                          => 'Url/url.php',
+    'ezcUrlConfiguration'             => 'Url/url_configuration.php',
+    'ezcUrlCreator'                   => 'Url/url_creator.php',
 );
 ?>
