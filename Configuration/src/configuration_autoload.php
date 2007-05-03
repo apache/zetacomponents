a54db4d1472761cbf92e590d6f14f276ca59f9df
dr:ez.no <dev-null@apache.org>
2007-05-03 12:07:57 +0000

    - Update autoload file for Configuration, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972319 13f79535-47bb-0310-9956-ffa450edef68

--- Configuration/src/configuration_autoload.php
+++ Configuration/src/configuration_autoload.php
@@ -1,42 +1,43 @@
 <?php
 /**
- * Autoload definition for classes in Configuration package.
+ * Autoloader definition for the Configuration component.
  *
- * @package Configuration
- * @version //autogen//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Configuration
  */
-return array(
-    'ezcConfiguration'                              => 'Configuration/configuration.php',
-    'ezcConfigurationArrayReader'                   => 'Configuration/array/array_reader.php',
-    'ezcConfigurationArrayWriter'                   => 'Configuration/array/array_writer.php',
-    'ezcConfigurationFileReader'                    => 'Configuration/file_reader.php',
-    'ezcConfigurationFileWriter'                    => 'Configuration/file_writer.php',
-    'ezcConfigurationIniParser'                     => 'Configuration/ini/ini_parser.php',
-    'ezcConfigurationIniReader'                     => 'Configuration/ini/ini_reader.php',
-    'ezcConfigurationIniWriter'                     => 'Configuration/ini/ini_writer.php',
-    'ezcConfigurationIniItem'                       => 'Configuration/structs/ini_item.php',
-    'ezcConfigurationManager'                       => 'Configuration/configuration_manager.php',
-    'ezcConfigurationReader'                        => 'Configuration/interfaces/reader.php',
-    'ezcConfigurationValidationItem'                => 'Configuration/structs/validation_item.php',
-    'ezcConfigurationValidationResult'              => 'Configuration/validation_result.php',
-    'ezcConfigurationWriter'                        => 'Configuration/interfaces/writer.php',
 
-    'ezcConfigurationException'                     => 'Configuration/exceptions/exception.php',
-    'ezcConfigurationGroupExistsAlreadyException'   => 'Configuration/exceptions/group_exists_already.php',
-    'ezcConfigurationInvalidReaderClassException'   => 'Configuration/exceptions/invalid_reader_class.php',
-    'ezcConfigurationInvalidSuffixException'        => 'Configuration/exceptions/invalid_suffix.php',
-    'ezcConfigurationManagerNotInitializedException'=> 'Configuration/exceptions/manager_no_init.php',
-    'ezcConfigurationNoConfigException'             => 'Configuration/exceptions/no_config.php',
-    'ezcConfigurationNoConfigObjectException'       => 'Configuration/exceptions/no_config_object.php',
-    'ezcConfigurationParseErrorException'           => 'Configuration/exceptions/parse_error.php',
-    'ezcConfigurationReadFailedException'           => 'Configuration/exceptions/read_failed.php',
-    'ezcConfigurationSettingWrongTypeException'     => 'Configuration/exceptions/setting_wrong_type.php',
-    'ezcConfigurationSettingnameNotStringException' => 'Configuration/exceptions/settingname_not_string.php',
-    'ezcConfigurationUnknownConfigException'        => 'Configuration/exceptions/unknown_config.php',
-    'ezcConfigurationUnknownGroupException'         => 'Configuration/exceptions/unknown_group.php',
-    'ezcConfigurationUnknownSettingException'       => 'Configuration/exceptions/unknown_setting.php',
-    'ezcConfigurationWriteFailedException'          => 'Configuration/exceptions/write_failed.php',
+return array(
+    'ezcConfigurationException'                      => 'Configuration/exceptions/exception.php',
+    'ezcConfigurationGroupExistsAlreadyException'    => 'Configuration/exceptions/group_exists_already.php',
+    'ezcConfigurationInvalidReaderClassException'    => 'Configuration/exceptions/invalid_reader_class.php',
+    'ezcConfigurationInvalidSuffixException'         => 'Configuration/exceptions/invalid_suffix.php',
+    'ezcConfigurationManagerNotInitializedException' => 'Configuration/exceptions/manager_no_init.php',
+    'ezcConfigurationNoConfigException'              => 'Configuration/exceptions/no_config.php',
+    'ezcConfigurationNoConfigObjectException'        => 'Configuration/exceptions/no_config_object.php',
+    'ezcConfigurationParseErrorException'            => 'Configuration/exceptions/parse_error.php',
+    'ezcConfigurationReadFailedException'            => 'Configuration/exceptions/read_failed.php',
+    'ezcConfigurationSettingWrongTypeException'      => 'Configuration/exceptions/setting_wrong_type.php',
+    'ezcConfigurationSettingnameNotStringException'  => 'Configuration/exceptions/settingname_not_string.php',
+    'ezcConfigurationUnknownConfigException'         => 'Configuration/exceptions/unknown_config.php',
+    'ezcConfigurationUnknownGroupException'          => 'Configuration/exceptions/unknown_group.php',
+    'ezcConfigurationUnknownSettingException'        => 'Configuration/exceptions/unknown_setting.php',
+    'ezcConfigurationWriteFailedException'           => 'Configuration/exceptions/write_failed.php',
+    'ezcConfigurationReader'                         => 'Configuration/interfaces/reader.php',
+    'ezcConfigurationWriter'                         => 'Configuration/interfaces/writer.php',
+    'ezcConfigurationFileReader'                     => 'Configuration/file_reader.php',
+    'ezcConfigurationFileWriter'                     => 'Configuration/file_writer.php',
+    'ezcConfiguration'                               => 'Configuration/configuration.php',
+    'ezcConfigurationArrayReader'                    => 'Configuration/array/array_reader.php',
+    'ezcConfigurationArrayWriter'                    => 'Configuration/array/array_writer.php',
+    'ezcConfigurationIniItem'                        => 'Configuration/structs/ini_item.php',
+    'ezcConfigurationIniParser'                      => 'Configuration/ini/ini_parser.php',
+    'ezcConfigurationIniReader'                      => 'Configuration/ini/ini_reader.php',
+    'ezcConfigurationIniWriter'                      => 'Configuration/ini/ini_writer.php',
+    'ezcConfigurationManager'                        => 'Configuration/configuration_manager.php',
+    'ezcConfigurationValidationItem'                 => 'Configuration/structs/validation_item.php',
+    'ezcConfigurationValidationResult'               => 'Configuration/validation_result.php',
 );
 ?>
