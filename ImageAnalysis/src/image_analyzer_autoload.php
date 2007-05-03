38709b96b9d01186389023ca42660c054668f42b
dr:ez.no <dev-null@apache.org>
2007-05-03 12:07:03 +0000

    - Update autoload file for ImageAnalysis, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972311 13f79535-47bb-0310-9956-ffa450edef68

--- ImageAnalysis/src/image_analyzer_autoload.php
+++ ImageAnalysis/src/image_analyzer_autoload.php
@@ -1,24 +1,22 @@
 <?php
 /**
- * Autoload map for ImageAnalysis package.
+ * Autoloader definition for the ImageAnalysis component.
  *
- * @package ImageAnalysis
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package ImageAnalysis
  */
 
-return array (
-    'ezcImageAnalyzer'                              => 'ImageAnalysis/analyzer.php',
-    'ezcImageAnalyzerData'                          => 'ImageAnalysis/structs/analyzer_data.php',
-    'ezcImageAnalyzerHandler'                       => 'ImageAnalysis/interfaces/handler.php',
-    'ezcImageAnalyzerPhpHandler'                    => 'ImageAnalysis/handlers/php.php',
-    'ezcImageAnalyzerImagemagickHandler'            => 'ImageAnalysis/handlers/imagemagick.php',
-    
-    'ezcImageAnalyzerException'                     => 'ImageAnalysis/exceptions/exception.php',
-    'ezcImageAnalyzerFileNotProcessableException'   => 'ImageAnalysis/exceptions/file_not_processable.php',
-    'ezcImageAnalyzerInvalidHandlerException'       => 'ImageAnalysis/exceptions/invalid_handler.php',
+return array(
+    'ezcImageAnalyzerException'                   => 'ImageAnalysis/exceptions/exception.php',
+    'ezcImageAnalyzerFileNotProcessableException' => 'ImageAnalysis/exceptions/file_not_processable.php',
+    'ezcImageAnalyzerInvalidHandlerException'     => 'ImageAnalysis/exceptions/invalid_handler.php',
+    'ezcImageAnalyzerHandler'                     => 'ImageAnalysis/interfaces/handler.php',
+    'ezcImageAnalyzer'                            => 'ImageAnalysis/analyzer.php',
+    'ezcImageAnalyzerData'                        => 'ImageAnalysis/structs/analyzer_data.php',
+    'ezcImageAnalyzerImagemagickHandler'          => 'ImageAnalysis/handlers/imagemagick.php',
+    'ezcImageAnalyzerPhpHandler'                  => 'ImageAnalysis/handlers/php.php',
 );
-
 ?>
