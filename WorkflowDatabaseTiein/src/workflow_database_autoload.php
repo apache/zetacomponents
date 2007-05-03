0abc0b854f80e4afae302a678ff569c9851f56ad
dr:ez.no <dev-null@apache.org>
2007-05-03 12:06:57 +0000

    - Update autoload file for WorkflowDatabaseTiein, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972310 13f79535-47bb-0310-9956-ffa450edef68

--- WorkflowDatabaseTiein/src/workflow_database_autoload.php
+++ WorkflowDatabaseTiein/src/workflow_database_autoload.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Autoload map for WorkflowDatabaseTiein package. 
+ * Autoloader definition for the WorkflowDatabaseTiein component.
  *
- * @package WorkflowDatabaseTiein
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package WorkflowDatabaseTiein
  */
 
-return array( 'ezcWorkflowDatabaseDefinition' => 'WorkflowDatabaseTiein/definition.php',
-              'ezcWorkflowDatabaseExecution' => 'WorkflowDatabaseTiein/execution.php',
-              'ezcWorkflowDatabaseUtil' => 'WorkflowDatabaseTiein/util.php'
-            );
+return array(
+    'ezcWorkflowDatabaseDefinition' => 'WorkflowDatabaseTiein/definition.php',
+    'ezcWorkflowDatabaseExecution'  => 'WorkflowDatabaseTiein/execution.php',
+    'ezcWorkflowDatabaseUtil'       => 'WorkflowDatabaseTiein/util.php',
+);
 ?>
