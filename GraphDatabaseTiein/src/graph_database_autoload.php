cad4f5207092397e887c0ced64cf793ea9dd62f2
dr:ez.no <dev-null@apache.org>
2007-05-03 12:08:50 +0000

    - Update autoload file for GraphDatabaseTiein, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972327 13f79535-47bb-0310-9956-ffa450edef68

--- GraphDatabaseTiein/src/graph_database_autoload.php
+++ GraphDatabaseTiein/src/graph_database_autoload.php
@@ -1,22 +1,19 @@
 <?php
 /**
- * Autoloader definition for the ezcGraphDatabaseTiein component.
+ * Autoloader definition for the GraphDatabaseTiein component.
  *
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @version //autogentag//
  * @filesource
- * @package Graph
+ * @package GraphDatabaseTiein
  */
 
 return array(
-    'ezcGraphDatabaseDataSet'                       => 'GraphDatabaseTiein/dataset.php',
-
     'ezcGraphDatabaseException'                     => 'GraphDatabaseTiein/exceptions/exception.php',
-    'ezcGraphDatabaseTooManyColumnsException'       => 'GraphDatabaseTiein/exceptions/too_many_columns.php',
-    'ezcGraphDatabaseTooManyColumnsException'       => 'GraphDatabaseTiein/exceptions/too_many_columns.php',
     'ezcGraphDatabaseMissingColumnException'        => 'GraphDatabaseTiein/exceptions/missing_column.php',
     'ezcGraphDatabaseStatementNotExecutedException' => 'GraphDatabaseTiein/exceptions/statement_not_executed.php',
+    'ezcGraphDatabaseTooManyColumnsException'       => 'GraphDatabaseTiein/exceptions/too_many_columns.php',
+    'ezcGraphDatabaseDataSet'                       => 'GraphDatabaseTiein/dataset.php',
 );
-
 ?>
