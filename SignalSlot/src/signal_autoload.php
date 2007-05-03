952d4067dda36c29be626d3f4cdc3d100d80de4d
dr:ez.no <dev-null@apache.org>
2007-05-03 12:08:44 +0000

    - Update autoload file for SignalSlot, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972326 13f79535-47bb-0310-9956-ffa450edef68

--- SignalSlot/src/signal_autoload.php
+++ SignalSlot/src/signal_autoload.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Autoload definition for classes in SignalSlot package.
+ * Autoloader definition for the SignalSlot component.
  *
- * @package SignalSlot
- * @version //autogen//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package SignalSlot
  */
-return array(
-    'ezcSignalCollection' => 'SignalSlot/signal_collection.php',
-    'ezcSignalStaticConnections' => 'SignalSlot/static_connections.php',
-    'ezcSignalCallbackComparer' => 'SignalSlot/internal/callback_comparer.php',
-    'ezcSignalStaticConnectionsBase' => 'SignalSlot/interfaces/static_connections_base.php'
-);
 
+return array(
+    'ezcSignalStaticConnectionsBase' => 'SignalSlot/interfaces/static_connections_base.php',
+    'ezcSignalCallbackComparer'      => 'SignalSlot/internal/callback_comparer.php',
+    'ezcSignalCollection'            => 'SignalSlot/signal_collection.php',
+    'ezcSignalStaticConnections'     => 'SignalSlot/static_connections.php',
+);
 ?>
