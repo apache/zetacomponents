fccaf97e6c325afc40ee749cd88bd47963e97c83
dr:ez.no <dev-null@apache.org>
2007-05-03 12:06:45 +0000

    - Update autoload file for Archive, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972308 13f79535-47bb-0310-9956-ffa450edef68

--- Archive/src/archive_autoload.php
+++ Archive/src/archive_autoload.php
@@ -1,54 +1,45 @@
 <?php
 /**
- * Autoload map for Archive package. 
+ * Autoloader definition for the Archive component.
  *
- * @package Archive
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package Archive
  */
 
-return array( "ezcArchive"               =>  "Archive/archive.php",
-              "ezcArchiveEntry"          =>  "Archive/archive_entry.php",
-
-              "ezcArchiveFile"           =>  "Archive/file/file.php",
-              "ezcArchiveBlockFile"      =>  "Archive/file/block_file.php",
-              "ezcArchiveCharacterFile"  =>  "Archive/file/character_file.php",
- 
-              "ezcArchiveFileStructure"  =>  "Archive/file_structure.php",
- 
-              "ezcArchiveV7Tar"          =>  "Archive/tar/v7_tar.php",
-              "ezcArchiveUstarTar"       =>  "Archive/tar/ustar_tar.php",
-              "ezcArchivePaxTar"         =>  "Archive/tar/pax_tar.php",
-              "ezcArchiveGnuTar"         =>  "Archive/tar/gnu_tar.php",
- 
-              "ezcArchiveZip"            =>  "Archive/zip/zip.php",
- 
-              "ezcArchiveException"         =>  "Archive/exceptions/archive_exception.php",
-              "ezcArchiveFileException"     =>  "Archive/exceptions/file_exception.php",
-              "ezcArchiveValueException"     =>  "Archive/exceptions/archive_value.php",
-              "ezcArchiveEmptyException"     =>  "Archive/exceptions/archive_empty.php",
-              "ezcArchiveUnknownTypeException"     =>  "Archive/exceptions/archive_unknown_type.php",
-              "ezcArchiveEntryPrefixException"     =>  "Archive/exceptions/archive_entry_prefix.php",
-              "ezcArchiveChecksumException"     =>  "Archive/exceptions/archive_checksum.php",
-              "ezcArchiveBlockSizeException"     =>  "Archive/exceptions/archive_block_size.php",
-              "ezcArchiveIoException"     =>  "Archive/exceptions/archive_io.php",
-              "ezcArchiveInternalException"     =>  "Archive/exceptions/archive_internal_exception.php",
- 
-              "ezcArchiveLocalFileHeader"            =>  "Archive/zip/headers/zip_local_file.php",
-              "ezcArchiveCentralDirectoryHeader"     =>  "Archive/zip/headers/zip_central_directory.php",
-              "ezcArchiveCentralDirectoryEndHeader"  =>  "Archive/zip/headers/zip_central_directory_end.php",
- 
-              "ezcArchiveV7Header"          =>  "Archive/tar/headers/tar_v7.php",
-              "ezcArchiveUstarHeader"       =>  "Archive/tar/headers/tar_ustar.php",
-              "ezcArchivePaxHeader"         =>  "Archive/tar/headers/tar_pax.php",
-              "ezcArchiveGnuHeader"         =>  "Archive/tar/headers/tar_gnu.php",
- 
-              "ezcArchiveStatMode"          =>  "Archive/stat_mode.php",
-              "ezcArchiveChecksums"         =>  "Archive/checksums.php",
-              "ezcArchiveMime"              =>  "Archive/archive_mime.php"
-            );
-
-
+return array(
+    'ezcArchiveException'                 => 'Archive/exceptions/archive_exception.php',
+    'ezcArchiveBlockSizeException'        => 'Archive/exceptions/archive_block_size.php',
+    'ezcArchiveChecksumException'         => 'Archive/exceptions/archive_checksum.php',
+    'ezcArchiveEmptyException'            => 'Archive/exceptions/archive_empty.php',
+    'ezcArchiveEntryPrefixException'      => 'Archive/exceptions/archive_entry_prefix.php',
+    'ezcArchiveFileException'             => 'Archive/exceptions/file_exception.php',
+    'ezcArchiveInternalException'         => 'Archive/exceptions/archive_internal_exception.php',
+    'ezcArchiveIoException'               => 'Archive/exceptions/archive_io.php',
+    'ezcArchiveUnknownTypeException'      => 'Archive/exceptions/archive_unknown_type.php',
+    'ezcArchiveValueException'            => 'Archive/exceptions/archive_value.php',
+    'ezcArchive'                          => 'Archive/archive.php',
+    'ezcArchiveV7Header'                  => 'Archive/tar/headers/tar_v7.php',
+    'ezcArchiveV7Tar'                     => 'Archive/tar/v7_tar.php',
+    'ezcArchiveFile'                      => 'Archive/file/file.php',
+    'ezcArchiveLocalFileHeader'           => 'Archive/zip/headers/zip_local_file.php',
+    'ezcArchiveUstarHeader'               => 'Archive/tar/headers/tar_ustar.php',
+    'ezcArchiveUstarTar'                  => 'Archive/tar/ustar_tar.php',
+    'ezcArchiveBlockFile'                 => 'Archive/file/block_file.php',
+    'ezcArchiveCentralDirectoryEndHeader' => 'Archive/zip/headers/zip_central_directory_end.php',
+    'ezcArchiveCentralDirectoryHeader'    => 'Archive/zip/headers/zip_central_directory.php',
+    'ezcArchiveCharacterFile'             => 'Archive/file/character_file.php',
+    'ezcArchiveChecksums'                 => 'Archive/checksums.php',
+    'ezcArchiveEntry'                     => 'Archive/archive_entry.php',
+    'ezcArchiveFileStructure'             => 'Archive/file_structure.php',
+    'ezcArchiveGnuHeader'                 => 'Archive/tar/headers/tar_gnu.php',
+    'ezcArchiveGnuTar'                    => 'Archive/tar/gnu_tar.php',
+    'ezcArchiveMime'                      => 'Archive/archive_mime.php',
+    'ezcArchivePaxHeader'                 => 'Archive/tar/headers/tar_pax.php',
+    'ezcArchivePaxTar'                    => 'Archive/tar/pax_tar.php',
+    'ezcArchiveStatMode'                  => 'Archive/stat_mode.php',
+    'ezcArchiveZip'                       => 'Archive/zip/zip.php',
+);
 ?>
