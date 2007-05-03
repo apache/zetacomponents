208c718523537f2a9fe661fa649efadc4c5a5f4c
dr:ez.no <dev-null@apache.org>
2007-05-03 12:07:51 +0000

    - Update autoload file for WorkflowEventLogTiein, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972318 13f79535-47bb-0310-9956-ffa450edef68

--- WorkflowEventLogTiein/src/workflow_event_autoload.php
+++ WorkflowEventLogTiein/src/workflow_event_autoload.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * Autoload map for WorkflowEventLogTiein package. 
+ * Autoloader definition for the WorkflowEventLogTiein component.
  *
- * @package WorkflowEventLogTiein
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package WorkflowEventLogTiein
  */
 
-return array( 'ezcWorkflowEventLogListener' => 'WorkflowEventLogTiein/listener.php' );
+return array(
+    'ezcWorkflowEventLogListener' => 'WorkflowEventLogTiein/listener.php',
+);
 ?>
