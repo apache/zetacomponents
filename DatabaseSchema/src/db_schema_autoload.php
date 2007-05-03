cd9b2144fc1102c49b198e66b615f6825b264260
dr:ez.no <dev-null@apache.org>
2007-05-03 12:09:08 +0000

    - Update autoload file for DatabaseSchema, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972330 13f79535-47bb-0310-9956-ffa450edef68

--- DatabaseSchema/src/db_schema_autoload.php
+++ DatabaseSchema/src/db_schema_autoload.php
@@ -1,73 +1,64 @@
 <?php
 /**
- * Autoloading helper.
+ * Autoloader definition for the DatabaseSchema component.
  *
- * @package DatabaseSchema
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
- * @ignore
+ * @version //autogentag//
+ * @filesource
+ * @package DatabaseSchema
  */
 
-ezcBase::checkDependency( 'DatabaseSchema', ezcBase::DEP_PHP_EXTENSION, 'simplexml' );
-
 return array(
-    'ezcDbSchema'                       => 'DatabaseSchema/schema.php',
-    'ezcDbSchemaDiff'                   => 'DatabaseSchema/schema_diff.php',
-    'ezcDbSchemaHandlerManager'         => 'DatabaseSchema/handler_manager.php',
-    'ezcDbSchemaOptions'                => 'DatabaseSchema/options/schema.php',
-
-    'ezcDbSchemaValidator'              => 'DatabaseSchema/validator.php',
-    'ezcDbSchemaTypesValidator'         => 'DatabaseSchema/validators/types.php',
-    'ezcDbSchemaIndexFieldsValidator'   => 'DatabaseSchema/validators/index_fields.php',
-    'ezcDbSchemaAutoIncrementIndexValidator' => 'DatabaseSchema/validators/auto_increment_index.php',
-
-    'ezcDbSchemaComparator'             => 'DatabaseSchema/comparator.php',
-    'ezcDbSchemaTableDiff'              => 'DatabaseSchema/structs/table_diff.php',
-
-    'ezcDbSchemaReader'                 => 'DatabaseSchema/interfaces/schema_reader.php',
-    'ezcDbSchemaWriter'                 => 'DatabaseSchema/interfaces/schema_writer.php',
-    'ezcDbSchemaDbReader'               => 'DatabaseSchema/interfaces/db_reader.php',
-    'ezcDbSchemaDbWriter'               => 'DatabaseSchema/interfaces/db_writer.php',
-    'ezcDbSchemaFileReader'             => 'DatabaseSchema/interfaces/file_reader.php',
-    'ezcDbSchemaFileWriter'             => 'DatabaseSchema/interfaces/file_writer.php',
-
-    'ezcDbSchemaDiffReader'             => 'DatabaseSchema/interfaces/schema_diff_reader.php',
-    'ezcDbSchemaDiffWriter'             => 'DatabaseSchema/interfaces/schema_diff_writer.php',
-    'ezcDbSchemaDiffDbWriter'           => 'DatabaseSchema/interfaces/db_diff_writer.php',
-    'ezcDbSchemaDiffFileReader'         => 'DatabaseSchema/interfaces/file_diff_reader.php',
-    'ezcDbSchemaDiffFileWriter'         => 'DatabaseSchema/interfaces/file_diff_writer.php',
-
-    'ezcDbSchemaCommonSqlWriter'        => 'DatabaseSchema/handlers/common_sql_writer.php',
-    'ezcDbSchemaPhpArrayReader'         => 'DatabaseSchema/handlers/php_array/reader.php',
-    'ezcDbSchemaPhpArrayWriter'         => 'DatabaseSchema/handlers/php_array/writer.php',
-    'ezcDbSchemaXmlReader'              => 'DatabaseSchema/handlers/xml/reader.php',
-    'ezcDbSchemaXmlWriter'              => 'DatabaseSchema/handlers/xml/writer.php',
-    'XMLWriter'                         => 'DatabaseSchema/handlers/xml/xmlwritersubstitute.php',
-    'ezcDbSchemaPersistentWriter'       => 'DatabaseSchema/handlers/persistent/writer.php',
-    'ezcDbSchemaMysqlReader'            => 'DatabaseSchema/handlers/mysql/reader.php',
-    'ezcDbSchemaMysqlWriter'            => 'DatabaseSchema/handlers/mysql/writer.php',
-    'ezcDbSchemaOracleReader'            => 'DatabaseSchema/handlers/oracle/reader.php',
-    'ezcDbSchemaOracleWriter'            => 'DatabaseSchema/handlers/oracle/writer.php',
-    'ezcDbSchemaPgsqlReader'            => 'DatabaseSchema/handlers/pgsql/reader.php',
-    'ezcDbSchemaPgsqlWriter'            => 'DatabaseSchema/handlers/pgsql/writer.php',
-    'ezcDbSchemaSqliteReader'            => 'DatabaseSchema/handlers/sqlite/reader.php',
-    'ezcDbSchemaSqliteWriter'            => 'DatabaseSchema/handlers/sqlite/writer.php',
-
-    'ezcDbSchemaTable'                  => 'DatabaseSchema/structs/table.php',
-    'ezcDbSchemaField'                  => 'DatabaseSchema/structs/field.php',
-    'ezcDbSchemaIndex'                  => 'DatabaseSchema/structs/index.php',
-    'ezcDbSchemaIndexField'             => 'DatabaseSchema/structs/index_field.php',
-
-    'ezcDbSchemaException'              => 'DatabaseSchema/exceptions/exception.php',
-    'ezcDbSchemaInvalidSchemaException' => 'DatabaseSchema/exceptions/invalid_schema.php',
-    'ezcDbSchemaUnknownFormatException' => 'DatabaseSchema/exceptions/unknown_format.php',
-    'ezcDbSchemaInvalidParentClassException' => 'DatabaseSchema/exceptions/invalid_parent_class.php',
-    'ezcDbSchemaInvalidReaderClassException' => 'DatabaseSchema/exceptions/invalid_reader_class.php',
-    'ezcDbSchemaInvalidWriterClassException' => 'DatabaseSchema/exceptions/invalid_writer_class.php',
+    'ezcDbSchemaException'                       => 'DatabaseSchema/exceptions/exception.php',
+    'ezcDbSchemaDropAllColumnsException'         => 'DatabaseSchema/exceptions/drop_all_columns_exception.php',
     'ezcDbSchemaInvalidDiffReaderClassException' => 'DatabaseSchema/exceptions/invalid_diff_reader_class.php',
     'ezcDbSchemaInvalidDiffWriterClassException' => 'DatabaseSchema/exceptions/invalid_diff_writer_class.php',
-    'ezcDbSchemaSqliteDropFieldException' => 'DatabaseSchema/exceptions/sqlite_drop_field_exception.php',
-    'ezcDbSchemaDropAllColumnsException'  => 'DatabaseSchema/exceptions/drop_all_columns_exception.php',
+    'ezcDbSchemaInvalidReaderClassException'     => 'DatabaseSchema/exceptions/invalid_reader_class.php',
+    'ezcDbSchemaInvalidSchemaException'          => 'DatabaseSchema/exceptions/invalid_schema.php',
+    'ezcDbSchemaInvalidWriterClassException'     => 'DatabaseSchema/exceptions/invalid_writer_class.php',
+    'ezcDbSchemaSqliteDropFieldException'        => 'DatabaseSchema/exceptions/sqlite_drop_field_exception.php',
+    'ezcDbSchemaUnknownFormatException'          => 'DatabaseSchema/exceptions/unknown_format.php',
+    'ezcDbSchemaDiffReader'                      => 'DatabaseSchema/interfaces/schema_diff_reader.php',
+    'ezcDbSchemaDiffWriter'                      => 'DatabaseSchema/interfaces/schema_diff_writer.php',
+    'ezcDbSchemaReader'                          => 'DatabaseSchema/interfaces/schema_reader.php',
+    'ezcDbSchemaWriter'                          => 'DatabaseSchema/interfaces/schema_writer.php',
+    'ezcDbSchemaCommonSqlWriter'                 => 'DatabaseSchema/handlers/common_sql_writer.php',
+    'ezcDbSchemaDbReader'                        => 'DatabaseSchema/interfaces/db_reader.php',
+    'ezcDbSchemaDbWriter'                        => 'DatabaseSchema/interfaces/db_writer.php',
+    'ezcDbSchemaDiffDbWriter'                    => 'DatabaseSchema/interfaces/db_diff_writer.php',
+    'ezcDbSchemaDiffFileReader'                  => 'DatabaseSchema/interfaces/file_diff_reader.php',
+    'ezcDbSchemaDiffFileWriter'                  => 'DatabaseSchema/interfaces/file_diff_writer.php',
+    'ezcDbSchemaFileReader'                      => 'DatabaseSchema/interfaces/file_reader.php',
+    'ezcDbSchemaFileWriter'                      => 'DatabaseSchema/interfaces/file_writer.php',
+    'XMLWriter'                                  => 'DatabaseSchema/handlers/xml/xmlwritersubstitute.php',
+    'ezcDbSchema'                                => 'DatabaseSchema/schema.php',
+    'ezcDbSchemaAutoIncrementIndexValidator'     => 'DatabaseSchema/validators/auto_increment_index.php',
+    'ezcDbSchemaComparator'                      => 'DatabaseSchema/comparator.php',
+    'ezcDbSchemaDiff'                            => 'DatabaseSchema/schema_diff.php',
+    'ezcDbSchemaField'                           => 'DatabaseSchema/structs/field.php',
+    'ezcDbSchemaHandlerDataTransfer'             => 'DatabaseSchema/handlers/data_transfer.php',
+    'ezcDbSchemaHandlerManager'                  => 'DatabaseSchema/handler_manager.php',
+    'ezcDbSchemaIndex'                           => 'DatabaseSchema/structs/index.php',
+    'ezcDbSchemaIndexField'                      => 'DatabaseSchema/structs/index_field.php',
+    'ezcDbSchemaIndexFieldsValidator'            => 'DatabaseSchema/validators/index_fields.php',
+    'ezcDbSchemaMysqlReader'                     => 'DatabaseSchema/handlers/mysql/reader.php',
+    'ezcDbSchemaMysqlWriter'                     => 'DatabaseSchema/handlers/mysql/writer.php',
+    'ezcDbSchemaOptions'                         => 'DatabaseSchema/options/schema.php',
+    'ezcDbSchemaOracleReader'                    => 'DatabaseSchema/handlers/oracle/reader.php',
+    'ezcDbSchemaOracleWriter'                    => 'DatabaseSchema/handlers/oracle/writer.php',
+    'ezcDbSchemaPersistentWriter'                => 'DatabaseSchema/handlers/persistent/writer.php',
+    'ezcDbSchemaPgsqlReader'                     => 'DatabaseSchema/handlers/pgsql/reader.php',
+    'ezcDbSchemaPgsqlWriter'                     => 'DatabaseSchema/handlers/pgsql/writer.php',
+    'ezcDbSchemaPhpArrayReader'                  => 'DatabaseSchema/handlers/php_array/reader.php',
+    'ezcDbSchemaPhpArrayWriter'                  => 'DatabaseSchema/handlers/php_array/writer.php',
+    'ezcDbSchemaSqliteReader'                    => 'DatabaseSchema/handlers/sqlite/reader.php',
+    'ezcDbSchemaSqliteWriter'                    => 'DatabaseSchema/handlers/sqlite/writer.php',
+    'ezcDbSchemaTable'                           => 'DatabaseSchema/structs/table.php',
+    'ezcDbSchemaTableDiff'                       => 'DatabaseSchema/structs/table_diff.php',
+    'ezcDbSchemaTypesValidator'                  => 'DatabaseSchema/validators/types.php',
+    'ezcDbSchemaValidator'                       => 'DatabaseSchema/validator.php',
+    'ezcDbSchemaXmlReader'                       => 'DatabaseSchema/handlers/xml/reader.php',
+    'ezcDbSchemaXmlWriter'                       => 'DatabaseSchema/handlers/xml/writer.php',
 );
 ?>
