10049bad484121b159abb197fe800a8acd8663d0
dr:ez.no <dev-null@apache.org>
2007-05-03 12:07:32 +0000

    - Update autoload file for ImageConversion, they are now sorted according to dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972315 13f79535-47bb-0310-9956-ffa450edef68

--- ImageConversion/src/image_autoload.php
+++ ImageConversion/src/image_autoload.php
@@ -1,53 +1,45 @@
 <?php
 /**
- * Autoload map for ImageConversion package.
+ * Autoloader definition for the ImageConversion component.
  *
- * @package ImageConversion
- * @version //autogentag//
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
+ * @package ImageConversion
  */
 
-return array (
-    'ezcImageConverter'                             => 'ImageConversion/converter.php',
-    'ezcImageConverterSettings'                     => 'ImageConversion/structs/converter_settings.php',
-
-    'ezcImageTransformation'                        => 'ImageConversion/transformation.php',
-
-    'ezcImageHandler'                               => 'ImageConversion/interfaces/handler.php',
-    'ezcImageHandlerSettings'                       => 'ImageConversion/structs/handler_settings.php',
-    'ezcImageMethodcallHandler'                     => 'ImageConversion/interfaces/methodcall_handler.php',
-    'ezcImageSaveOptions'                           => 'ImageConversion/options/save_options.php',
-
-    'ezcImageFilter'                                => 'ImageConversion/structs/filter.php',
-
-    'ezcImageGeometryFilters'                       => 'ImageConversion/interfaces/geometry.php',
-    'ezcImageColorspaceFilters'                     => 'ImageConversion/interfaces/colorspace.php',
-    'ezcImageEffectFilters'                         => 'ImageConversion/interfaces/effect.php',
-    'ezcImageWatermarkFilters'                      => 'ImageConversion/interfaces/watermark.php',
-    'ezcImageThumbnailFilters'                      => 'ImageConversion/interfaces/thumbnail.php',
-
-    'ezcImageGdHandler'                             => 'ImageConversion/handlers/gd.php',
-    'ezcImageGdBaseHandler'                         => 'ImageConversion/handlers/gd_base.php',
-
-    'ezcImageImagemagickHandler'                    => 'ImageConversion/handlers/imagemagick.php',
-    'ezcImageImagemagickBaseHandler'                => 'ImageConversion/handlers/imagemagick_base.php',
-    
-    'ezcImageException'                             => 'ImageConversion/exceptions/exception.php',
-    'ezcImageMissingFilterParameterException'       => 'ImageConversion/exceptions/missing_filter_parameter.php',
-    'ezcImageFileNotProcessableException'           => 'ImageConversion/exceptions/file_not_processable.php',
-    'ezcImageFileNameInvalidException'              => 'ImageConversion/exceptions/file_name_invalid.php',
-    'ezcImageFilterFailedException'                 => 'ImageConversion/exceptions/filter_failed.php',
-    'ezcImageFilterNotAvailableException'           => 'ImageConversion/exceptions/filter_not_available.php',
-    'ezcImageHandlerNotAvailableException'          => 'ImageConversion/exceptions/handler_not_available.php',
-    'ezcImageHandlerSettingsInvalidException'       => 'ImageConversion/exceptions/handler_settings_invalid.php',
-    'ezcImageInvalidFilterParameterException'       => 'ImageConversion/exceptions/invalid_filter_parameter.php',
-    'ezcImageInvalidReferenceException'             => 'ImageConversion/exceptions/invalid_reference.php',
-    'ezcImageMimeTypeUnsupportedException'          => 'ImageConversion/exceptions/mime_type_unsupported.php',
-    'ezcImageTransformationException'               => 'ImageConversion/exceptions/transformation.php',
-    'ezcImageTransformationAlreadyExistsException'  => 'ImageConversion/exceptions/transformation_already_exists.php',
-    'ezcImageTransformationNotAvailableException'   => 'ImageConversion/exceptions/transformation_not_available.php',
+return array(
+    'ezcImageException'                            => 'ImageConversion/exceptions/exception.php',
+    'ezcImageFileNameInvalidException'             => 'ImageConversion/exceptions/file_name_invalid.php',
+    'ezcImageFileNotProcessableException'          => 'ImageConversion/exceptions/file_not_processable.php',
+    'ezcImageFilterFailedException'                => 'ImageConversion/exceptions/filter_failed.php',
+    'ezcImageFilterNotAvailableException'          => 'ImageConversion/exceptions/filter_not_available.php',
+    'ezcImageHandlerNotAvailableException'         => 'ImageConversion/exceptions/handler_not_available.php',
+    'ezcImageHandlerSettingsInvalidException'      => 'ImageConversion/exceptions/handler_settings_invalid.php',
+    'ezcImageInvalidFilterParameterException'      => 'ImageConversion/exceptions/invalid_filter_parameter.php',
+    'ezcImageInvalidReferenceException'            => 'ImageConversion/exceptions/invalid_reference.php',
+    'ezcImageMimeTypeUnsupportedException'         => 'ImageConversion/exceptions/mime_type_unsupported.php',
+    'ezcImageMissingFilterParameterException'      => 'ImageConversion/exceptions/missing_filter_parameter.php',
+    'ezcImageTransformationAlreadyExistsException' => 'ImageConversion/exceptions/transformation_already_exists.php',
+    'ezcImageTransformationException'              => 'ImageConversion/exceptions/transformation.php',
+    'ezcImageTransformationNotAvailableException'  => 'ImageConversion/exceptions/transformation_not_available.php',
+    'ezcImageHandler'                              => 'ImageConversion/interfaces/handler.php',
+    'ezcImageMethodcallHandler'                    => 'ImageConversion/interfaces/methodcall_handler.php',
+    'ezcImageColorspaceFilters'                    => 'ImageConversion/interfaces/colorspace.php',
+    'ezcImageEffectFilters'                        => 'ImageConversion/interfaces/effect.php',
+    'ezcImageGdBaseHandler'                        => 'ImageConversion/handlers/gd_base.php',
+    'ezcImageGeometryFilters'                      => 'ImageConversion/interfaces/geometry.php',
+    'ezcImageImagemagickBaseHandler'               => 'ImageConversion/handlers/imagemagick_base.php',
+    'ezcImageThumbnailFilters'                     => 'ImageConversion/interfaces/thumbnail.php',
+    'ezcImageWatermarkFilters'                     => 'ImageConversion/interfaces/watermark.php',
+    'ezcImageConverter'                            => 'ImageConversion/converter.php',
+    'ezcImageConverterSettings'                    => 'ImageConversion/structs/converter_settings.php',
+    'ezcImageFilter'                               => 'ImageConversion/structs/filter.php',
+    'ezcImageGdHandler'                            => 'ImageConversion/handlers/gd.php',
+    'ezcImageHandlerSettings'                      => 'ImageConversion/structs/handler_settings.php',
+    'ezcImageImagemagickHandler'                   => 'ImageConversion/handlers/imagemagick.php',
+    'ezcImageSaveOptions'                          => 'ImageConversion/options/save_options.php',
+    'ezcImageTransformation'                       => 'ImageConversion/transformation.php',
 );
-
 ?>
