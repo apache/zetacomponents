ef9cc6797d273d1ec6f0dc15adf1b40bc6f76f03
ts:ez.no <dev-null@apache.org>
2007-05-04 07:53:03 +0000

    - Implemented issue #8487: an Interface for Persistent Objects. # We possibly need to discuss the name of the interface after the beta release, # since nobody objected so far I'm commiting for the beta.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ PersistentObject/src/interfaces/persistent_object.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * File containing the ezcPersistentObject interface
+ *
+ * @package PersistentObject
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * ezcPersistentObject is an (optional) interface for classes that provide persistent objects.
+ *
+ * The PersistentObject component does not require a class to inherit from a
+ * certain base class or implement a certain interface to be used with the
+ * component. However, this interface can (optionally) be implemented by your
+ * persistent classes, to ensure they provide all necessary methods.
+ *
+ * @package PersistentObject
+ */
+interface ezcPersistentObject
+{
+    /**
+     * Returns the current state of an object.
+     * This method returns an array representing the current state of the
+     * object. The array must contain a key for every attribute of the
+     * object, assigned to the value of the attribute.
+     * 
+     * @return array(string=>mixed) The state of the object.
+     */
+    public function getState();
+
+    /**
+     * Sets the state of the object.
+     * This method sets the state of the object accoring to a given array,
+     * which must conform to the standards defined at {@see getState()}.
+     * 
+     * @param array $state The new state for the object.
+     * @return void
+     */
+    public function setState( array $state );
+}
+
+?>
