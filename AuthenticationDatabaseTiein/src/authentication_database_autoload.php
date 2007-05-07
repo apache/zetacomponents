2ce9267f5fd7040f0f25dd13012b1c6b0b3ad5a3
dr:ez.no <dev-null@apache.org>
2007-05-07 06:51:13 +0000

    - Reorder autoload files, added class diagram.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972392 13f79535-47bb-0310-9956-ffa450edef68

--- AuthenticationDatabaseTiein/src/authentication_database_autoload.php
+++ AuthenticationDatabaseTiein/src/authentication_database_autoload.php
@@ -1,20 +1,17 @@
 <?php
 /**
- * Autoloader definition for the ezcAuthenticationDatabaseTiein component.
+ * Autoloader definition for the AuthenticationDatabaseTiein component.
  *
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
- * @package AuthenticationDatabaseTiein 
- * @version //autogen//
+ * @package AuthenticationDatabaseTiein
  */
 
 return array(
-
-    // authentication Database filter
-    'ezcAuthenticationDatabaseFilter'         => 'AuthenticationDatabaseTiein/filters/database/database_filter.php',
-    'ezcAuthenticationDatabaseInfo'           => 'AuthenticationDatabaseTiein/filters/database/database_info.php',
-    'ezcAuthenticationDatabaseOptions'        => 'AuthenticationDatabaseTiein/filters/database/database_options.php',
-
-    );
+    'ezcAuthenticationDatabaseFilter'  => 'AuthenticationDatabaseTiein/filters/database/database_filter.php',
+    'ezcAuthenticationDatabaseInfo'    => 'AuthenticationDatabaseTiein/filters/database/database_info.php',
+    'ezcAuthenticationDatabaseOptions' => 'AuthenticationDatabaseTiein/filters/database/database_options.php',
+);
 ?>
