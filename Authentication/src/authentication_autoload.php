930a9b98e1e87ef2afe5c7859b9a41ca9d9181bf
dr:ez.no <dev-null@apache.org>
2007-05-07 06:49:06 +0000

    - Fixed autoload order.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972391 13f79535-47bb-0310-9956-ffa450edef68

--- Authentication/src/authentication_autoload.php
+++ Authentication/src/authentication_autoload.php
@@ -1,62 +1,39 @@
 <?php
 /**
- * Autoloader definition for the ezcAuthentication component.
+ * Autoloader definition for the Authentication component.
  *
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
  * @filesource
  * @package Authentication
- * @version //autogen//
  */
 
 return array(
-
-    // authentication class
-    'ezcAuthentication'                       => 'Authentication/authentication.php',
-
-    // authentication credentials
-    'ezcAuthenticationCredentials'            => 'Authentication/credentials/credentials.php',
-    'ezcAuthenticationIdCredentials'          => 'Authentication/credentials/id_credentials.php',
-    'ezcAuthenticationPasswordCredentials'    => 'Authentication/credentials/password_credentials.php',
-
-    // authentication exceptions
-    'ezcAuthenticationException'              => 'Authentication/exceptions/authentication_exception.php',
-
-    // authentication interfaces
-    'ezcAuthenticationFilter'                 => 'Authentication/interfaces/authentication_filter.php',
-    'ezcAuthenticationSession'                => 'Authentication/interfaces/authentication_session.php',
-    'ezcAuthenticationStatus'                 => 'Authentication/interfaces/authentication_status.php',
-
-    // authentication options
-    'ezcAuthenticationOptions'                => 'Authentication/options/authentication_options.php',
-    'ezcAuthenticationFilterOptions'          => 'Authentication/options/filter_options.php',
-
-    // authentication Group filter
-    'ezcAuthenticationGroupFilter'            => 'Authentication/filters/group/group_filter.php',
-    'ezcAuthenticationGroupOptions'           => 'Authentication/filters/group/group_options.php',
-
-    // authentication Htpasswd filter
-    'ezcAuthenticationHtpasswdFilter'         => 'Authentication/filters/htpasswd/htpasswd_filter.php',
-    'ezcAuthenticationHtpasswdOptions'        => 'Authentication/filters/htpasswd/htpasswd_options.php',
-
-    // authentication LDAP filter
-    'ezcAuthenticationLdapFilter'             => 'Authentication/filters/ldap/ldap_filter.php',
-    'ezcAuthenticationLdapException'          => 'Authentication/filters/ldap/ldap_exception.php',
-    'ezcAuthenticationLdapInfo'               => 'Authentication/filters/ldap/ldap_info.php',
-    'ezcAuthenticationLdapOptions'            => 'Authentication/filters/ldap/ldap_options.php',
-
-    // authentication Session filter
-    'ezcAuthenticationSessionFilter'          => 'Authentication/filters/session/session_filter.php',
-    'ezcAuthenticationSessionOptions'         => 'Authentication/filters/session/session_options.php',
-
-    // authentication Token filter
-    'ezcAuthenticationTokenFilter'            => 'Authentication/filters/token/token_filter.php',
-    'ezcAuthenticationTokenOptions'           => 'Authentication/filters/token/token_options.php',
-
-    // authentication TypeKey filter
-    'ezcAuthenticationTypekeyFilter'          => 'Authentication/filters/typekey/typekey_filter.php',
-    'ezcAuthenticationTypekeyException'       => 'Authentication/filters/typekey/typekey_exception.php',
-    'ezcAuthenticationTypekeyOptions'         => 'Authentication/filters/typekey/typekey_options.php',
-
-    );
+    'ezcAuthenticationException'           => 'Authentication/exceptions/authentication_exception.php',
+    'ezcAuthenticationCredentials'         => 'Authentication/credentials/credentials.php',
+    'ezcAuthenticationFilter'              => 'Authentication/interfaces/authentication_filter.php',
+    'ezcAuthenticationFilterOptions'       => 'Authentication/options/filter_options.php',
+    'ezcAuthenticationSession'             => 'Authentication/interfaces/authentication_session.php',
+    'ezcAuthentication'                    => 'Authentication/authentication.php',
+    'ezcAuthenticationGroupFilter'         => 'Authentication/filters/group/group_filter.php',
+    'ezcAuthenticationGroupOptions'        => 'Authentication/filters/group/group_options.php',
+    'ezcAuthenticationHtpasswdFilter'      => 'Authentication/filters/htpasswd/htpasswd_filter.php',
+    'ezcAuthenticationHtpasswdOptions'     => 'Authentication/filters/htpasswd/htpasswd_options.php',
+    'ezcAuthenticationIdCredentials'       => 'Authentication/credentials/id_credentials.php',
+    'ezcAuthenticationLdapException'       => 'Authentication/filters/ldap/ldap_exception.php',
+    'ezcAuthenticationLdapFilter'          => 'Authentication/filters/ldap/ldap_filter.php',
+    'ezcAuthenticationLdapInfo'            => 'Authentication/filters/ldap/ldap_info.php',
+    'ezcAuthenticationLdapOptions'         => 'Authentication/filters/ldap/ldap_options.php',
+    'ezcAuthenticationOptions'             => 'Authentication/options/authentication_options.php',
+    'ezcAuthenticationPasswordCredentials' => 'Authentication/credentials/password_credentials.php',
+    'ezcAuthenticationSessionFilter'       => 'Authentication/filters/session/session_filter.php',
+    'ezcAuthenticationSessionOptions'      => 'Authentication/filters/session/session_options.php',
+    'ezcAuthenticationStatus'              => 'Authentication/interfaces/authentication_status.php',
+    'ezcAuthenticationTokenFilter'         => 'Authentication/filters/token/token_filter.php',
+    'ezcAuthenticationTokenOptions'        => 'Authentication/filters/token/token_options.php',
+    'ezcAuthenticationTypekeyException'    => 'Authentication/filters/typekey/typekey_exception.php',
+    'ezcAuthenticationTypekeyFilter'       => 'Authentication/filters/typekey/typekey_filter.php',
+    'ezcAuthenticationTypekeyOptions'      => 'Authentication/filters/typekey/typekey_options.php',
+);
 ?>
