d3ed4cd439d030fd3d32a8c87ccb3a8f1089c803
dr:ez.no <dev-null@apache.org>
2007-05-31 11:44:37 +0000

    - .php files need to be valid PHP files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972653 13f79535-47bb-0310-9956-ffa450edef68

--- WorkflowDatabaseTiein/docs/load_dba.php
+++ WorkflowDatabaseTiein/docs/load_dba.php
@@ -1,4 +1,6 @@
+<?php
 $db = ezcDbInstance::get(); // replace if you get your database instance differently
 
 $schema = ezcDbSchema::createFromFile( 'array', 'workflow.dba' );
 $schema->writeToDb( $db );
+?>
