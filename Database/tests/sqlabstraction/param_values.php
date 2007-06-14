090a09540dcba5d73f55675967529bdd95de52fe
kn:ez.no <dev-null@apache.org>
2007-06-14 13:52:43 +0000

    - Added tests for bindValues including value type spcification   # Not added to test suite for now, because of massive errors   # MySQL: Runs just fine   # SQLite: Every test with param specification results in error   # PGSql: Segfault on first param specification
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972743 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Database/tests/sqlabstraction/param_values.php
@@ -0,0 +1,322 @@
+<?php
+/**
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Database
+ * @subpackage Tests
+ */
+
+/**
+ * testing sql abstraction for common rdbms limits
+ *
+ * @package Database
+ * @subpackage Tests
+ */
+class ezcRdbmsLimitTest extends ezcTestCase
+{
+
+    protected $boolColumn;
+    protected $intColumn;
+    protected $nullColumn;
+    protected $strColumn;
+    protected $blobColumn;
+    protected $clobColumn;
+
+    protected $blob;
+    protected $clob;
+
+    protected $columnMapping = array(
+        'bool' => array(
+            'ezcDbHandlerMysql' => 'boolean',
+            'ezcDbHandlerOracle' => 'char',
+            'ezcDbHandlerPgsql' => 'boolean',
+            'ezcDbHandlerSqlite' => 'integer',
+        ),
+        'int' => array(
+            'ezcDbHandlerMysql' => 'bigint',
+            'ezcDbHandlerOracle' => 'number',
+            'ezcDbHandlerPgsql' => 'bigint',
+            'ezcDbHandlerSqlite' => 'integer',
+        ),
+        'null' => array(
+            'ezcDbHandlerMysql' => 'bigint',
+            'ezcDbHandlerOracle' => 'number',
+            'ezcDbHandlerPgsql' => 'bigint',
+            'ezcDbHandlerSqlite' => 'integer',
+        ),
+        'str' => array(
+            'ezcDbHandlerMysql' => 'varchar( 255 )',
+            'ezcDbHandlerOracle' => 'varchar2( 255 )',
+            'ezcDbHandlerPgsql' => 'varchar( 255 )',
+            'ezcDbHandlerSqlite' => 'text',
+        ),
+        'blob' => array(
+            'ezcDbHandlerMysql' => 'longblob',
+            'ezcDbHandlerOracle' => 'blob',
+            'ezcDbHandlerPgsql' => 'bytea',
+            'ezcDbHandlerSqlite' => 'blob',
+        ),
+        'clob' => array(
+            'ezcDbHandlerMysql' => 'longtext',
+            'ezcDbHandlerOracle' => 'clob',
+            'ezcDbHandlerPgsql' => 'text',
+            'ezcDbHandlerSqlite' => 'clob',
+        ),
+    );
+
+    public static function suite()
+    {
+        return new PHPUnit_Framework_TestSuite( 'ezcRdbmsLimitTest' );
+    }
+
+    protected function setUp()
+    {
+        try {
+            $db = ezcDbInstance::get();
+        }
+        catch ( Exception $e )
+        {
+            $this->markTestSkipped();
+        }
+
+        $this->assertNotNull( $db, 'Database instance is not initialized.' );
+
+        $db->exec( 'CREATE TABLE ' .
+            $db->quoteIdentifier( __CLASS__ ) . 
+            '( ' . 
+                ( $this->boolColumn = $db->quoteIdentifier( 'bool' ) ) . ' ' . $this->columnMapping['bool'][get_class( $db )] . ', ' .
+                ( $this->intColumn =  $db->quoteIdentifier( 'int' )  ) . ' ' . $this->columnMapping['int' ][get_class( $db )] . ', ' .
+                ( $this->nullColumn = $db->quoteIdentifier( 'null' ) ) . ' ' . $this->columnMapping['null'][get_class( $db )] . ', ' .
+                ( $this->strColumn =  $db->quoteIdentifier( 'str' )  ) . ' ' . $this->columnMapping['str' ][get_class( $db )] . ', ' .
+                ( $this->blobColumn = $db->quoteIdentifier( 'blob' ) ) . ' ' . $this->columnMapping['blob'][get_class( $db )] . ', ' .
+                ( $this->clobColumn = $db->quoteIdentifier( 'clob' ) ) . ' ' . $this->columnMapping['clob'][get_class( $db )] .
+            ')'
+        );
+
+        // Initialize content
+        $this->blob = str_repeat( "\x00\x05\x10\x42", 1024 );
+        $this->clob = str_repeat( "test", 1024 );
+    }
+
+    protected function tearDown()
+    {
+        $db = ezcDbInstance::get();
+
+        $db->exec( 'DROP TABLE ' .
+            $db->quoteIdentifier( __CLASS__ )
+        );
+    }
+
+    public function testBooleanParam()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->boolColumn,
+                $query->bindValue( true ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testBooleanParamForce()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->boolColumn,
+                $query->bindValue( true, PDO::PARAM_BOOL ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testBooleanParamForceBroken()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->boolColumn,
+                $query->bindValue( 'some string', PDO::PARAM_BOOL ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testIntegerParam()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->intColumn,
+                $query->bindValue( 42 ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testIntegerParamForce()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->intColumn,
+                $query->bindValue( 42, PDO::PARAM_INT ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testIntegerParamForceBroken()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->intColumn,
+                $query->bindValue( '42 strings', PDO::PARAM_INT ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testStringParam()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->strColumn,
+                $query->bindValue( 'some string' ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testStringParamForce()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->strColumn,
+                $query->bindValue( 'some string', PDO::PARAM_INT ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testStringParamForceBroken()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->strColumn,
+                $query->bindValue( true, PDO::PARAM_INT ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testClobParam()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->clobColumn,
+                $query->bindValue( $this->clob ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testClobParamForce()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->clobColumn,
+                $query->bindValue( $this->clob, PDO::PARAM_LOB ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testClobParamForceBroken()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->clobColumn,
+                $query->bindValue( 42, PDO::PARAM_LOB ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testBlobParam()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->blobColumn,
+                $query->bindValue( $this->blob ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testBlobParamForce()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->blobColumn,
+                $query->bindValue( $this->blob, PDO::PARAM_LOB ) 
+            );
+        $query->prepare()->execute();
+    }
+
+    public function testBlobParamForceBroken()
+    {
+        $db = ezcDbInstance::get();
+
+        $query = $db->createInsertQuery();
+        $query
+            ->insertInto( $db->quoteIdentifier( __CLASS__ ) )
+            ->set( 
+                $this->blobColumn,
+                $query->bindValue( 42, PDO::PARAM_LOB ) 
+            );
+        $query->prepare()->execute();
+    }
+}
+?>
