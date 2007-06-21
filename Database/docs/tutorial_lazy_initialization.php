cf7def4f89b33a73fc94914af251669216ce3662
kn:ez.no <dev-null@apache.org>
2007-06-21 13:20:40 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972823 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Database/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+class customLazyDatabaseConfiguration
+{
+    public static function configureObject( $instance )
+    {
+        switch ( $instance )
+        {
+            case false: // Default instance
+                return ezcDbFactory::create( 'mysql://user:password@host/database' );
+            case 'sqlite':
+                return ezcDbFactory::create( 'sqlite://:memory:' );
+        }
+    }
+}
+
+ezcBaseInit::setCallback( 
+    'ezcInitDatabaseInstance', 
+    'customLazyDatabaseConfiguration'
+);
+
+// Create and configure default mysql connection
+$db = ezcDbInstance::get();
+
+// Create and configure additional sqlite connection
+$sb = ezcDbInstance::get( 'sqlite' );
+?>
