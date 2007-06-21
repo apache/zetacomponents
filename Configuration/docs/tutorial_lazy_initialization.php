1d224ee51056abde10b07241286c4fa9ef1e5d39
kn:ez.no <dev-null@apache.org>
2007-06-21 13:20:52 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972824 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Configuration/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+class customLazyConfigurationConfiguration
+{
+    public static function configureObject( $cfg )
+    {
+        $cfg->init( 'ezcConfigurationIniReader', dirname( __FILE__ ) . '/examples' );
+    }
+}
+
+ezcBaseInit::setCallback( 
+    'ezcInitConfigurationManager', 
+    'customLazyConfigurationConfiguration'
+);
+
+// Classes loaded and configured on first request
+$cfg = ezcConfigurationManager::getInstance();
+
+$pw = $cfg->getSetting( 'settings', 'db', 'password' );
+echo "The password is '{$pw}'.\n";
+?>
