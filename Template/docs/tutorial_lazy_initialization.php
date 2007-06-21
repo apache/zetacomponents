3180e8dde7e8eac4e6d306a848837bac6782173e
kn:ez.no <dev-null@apache.org>
2007-06-21 13:21:11 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972825 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Template/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+class customLazyTemplateConfiguration
+{
+    public static function configureObject( $cfg )
+    {
+        $cfg->templatePath = "/usr/share/templates";
+        $cfg->compilePath = "/tmp/compiled_templates";
+        $cfg->context = new ezcTemplateXhtmlContext();
+    }
+}
+
+ezcBaseInit::setCallback( 
+    'ezcInitTemplateConfiguration', 
+    'customLazyTemplateConfiguration'
+);
+
+$t = new ezcTemplate();
+$t->process( "hello_world.ezt" );
+?>
