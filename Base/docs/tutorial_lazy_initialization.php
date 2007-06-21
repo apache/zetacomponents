b201425930d1c4e0f2589ecd8ed04ba35920925b
kn:ez.no <dev-null@apache.org>
2007-06-21 13:18:48 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972819 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Base/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+// Create a custom class implementing the singleton pattern
+class customSingleton
+{
+    protected static $instance;
+
+    public static function getInstance()
+    {
+        if ( self::$instance === null )
+        {
+            self::$instance = new customSingleton();
+            ezcBaseInit::fetchConfig( 'customKey', self::$instance );
+        }
+
+        return self::$instance;
+    }
+}
+
+// Implement your configuration class
+class customSingletonConfiguration
+{
+    public static function configureObject( customSingleton $object )
+    {
+        echo "Configure customSingleton.\n";
+        $object->value = 42;
+    }
+}
+
+// Register for lazy initilization
+ezcBaseInit::setCallback( 'customKey', 'customSingletonConfiguration' );
+
+// Configure on first initilization
+$object = customSingleton::getInstance();
+var_dump( $object->value );
+
+?>
