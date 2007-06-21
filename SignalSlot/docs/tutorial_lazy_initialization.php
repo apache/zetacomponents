8dc77f39afa66efc6f550fa6139c5eb437bd2c8e
kn:ez.no <dev-null@apache.org>
2007-06-21 13:20:02 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972821 13f79535-47bb-0310-9956-ffa450edef68

---
+++ SignalSlot/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+class customLazySignalConfiguration
+{
+    public static function configureObject( $signal )
+    {
+        $signal->connect( 'default', 'signal', 'customFunction' );
+    }
+}
+
+ezcBaseInit::setCallback( 
+    'ezcInitSignalStaticConnections', 
+    'customLazySignalConfiguration'
+);
+
+function customFunction()
+{
+    echo "Custom function called with:\n", var_export( func_get_args(), true );
+}
+
+$signals = new ezcSignalCollection();
+$signals->emit( 'signal', 42 );
+?>
