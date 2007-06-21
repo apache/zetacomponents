81296548d7e6ba32cabdf6b39f05addcb47d88df
kn:ez.no <dev-null@apache.org>
2007-06-21 13:19:41 +0000

    - Added tutorial section about lazy initilization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972820 13f79535-47bb-0310-9956-ffa450edef68

---
+++ EventLog/docs/tutorial_lazy_initialization.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'tutorial_autoload.php';
+
+class customLazyLogConfiguration
+{
+    public static function configureObject( $log )
+    {
+        $writeAll = new ezcLogUnixFileWriter( "/tmp/logs", "general.log" );
+        $log->getMapper()->appendRule( new ezcLogFilterRule( new ezcLogFilter, $writeAll, true ) );
+    }
+}
+
+ezcBaseInit::setCallback( 
+    'ezcInitLog', 
+    'customLazyLogConfiguration'
+);
+
+// Use log
+$log = ezcLog::getInstance();
+
+$log->log( "Paid with credit card.", ezcLog::SUCCESS_AUDIT, array( "category" => "shop" ) );
+$log->log( "The credit card information is removed.", ezcLog::NOTICE, array( "category" => "shop" ) );
+?>
