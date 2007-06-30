c825c03c57c27e77a51751ba200c91353e411351
ts:ez.no <dev-null@apache.org>
2007-06-30 06:18:18 +0000

    - Removed @private tags. # Missed those from the discussion on the ML longer time ago.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972934 13f79535-47bb-0310-9956-ffa450edef68

--- ImageConversion/src/handlers/gd_base.php
+++ ImageConversion/src/handlers/gd_base.php
@@ -7,7 +7,6 @@
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @filesource
- * @access private
  */
 
 /**
@@ -25,7 +24,6 @@
  *
  * @package ImageConversion
  * @version //autogentag//
- * @access private
  */
 class ezcImageGdBaseHandler extends ezcImageMethodcallHandler 
 {
--- ImageConversion/src/handlers/imagemagick_base.php
+++ ImageConversion/src/handlers/imagemagick_base.php
@@ -7,7 +7,6 @@
  * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @filesource
- * @access private
  */
 
 
