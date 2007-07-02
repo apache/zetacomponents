36a0bde18a0b732243f807f4bab5e2fd9bb3d3e0
dr:ez.no <dev-null@apache.org>
2007-07-02 06:42:26 +0000

    - Make sure we mention the interface here.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@972941 13f79535-47bb-0310-9956-ffa450edef68

--- EventLog/docs/tutorial_lazy_initialization.php
+++ EventLog/docs/tutorial_lazy_initialization.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'tutorial_autoload.php';
 
-class customLazyLogConfiguration
+class customLazyLogConfiguration implements ezcBaseConfigurationInitializer
 {
     public static function configureObject( $log )
     {
