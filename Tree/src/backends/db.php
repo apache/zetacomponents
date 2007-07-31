72d5504248fe786cfadae4b56f2c2fbdb59d0912
dr:ez.no <dev-null@apache.org>
2007-07-31 08:12:03 +0000

    - Removed superfluous method definitions.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973078 13f79535-47bb-0310-9956-ffa450edef68

--- Tree/src/backends/db.php
+++ Tree/src/backends/db.php
@@ -48,57 +48,5 @@ abstract class ezcTreeDb extends ezcTree implements ezcTreeBackend
 
         return count( $s->fetchAll() ) ? true : false;
     }
-
-    public function fetchChildren( $id )
-    {
-    }
-
-    public function fetchPath( $id )
-    {
-    }
-
-    public function fetchSubtree( $id, $searchMethod = ezcTree::DEPTH_FIRST )
-    {
-    }
-
-    public function getChildCount( $id )
-    {
-    }
-
-    public function getChildCountRecursive( $id )
-    {
-    }
-
-    public function getPathLength( $id )
-    {
-    }
-
-    public function hasChildNodes( $id )
-    {
-    }
-
-    public function isDecendantOf( $childId, $parentId )
-    {
-    }
-
-    public function isSiblingOf( $child1Id, $child2Id )
-    {
-    }
-
-    public function setRootNode( ezcTreeNode $node )
-    {
-    }
-
-    public function addChild( $parentId, ezcTreeNode $childNode )
-    {
-    }
-
-    public function delete( $id )
-    {
-    }
-
-    public function move( $id, $targetParentId )
-    {
-    }
 }
 ?>
