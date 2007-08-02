050b0e086ee68b1a645693c85d9e70074d5549ea
dr:ez.no <dev-null@apache.org>
2007-08-02 12:58:50 +0000

    - And move those two tests over as well.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973121 13f79535-47bb-0310-9956-ffa450edef68

--- Tree/tests/db_parent_child_tree.php
+++ Tree/tests/db_parent_child_tree.php
@@ -72,51 +72,6 @@ class ezcTreeDbParentChildTest extends ezcDbTreeTest
         return $tree;
     }
 
-    public function testMultipleDataFields()
-    {
-        $tree = $this->setUpTestTree( 'data', null );
-
-        $node8 = $tree->fetchNodeById( 8 ); // returns 8
-        self::assertType( 'ezcTreeNode', $node8 );
-        self::assertSame( '8', $node8->id );
-        self::assertSame( array( 'data' => 'Node 8' ), $node8->data );
-    }
-
-    public function testStoreUpdatedDataMultipleDataFields()
-    {
-        $tree = $this->setUpEmptyTestTree( 'datam', null );
-
-        $root = $tree->createNode( 1, array( 'name' => 'Harald V', 'born' => '1937' ) );
-        $tree->setRootNode( $root );
-
-        $root->addChild( $tree->createNode( 2, array( 'name' => 'Haakon', 'born' => '1973' ) ) );
-        $root->addChild( $tree->createNode( 3, array( 'name' => 'Märtha Louise', 'born' => '1971' ) ) );
-
-        // start over
-        $store = new ezcTreeDbExternalTableDataStore( $this->dbh, 'datam', 'id' );
-        $tree = new ezcTreeDbParentChild(
-            $this->dbh,
-            'parent_child',
-            $store
-        );
-
-        $haakon = $tree->fetchNodeById( 2 );
-        self::assertEquals( array( 'name' => 'Haakon', 'born' => '1973' ), $haakon->data );
-        $haakon->data = array( 'name' => 'Haakon', 'born' => 1981 );
-        $haakon->data = array( 'name' => 'Haakon', 'born' => 1983 );
-
-        // start over
-        $store = new ezcTreeDbExternalTableDataStore( $this->dbh, 'datam', 'id' );
-        $tree = new ezcTreeDbParentChild(
-            $this->dbh,
-            'parent_child',
-            $store
-        );
-
-        $haakon = $tree->fetchNodeById( 2 );
-        self::assertEquals( array( 'name' => 'Haakon', 'born' => '1983' ), $haakon->data );
-    }
-
     public static function suite()
     {
          return new PHPUnit_Framework_TestSuite( "ezcTreeDbParentChildTest" );
