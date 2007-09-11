eca42d97b040f519fb2caf3047052ccdb44364f3
ts:ez.no <dev-null@apache.org>
2007-09-11 08:34:36 +0000

    - Started implementing request classes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973350 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/src/request.php
+++ Webdav/src/request.php
@@ -8,15 +8,15 @@
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 /**
- * Struct containing all relevant information about a webdav request, so that it;
- * can be handler properly by the server and the webdav backend. Will be
- * extended by specialized request classes which represent the several webdav
- * request types.
+ * Base class for request objects.
+ * This base class must be extended by all request representation classes.
  *
  * @version //autogentag//
  * @package Webdav
+ * @copyright Copyright (C) 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
  */
-class ezcWebdavRequest
+abstract class ezcWebdavRequest
 {
     /**
      * Constants for request types.
@@ -32,20 +32,98 @@ class ezcWebdavRequest
     const MKCOL     = 256;
 
     /**
-     * Optional property containing the name of the requesting client
-     * 
-     * @var string
+     * Container to hold the properties
+     *
+     * @var array(string=>mixed)
      */
-    public $clientName = false;
+    protected $properties = array();
+    
+    /**
+     * Creates a new request object.
+     * All request objects can have 
+     *
+     * @param array $headers Headers.
+     * @return void
+     */
+    public function __construct( ezcWebdavHeaderStorage $headers = null )
+    {
+        $this->headers = ( $headers === null ) ? new ezcWebdavHeaderStorage() : $headers;
+    }
+
+    public function validateHeaders()
+    {
+        // @todo: To be implemented...
+    }
 
     /**
-     * Optional property containing the remote IP address of the client
+     * Sets a property.
+     * This method is called when an property is to be set.
      * 
-     * @var string
+     * @param string $propertyName The name of the property to set.
+     * @param mixed $propertyValue The property value.
+     * @ignore
+     *
+     * @throws ezcBasePropertyNotFoundException
+     *         if the given property does not exist.
+     * @throws ezcBaseValueException
+     *         if the value to be assigned to a property is invalid.
+     * @throws ezcBasePropertyPermissionException
+     *         if the property to be set is a read-only property.
      */
-    public $remoteAddress = false;
+    public function __set( $propertyName, $propertyValue )
+    {
+        switch ( $propertyName )
+        {
+            case 'headers':
+                if ( is_array( $propertyValue ) === false )
+                {
+                    throw new ezcBaseValueException( $propertyName, $propertyValue, 'array' );
+                }
+                break;
+            default:
+                throw new ezcBasePropertyNotFoundException( $propertyName );
+        }
+        $this->properties[$propertyName] = $propertyValue;
+    }
 
-    // ...
+    /**
+     * Property get access.
+     * Simply returns a given property.
+     * 
+     * @throws ezcBasePropertyNotFoundException
+     *         If a the value for the property propertys is not an instance of
+     * @param string $propertyName The name of the property to get.
+     * @return mixed The property value.
+     *
+     * @ignore
+     *
+     * @throws ezcBasePropertyNotFoundException
+     *         if the given property does not exist.
+     * @throws ezcBasePropertyPermissionException
+     *         if the property to be set is a write-only property.
+     */
+    public function __get( $propertyName )
+    {
+        if ( $this->__isset( $propertyName ) === true )
+        {
+            return $this->properties[$propertyName];
+        }
+        throw new ezcBasePropertyNotFoundException( $propertyName );
+    }
+
+    /**
+     * Returns if a property exists.
+     * Returns true if the property exists in the {@link $properties} array
+     * (even if it is null) and false otherwise. 
+     *
+     * @param string $propertyName Option name to check for.
+     * @return void
+     * @ignore
+     */
+    public function __isset( $propertyName )
+    {
+        return array_key_exists( $propertyName, $this->properties );
+    }
 }
 
 ?>
