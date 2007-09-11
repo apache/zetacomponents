059c3c21c8c2acb774113ccfb6d26013438badb3
ts:ez.no <dev-null@apache.org>
2007-09-11 09:07:16 +0000

    - Started implementing response base class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973354 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/src/response.php
+++ Webdav/src/response.php
@@ -8,22 +8,109 @@
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 /**
- * Struct containing all relevant information about a webdav response, so that
- * it can be handled properly by transport mechanism. Will be extended by more
- * specific responses.
+ * Base class for all response objects.
+ * This base class must be extended by all response representation classes.
  *
  * @version //autogentag//
  * @package Webdav
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
  */
 class ezcWebdavResponse
 {
     /**
-     * Optional property containing the name of the requesting client
-     * 
-     * @var string
+     * Container to hold the properties
+     *
+     * @var array(string=>mixed)
      */
-    public $serverName = 'ezcWebdavServer';
-    // ...
+    protected $properties = array();
+    
+    /**
+     * Creates a new request object.
+     * All request objects can have 
+     *
+     * @param array $headers Headers.
+     * @return void
+     */
+    public function __construct( ezcWebdavHeaderStorage $headers = null )
+    {
+        $this->headers = ( $headers === null ) ? new ezcWebdavHeaderStorage() : $headers;
+    }
+
+    public function validateHeaders()
+    {
+        // @todo: To be implemented...
+    }
+
+    /**
+     * Sets a property.
+     * This method is called when an property is to be set.
+     * 
+     * @param string $propertyName The name of the property to set.
+     * @param mixed $propertyValue The property value.
+     * @ignore
+     *
+     * @throws ezcBasePropertyNotFoundException
+     *         if the given property does not exist.
+     * @throws ezcBaseValueException
+     *         if the value to be assigned to a property is invalid.
+     * @throws ezcBasePropertyPermissionException
+     *         if the property to be set is a read-only property.
+     */
+    public function __set( $propertyName, $propertyValue )
+    {
+        switch ( $propertyName )
+        {
+            case 'headers':
+                if ( is_array( $propertyValue ) === false )
+                {
+                    throw new ezcBaseValueException( $propertyName, $propertyValue, 'array' );
+                }
+                break;
+            default:
+                throw new ezcBasePropertyNotFoundException( $propertyName );
+        }
+        $this->properties[$propertyName] = $propertyValue;
+    }
+
+    /**
+     * Property get access.
+     * Simply returns a given property.
+     * 
+     * @throws ezcBasePropertyNotFoundException
+     *         If a the value for the property propertys is not an instance of
+     * @param string $propertyName The name of the property to get.
+     * @return mixed The property value.
+     *
+     * @ignore
+     *
+     * @throws ezcBasePropertyNotFoundException
+     *         if the given property does not exist.
+     * @throws ezcBasePropertyPermissionException
+     *         if the property to be set is a write-only property.
+     */
+    public function __get( $propertyName )
+    {
+        if ( $this->__isset( $propertyName ) === true )
+        {
+            return $this->properties[$propertyName];
+        }
+        throw new ezcBasePropertyNotFoundException( $propertyName );
+    }
+
+    /**
+     * Returns if a property exists.
+     * Returns true if the property exists in the {@link $properties} array
+     * (even if it is null) and false otherwise. 
+     *
+     * @param string $propertyName Option name to check for.
+     * @return void
+     * @ignore
+     */
+    public function __isset( $propertyName )
+    {
+        return array_key_exists( $propertyName, $this->properties );
+    }
 }
 
 ?>
