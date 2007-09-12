b5bd1222103d4c9fd3352648c1fb45a5c61f865d
as:ez.no <dev-null@apache.org>
2007-09-12 07:17:11 +0000

    - Fixed wrong package.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973390 13f79535-47bb-0310-9956-ffa450edef68

--- TreePersistentObjectTiein/src/tree_persistent_autoload.php
+++ TreePersistentObjectTiein/src/tree_persistent_autoload.php
@@ -6,7 +6,7 @@
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @version //autogentag//
  * @filesource
- * @package TreePersistentObjectTieinPersistentObjectTiein
+ * @package TreePersistentObjectTiein
  */
 
 return array(
