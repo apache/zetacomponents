f109c0f942c84f30439b1dae548d3c864c4d3579
ts:ez.no <dev-null@apache.org>
2007-09-14 11:58:43 +0000

    - Started implementation of PROPFIND request parsing. - Added log for the initial PROPFIND request, issued by cadaver.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973458 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/src/transport.php
+++ Webdav/src/transport.php
@@ -19,6 +19,13 @@
  */
 class ezcWebdavTransport
 {
+
+    /**
+     * Regedx to parse the <getcontenttype /> XML elemens content.
+     * Example: 'text/html; charset=UTF-8'
+     */
+    const GETCONTENTTYPE_REGEX = '(^(?P<mime>\w+/\w+)\s*;\s*charset\s*=\s*(?P<charset>.+)\s*$)i';
+
     /**
      * Parses the webserver environment variables to create a proper request
      * object from then containing all relevant information to handle the
@@ -47,11 +54,24 @@ class ezcWebdavTransport
         }
     }
 
+    /**
+     * Parses the PROPFIN request and returns a request object.
+     * This method is responsible for parsing the PROPFIND request. It
+     * retrieves the current request URI in $uri and the request body as $body.
+     * The return value, if no exception is thrown, is a valid {@link
+     * ezcWebdavPropFindRequest} object.
+     * 
+     * @param string $uri 
+     * @param string $body 
+     * @return ezcWebdavPropFindRequest
+     */
     protected function parsePropFindRequest( $uri, $body )
     {
         $request = new ezcWebdavPropFindRequest( $uri );
 
-        $dom = DOMDocument::loadXML( $body );
+        $dom = new DOMDocument();
+        $dom->loadXML( $body, LIBXML_NOWARNING );
+
         if ( $dom->documentElement->tagName !== 'propfind' )
         {
             throw new ezcWebdavInvalidRequestBodyException(
@@ -66,6 +86,7 @@ class ezcWebdavTransport
                 "Element <propfind /> does not have a child element."
             );
         }
+
         switch ( $dom->documentElement->firstChild->tagName )
         {
             case 'allprop':
@@ -76,7 +97,7 @@ class ezcWebdavTransport
                 break;
             case 'prop':
                 $request->prop = $this->extractProperties(
-                    $dom->documentElement->getElementsByTagNameNS( 'DAV:', 'prop' )
+                    $dom->documentElement->firstChild->childNodes
                 );
                 break;
             default:
@@ -86,6 +107,206 @@ class ezcWebdavTransport
                 );
         }
         return $request;
+    }
+
+    /**
+     * Returns extracted properties in an ezcWebdavPropertyStorage.
+     * This method receives a DOMNodeList $domNodes, which must contain a set
+     * of DOMElement objects, while each of those represents a WebDAV property.
+     * The list may contain live properties as well as dead ones. Live
+     * properties as defined in RFC 2518 are currently recognized. All other
+     * properties in the DAV: namespace are silently ignored. Dead properties
+     * are parsed.
+     * 
+     * @param DOMNodeList $domNodes 
+     * @return ezcWebdavPropertyStorage
+     */
+    protected function extractProperties( DOMNodeList $domNodes )
+    {
+        $storage = new ezcWebdavPropertyStorage();
+
+        for ( $i = 0; $i < $domNodes->length; ++$i )
+        {
+            $currentNode = $domNodes->item( $i );
+            
+            // DAV: namespace indicates live property!
+            if ( $currentNode->namespaceURI === 'DAV:' )
+            {
+                $property = $this->extractLiveProperty( $currentNode );
+                // In case we don't know the property, we currently ignore it!
+                if ( $property !== null )
+                {
+                    $storage->append( $property );
+                }
+            }
+            
+            // Other namespaces are always dead properties
+            else
+            {
+                // Create standalone XML for property
+                // @TODO Do we need to take care about different namespaces here?? We should!
+                $propDom = new DOMDocument();
+                $propDom->importNode( $currentNode, true );
+                $storage->append(
+                    new ezcWebdavDeadProperty(
+                        $currentNode->namespaceURI,
+                        $currentNode->tagName,
+                        $propDom->saveXML()
+                    )
+                );
+            }
+        }
+        return $storage;
+    }
+
+    /**
+     * Extracts a live property from an DOMElement.
+     * This method is responsible for parsing WebDAV live properties. The
+     * DOMElement $domElement must be an XML element in the DAV: namepsace. If
+     * the received property is not defined in RFC 2518, null is returned.
+     * 
+     * @param DOMElement $domElement 
+     * @return ezcWebdavLiveProperty|null
+     */
+    protected function extractLiveProperty( DOMElement $domElement )
+    {
+        switch ( $domElement->tagName )
+        {
+            case 'creationdate':
+                $property = new ezcWebdavCreationDateProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->date = new DateTime( $domElement->nodeValue );
+                }
+                break;
+            case 'displayname':
+                $property = new ezcWebdavDisplayNameProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->displayName = $domElement->nodeValue;
+                }
+                break;
+            case 'getcontentlanguage':
+                $property = new ezcWebdavGetContentLanguageProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    // e.g. 'de, en'
+                    $property->displayName = array_map( 'trim', explode( ',', $domElement->nodeValue ) );
+                }
+                break;
+            case 'getcontentlength':
+                $property = new ezcWebdavGetContentLength();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->length = trim( $domElement->nodeValue );
+                }
+                break;
+            case 'getcontenttype':
+                $property = new ezcWebdavGetContentLength();
+                // @TODO: Should this throw an exception, if the match fails?
+                // Currently, the property stays empty and the backend needs to handle this
+                if ( empty( $domElement->nodeValue ) === false 
+                  && preg_match( self::GETCONTENTTYPE_REGEX, $domElement->nodeValue, $matches ) > 0 )
+                {
+                    $property->mime    = $matches['mime'];
+                    $property->charset = $matches['charset'];
+                }
+                break;
+            case 'getetag':
+                $property = new ezcWebdavGetEtagProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->etag = $domElement->nodeValue;
+                }
+                break;
+            case 'getlastmodified':
+                $property = new ezcWebdavGetLastModifiedProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->date = new DateTime( $domElement->nodeValue );
+                }
+                break;
+            case 'lockdiscovery':
+                $property = new ezcWebdavLockDiscoveryProperty();
+                if ( $domElement->hasChildNodes() === true )
+                {
+                    $property->activeLock = $this->extractActiveLockContent( $domElement );
+                }
+                break;
+            case 'resourcetype':
+                $property = new ezcWebdavResourceTypeProperty();
+                if ( empty( $domElement->nodeValue ) === false )
+                {
+                    $property->type = $domElement->nodeValue;
+                }
+                break;
+            case 'source':
+                $property = new ezcWebdavSourceProperty();
+                if ( $domElement->hasChildNodes() === true )
+                {
+                    $property->links = $this->extractLinkContent( $domElemente );
+                }
+                break;
+            case 'supportedlock':
+                $property = new ezcWebdavSupportedLockProperty();
+                if ( $domElement->hasChildNodes() === true )
+                {
+                    $property->links = $this->extractLockEntryContent( $domElemente );
+                }
+                break;
+            default:
+                // @TODO Implement extension plugins
+                // Currently just ignore
+                $property = null;
+        }
+        return $property;
+    }
+
+    /**
+     * Extracts the <activelock /> XML elements.
+     * This method extracts the <activelock /> XML elements from the
+     * <lockdiscovery /> element and returns the corresponding
+     * ezcWebdavLockDiscoveryPropertyActiveLock object to be used as the
+     * content of ezcWebdavLockDiscoveryProperty.
+     * 
+     * @param DOMElement $domElement 
+     * @return ezcWebdavLockDiscoveryPropertyActiveLock
+     */
+    protected function extractActiveLockContent( DOMElement $domElement )
+    {
+        // @TODO Implement
+        return null;
+    }
+
+    /**
+     * Extracts the <link /> XML elements.
+     * This method extracts the <link /> XML elements from the <source />
+     * element and returns the corresponding ezcWebdavSourcePropertyLink object
+     * to be used as the content of ezcWebdavSourceProperty.
+     * 
+     * @param DOMElement $domElement 
+     * @return ezcWebdavSourcePropertyLink
+     */
+    protected function extractLinkContent( DOMElement $domElement )
+    {
+        // @TODO Implement
+        return null;
+    }
+    
+    /**
+     * Extracts the <lockentry /> XML elements.
+     * This method extracts the <lockentry /> XML elements from the <supportedlock />
+     * element and returns the corresponding
+     * ezcWebdavSupportedLockPropertyLockentry object to be used as the content
+     * of ezcWebdavSupportedLockProperty.
+     * 
+     * @param DOMElement $domElement 
+     * @return ezcWebdavSupportedLockProperty
+     */
+    protected function extractLockEntryContent( DOMElement $domElement )
+    {
+        // @TODO Implement
+        return null;
     }
 
     /**
