7f0cd0a7851cd408c4646ed8910a4a51c200cc41
ts:ez.no <dev-null@apache.org>
2007-09-18 16:17:57 +0000

    - Restructured client tests to be more generic. - Prepared test structure for integration of RFC tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973503 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/tests/client_test.php
+++ Webdav/tests/client_test.php
@@ -2,16 +2,21 @@
 
 abstract class ezcWebdavClientTest extends ezcTestCase
 {
-    protected $testSets = array();
 
     protected $dataDir;
 
     protected $transportClass;
 
-    protected $currentTestSet;
+    private $testSets = array();
+    
+    private $currentTestSet;
 
     /**
-     * Needs 
+     * Needs to set different options.
+     * {@link $this->dataDir} needs to be set to the base path of
+     * Webdav/tests/clients/<clientname>
+     * {@link $this->transportClass} needs to be set to the transport class to
+     * use, e.g. {@link ezcWebdavTransportTestMock} for a RFC compliant test.
      * 
      * @return void
      */
@@ -22,9 +27,9 @@ abstract class ezcWebdavClientTest extends ezcTestCase
         parent::__construct();
         $this->setupTestEnvironment();
 
-        foreach ( glob( $this->dataDir . '/*.result.txt' ) as $resultFile )
+        foreach ( glob( $this->dataDir . '/*' ) as $testSetDir )
         {
-            $this->testSets[] = dirname( $resultFile ) . '/' . basename( $resultFile, '.result.txt' );
+            $this->testSets[] = $testSetDir;
         }
     }
 
@@ -45,62 +50,103 @@ abstract class ezcWebdavClientTest extends ezcTestCase
             throw new PHPUnit_Framework_ExpectationFailedException( "No currentTestSet set for test " . __CLASS__ );
         }
 
-        $this->runTestSet();
+        $this->runTestSet( $this->currentTestSet );
     }
 
-    protected function runTestSet()
+    protected function runTestSet( $testSetName )
     {
-        if ( file_exists( ( $resultFile = $this->currentTestSet . '.result.txt' ) ) === true 
-            && ( $referenceResult = file_get_contents( $resultFile ) ) !== ''
-           )
+        $requestObject = null;
+
+        // Request test
+        if ( file_exists( ( $requestDir = "{$testSetName}/request" ) ) === true )
         {
-            $referenceResult = unserialize( file_get_contents( $resultFile ) );
-        }
-        else
-        {
-            $referenceResult = false;
+            // Settings
+            $request = array();
+            $request['result'] = $this->getFileContent( $requestDir, 'result' );
+            $request['server'] = $this->getFileContent( $requestDir, 'server' );
+            $request['body']   = $this->getFileContent( $requestDir, 'body' );
+            $request['uri']    = $this->getFileContent( $requestDir, 'uri' );
+            
+            $requestObject = $this->runRequestTest( $request );
         }
 
+        // Response test
+        if ( file_exists( ( $responseDir = "{$this->dataDir}/response" ) ) === true && $requestObject !== null )
+        {
+            // Settings
+            $response = array();
+            $response['result'] = $this->getFileContent( $responseDir, 'result' );
+            $response['headers'] = $this->getFileContent( $responseDir, 'headers' );
+            $response['body']   = $this->getFileContent( $responseDir, 'body' );
+            
+            $responseObject = $this->runResponseTest( $response, $requestObject );
+        }
+    }
+
+    protected function getFileContent( $dir, $file )
+    {
+        // No file exists
+        if ( count( $files = glob( "{$dir}/{$file}.*" ) ) < 1 )
+        {
+            return false;
+        }
+
+        // The first file overrides
+        $fileInfo    = pathinfo( ( $filePath = $files[0] ) );
+        $fileContent = '';
+        switch( $fileInfo['extension'] )
+        {
+            case 'php':
+                $fileContent = require $filePath;
+                break;
+            case 'ser':
+                $fileContent = unserialize( file_get_contents( $filePath ) );
+                break;
+            case 'txt':
+            default:
+                $fileContent = file_get_contents( $filePath );
+                break;
+        }
+        return $fileContent;
+    }
+
+    protected function runRequestTest( array $request )
+    {
         // Optionally set a body.
-        if ( file_exists( ($bodyFile = $this->currentTestSet . '.body.xml' ) ) )
-        {
-            $GLOBALS['EZC_WEBDAV_TRANSPORT_TEST_BODY'] = file_get_contents( $bodyFile );
-        }
-        else
-        {
-            $GLOBALS['EZC_WEBDAV_TRANSPORT_TEST_BODY'] = '';
-        }
+        $GLOBALS['EZC_WEBDAV_TRANSPORT_TEST_BODY'] = ( $request['body'] !== false ? $request['body'] : '' );
 
         // Optionally overwrite $_SERVER
-        if ( file_exists( ( $serverFile = $this->currentTestSet . '.server.php' ) ) )
-        {
-            $_SERVER = require $serverFile;
-        }
-        
-        if ( file_exists( ( $uriFile = $this->currentTestSet . 'uri.txt' ) ) )
-        {
-            $uri = file_get_contents( $uriFile );
-        }
-        else
-        {
-            $uri = 'http://localhost/webdav.php';
-        }
+        $_SERVER = ( $request['server'] !== false ? $request['server'] : $_SERVER );
 
+        // Optionally set an URI different from 'http://localhost/webdav.php'
+        $uri = ( $request['uri'] !== false  ? $request['uri'] : 'http://localhost/webdav.php' );
+
+        // Setup test environment
         $transportClass = $this->transportClass;
         $transport = new $transportClass();
+        
+        // Begin request test
         $result = $transport->parseRequest( $uri );
 
-        if ( $referenceResult === false )
+        if ( $request['result'] === false )
         {
             // Regenerate
-            file_put_contents( $resultFile, serialize( $result ) );
+            file_put_contents(
+                "{$this->dataDir}/request/result.txt",
+                serialize( $result )
+            );
         }
 
         $this->assertEquals(
-            $referenceResult,
+            $request['result'],
             $result,
             "Result not parsed correctly for test set '{$this->currentTestSet}'."
         );
+    }
+
+    protected function runResponseTest( array $response, ezcWebdavRequest $requestObject )
+    {
+        // To be implemented...
     }
 
 }
