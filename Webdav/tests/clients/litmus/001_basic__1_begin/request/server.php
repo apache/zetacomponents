5c9b3999141869450df89afbb6cb951ea3c188a5
ts:ez.no <dev-null@apache.org>
2007-09-27 17:10:02 +0000

    - Updated itmus test suite after another clean run with litmus. # All 862 tests running now. :)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973616 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/tests/clients/litmus/001_basic__1_begin/request/server.php
+++ Webdav/tests/clients/litmus/001_basic__1_begin/request/server.php
@@ -21,7 +21,7 @@ return array (
   'GATEWAY_INTERFACE' => 'CGI/1.1',
   'SERVER_PORT' => '80',
   'SERVER_ADDR' => '127.0.0.1',
-  'REMOTE_PORT' => '53088',
+  'REMOTE_PORT' => '54505',
   'REMOTE_ADDR' => '127.0.0.1',
   'SCRIPT_NAME' => '/index.php',
   'PATH_INFO' => '/litmus/',
@@ -40,7 +40,7 @@ return array (
   'HTTP_TE' => 'trailers',
   'HTTP_X_LITMUS' => 'basic: 1 (begin)',
   'PHP_SELF' => '/index.php/litmus/',
-  'REQUEST_TIME' => 1190839811,
+  'REQUEST_TIME' => 1190913145,
   'argv' => 
   array (
   ),
