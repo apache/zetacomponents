ae3b0f9072c36be8b6fb2985ce048ff04c8c910d
as:ez.no <dev-null@apache.org>
2007-10-09 15:38:03 +0000

    - Added more doc blocks to modules.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973716 13f79535-47bb-0310-9956-ffa450edef68

--- Feed/src/interfaces/module.php
+++ Feed/src/interfaces/module.php
@@ -10,17 +10,102 @@
  */
 
 /**
+ * Interface for feed modules.
+ *
+ * Currently implemented for these feed modules:
+ *  - Content ({@link ezcFeedModuleContent})
+ *  - DublinCore ({@link ezcFeedModuleDublinCore})
+ *
  * @package Feed
  * @version //autogentag//
  */
 interface ezcFeedModule
 {
+    /**
+     * Returns the module name (eg. 'DublinCore').
+     *
+     * @return string
+     */
     public static function getModuleName();
+
+    /**
+     * Returns the namespace for this module (eg. 'http://purl.org/dc/elements/1.1/').
+     *
+     * @return string
+     */
     public static function getNamespace();
+
+    /**
+     * Returns the namespace prefix for this module (eg. 'dc').
+     *
+     * @return string
+     */
     public static function getNamespacePrefix();
-    public function feedMetaSetHook( &$element, &$value );
-    public function feedItemSetHook( &$element, &$value );
+
+    /**
+     * Returns true if the name $element is allowed as a channel element.
+     *
+     * @param string $element The name of the element
+     * @return bool
+     */
     public function isChannelElementAllowed( $element );
+
+    /**
+     * Returns true if the name $element is allowed as an item element.
+     *
+     * @param string $element The name of the element
+     * @return bool
+     */
     public function isItemElementAllowed( $element );
+
+    /**
+     * Return true if setting the item $element to $value is allowed, false
+     * otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedMetaSetHook( $element, $value );
+
+    /**
+     * Return true if setting the item $element to $value is allowed with the
+     * module data $moduleData, false otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param array(string=>mixed) The module data
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedMetaGenerateHook( $moduleData, $element, $value );
+
+    /**
+     * Return true if setting the item $element to $value is allowed, false
+     * otherwise.
+     *
+     * It is a hook called before setting the item $element to $value.
+     *
+     * @param string $element The name of the item element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedItemSetHook( $element, $value );
+
+    /**
+     * Return true if setting the item $element to $value is allowed with the
+     * item module data $moduleData, false otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param array(string=>mixed) The item module data
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedItemGenerateHook( $moduleData, $element, $value );
 }
 ?>
--- Feed/src/modules/dublin_core.php
+++ Feed/src/modules/dublin_core.php
@@ -10,46 +10,180 @@
  */
 
 /**
+ * Class for handling a DublinCore feed module.
+ *
  * @package Feed
  * @version //autogentag//
  */
 class ezcFeedModuleDublinCore implements ezcFeedModule
 {
+    /**
+     * Holds the feed type of this module (eg. 'rss2').
+     *
+     * @var string
+     */
     protected $feedType;
+
+    /**
+     * Holds the supported elements for this module.
+     *
+     * @var array(string)
+     */
     protected $supportedElements = array(
         'title', 'creator', 'subject', 'description', 'publisher',
         'contributor', 'date', 'type', 'format', 'identifier',
         'source', 'language', 'relation', 'coverage', 'rights'
     );
 
+    /**
+     * Creates a new DublinCore module with the feed type as $feedType.
+     *
+     * @param string $feedType The feed type of the new module
+     */
     public function __construct( $feedType )
     {
         $this->feedType = $feedType;
     }
 
+    /**
+     * Returns the module name ('DublinCore').
+     *
+     * @return string
+     */
     public static function getModuleName()
     {
         return 'DublinCore';
     }
 
+    /**
+     * Returns the namespace for this module ('http://purl.org/dc/elements/1.1/').
+     *
+     * @return string
+     */
     public static function getNamespace()
     {
         return 'http://purl.org/dc/elements/1.1/';
     }
 
+    /**
+     * Returns the namespace prefix for this module ('dc').
+     *
+     * @return string
+     */
     public static function getNamespacePrefix()
     {
         return 'dc';
     }
 
+    /**
+     * Returns true if the name $element is allowed as a channel element.
+     *
+     * @param string $element The name of the element
+     * @return bool
+     */
     public function isChannelElementAllowed( $element )
     {
         return in_array( $element, $this->supportedElements );
     }
 
+    /**
+     * Returns true if the name $element is allowed as an item element.
+     *
+     * @param string $element The name of the element
+     * @return bool
+     */
     public function isItemElementAllowed( $element )
     {
         return in_array( $element, $this->supportedElements );
+    }
+
+    /**
+     * Return true if setting the item $element to $value is allowed, false
+     * otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedMetaSetHook( $element, $value )
+    {
+        if ( in_array( $this->feedType, array( 'rss1' ) ) )
+        {
+            return true;
+        }
+        if ( $element === 'published' )
+        {
+            return false;
+        }
+        return null;
+    }
+
+    /**
+     * Return true if setting the item $element to $value is allowed with the
+     * module data $moduleData, false otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param array(string=>mixed) $moduleData The module data
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedMetaGenerateHook( $moduleData, $element, $value )
+    {
+        if ( 
+            ( isset( $moduleData['creator'] ) && $element === 'author' ) ||
+            ( isset( $moduleData['date'] ) && $element === 'published' )
+        )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return true if setting the item $element to $value is allowed, false
+     * otherwise.
+     *
+     * It is a hook called before setting the item $element to $value.
+     *
+     * @param string $element The name of the item element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedItemSetHook( $element, $value )
+    {
+        if ( in_array( $this->feedType, array( 'rss1' ) ) )
+        {
+            return true;
+        }
+        if ( $element === 'published' )
+        {
+            return false;
+        }
+        return null;
+    }
+
+    /**
+     * Return true if setting the item $element to $value is allowed with the
+     * item module data $moduleData, false otherwise.
+     *
+     * It is a hook called before setting the feed $element to $value.
+     *
+     * @param array(string=>mixed) $moduleData The item module data
+     * @param string $element The name of the feed element
+     * @param mixed $value The new value for $element
+     * @return bool
+     */
+    public function feedItemGenerateHook( $moduleData, $element, $value )
+    {
+        if ( isset( $moduleData['date'] ) && $element === 'published' )
+        {
+            return false;
+        }
+        return true;
     }
 
     public function prepareDate( $date )
@@ -74,6 +208,7 @@ class ezcFeedModuleDublinCore implements ezcFeedModule
             case 'date':
                 $feedProcessor->generateMetaData( "$prefix:$element", date( DATE_W3C, $value ) );
                 break;
+
             default:
                 $feedProcessor->generateMetaData( "$prefix:$element", $value );
                 break;
@@ -88,6 +223,7 @@ class ezcFeedModuleDublinCore implements ezcFeedModule
             case 'date':
                 $feedProcessor->generateItemData( $itemTag, "$prefix:$element", date( DATE_W3C, $value ) );
                 break;
+
             default:
                 $feedProcessor->generateItemData( $itemTag, "$prefix:$element", $value );
                 break;
@@ -103,53 +239,6 @@ class ezcFeedModuleDublinCore implements ezcFeedModule
                 break;
         }
         return $value;
-    }
-
-    public function feedMetaSetHook( &$element, &$value )
-    {
-        if ( in_array( $this->feedType, array( 'rss1' ) ) )
-        {
-            return true;
-        }
-        if ( $element === 'published' )
-        {
-            return false;
-        }
-        return null;
-    }
-
-    public function feedMetaGenerateHook( $moduleData, &$element, &$value )
-    {
-        if ( 
-            ( isset( $moduleData['creator'] ) && $element === 'author' ) ||
-            ( isset( $moduleData['date'] ) && $element === 'published' )
-        )
-        {
-            return false;
-        }
-        return true;
-    }
-
-    public function feedItemSetHook( &$element, &$value )
-    {
-        if ( in_array( $this->feedType, array( 'rss1' ) ) )
-        {
-            return true;
-        }
-        if ( $element === 'published' )
-        {
-            return false;
-        }
-        return null;
-    }
-
-    public function feedItemGenerateHook( $moduleData, &$element, &$value )
-    {
-        if ( isset( $moduleData['date'] ) && $element === 'published' )
-        {
-            return false;
-        }
-        return true;
     }
 }
 ?>
