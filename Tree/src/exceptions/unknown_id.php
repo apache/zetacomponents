926b3c77c5cd07734da2e313cc3304654d774bcd
dr:ez.no <dev-null@apache.org>
2007-10-10 16:50:18 +0000

    - Added missing files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973729 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Tree/src/exceptions/unknown_id.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * File containing the ezcTreeUnknownIdException class.
+ *
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Tree
+ */
+
+/**
+ * Exception that is thrown when a node is request through an unknown
+ * (non-existing) ID.
+ *
+ * @package Tree
+ * @version //autogentag//
+ */
+class ezcTreeUnknownIdException extends ezcTreeException
+{
+    /**
+     * Constructs a new ezcTreeUnknownIdException for the ID $nodeId.
+     *
+     * @param string $nodeId
+     */
+    public function __construct( $nodeId )
+    {
+        parent::__construct( "The node with ID '{$nodeId}' could not be found." );
+    }
+}
+?>
---
+++ TreeDatabaseTiein/tests/db_materialized_path_tree_diff_separator.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package TreeDatabaseTiein
+ * @subpackage Tests
+ */
+
+require_once 'Tree/tests/tree.php';
+require_once 'db_tree.php';
+require_once 'db_materialized_path_tree.php';
+
+/**
+ * @package TreeDatabaseTiein
+ * @subpackage Tests
+ */
+class ezcTreeDbMaterializedPathTestWithDifferentSeparator extends ezcTreeDbMaterializedPathTest
+{
+    private $tempDir;
+
+    public function insertData()
+    {
+        // insert test data
+        $data = array(
+            // child -> parent
+            1 => array( 'null', '@1' ),
+            2 => array(      1, '@1@2' ),
+            3 => array(      1, '@1@3' ),
+            4 => array(      1, '@1@4' ),
+            6 => array(      4, '@1@4@6' ),
+            7 => array(      6, '@1@4@6@7' ),
+            8 => array(      6, '@1@4@6@8' ),
+            5 => array(      1, '@1@5' ),
+            9 => array(      5, '@1@5@9' ),
+        );
+        foreach( $data as $childId => $details )
+        {
+            list( $parentId, $path ) = $details;
+            $this->dbh->exec( "INSERT INTO materialized_path(id, parent_id, path) VALUES( $childId, $parentId, '$path' )" );
+        }
+
+        // add data
+        for ( $i = 1; $i <= 8; $i++ )
+        {
+            $this->dbh->exec( "INSERT INTO data(id, data) values ( $i, 'Node $i' )" );
+        }
+    }
+
+    protected function setUpEmptyTestTree( $dataTable = 'data', $dataField = 'data' )
+    {
+        $store = new ezcTreeDbExternalTableDataStore( $this->dbh, $dataTable, 'id', $dataField );
+        $tree = ezcTreeDbMaterializedPath::create(
+            $this->dbh,
+            'materialized_path',
+            $store,
+            '@'
+        );
+        $this->emptyTables();
+        return $tree;
+    }
+
+    protected function setUpTestTree( $dataTable = 'data', $dataField = 'data' )
+    {
+        $store = new ezcTreeDbExternalTableDataStore( $this->dbh, $dataTable, 'id', $dataField );
+        $tree = new ezcTreeDbMaterializedPath(
+            $this->dbh,
+            'materialized_path',
+            $store,
+            '@'
+        );
+        return $tree;
+    }
+
+    public function testWithWrongSeparationChar()
+    {
+        $store = new ezcTreeDbExternalTableDataStore( $this->dbh, 'data', 'id', 'data' );
+        $tree = new ezcTreeDbMaterializedPath(
+            $this->dbh,
+            'materialized_path',
+            $store,
+            '$'
+        );
+
+        $nodeList = $tree->fetchNodeById( 4 )->fetchSubtree();
+        self::assertSame( 1, $nodeList->size );
+    }
+
+    public static function suite()
+    {
+         return new PHPUnit_Framework_TestSuite( "ezcTreeDbMaterializedPathTestWithDifferentSeparator" );
+    }
+}
+
+?>
