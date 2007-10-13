2f79faa99f0b3b45a1a92e2012993bee53b39571
ts:ez.no <dev-null@apache.org>
2007-10-13 13:18:04 +0000

    - Removed litmus test suite for update.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973770 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/tests/clients/litmus/004_OPTIONS/response/headers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  'DAV' => '1, 2, 1#extended',
-  'Allow' => 'GET, HEAD, PROPFIND, PROPPATCH, OPTIONS, DELETE, COPY, MOVE, MKCOL, PUT',
-);
-
-?>
--- Webdav/tests/clients/litmus/154_HEAD/response/headers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  'Content-Type' => 'application/octet-stream; charset="utf-8"',
-  'Content-Length' => 1,
-);
-
-?>
