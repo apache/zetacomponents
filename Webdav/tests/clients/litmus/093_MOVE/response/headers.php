a4c17b3a8a6e783c5b24dabf0c0e0798899dd820
ts:ez.no <dev-null@apache.org>
2007-10-17 14:44:43 +0000

    - Re-added Litmus test cases.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973816 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Webdav/tests/clients/litmus/093_MOVE/response/headers.php
@@ -0,0 +1,7 @@
+<?php
+
+return array (
+  'Server' => 'lighttpd/1.4.18/eZComponents/dev/ezcWebdavTransportMock',
+);
+
+?>
---
+++ Webdav/tests/clients/litmus/129_PROPFIND/response/headers.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'Warning' => 'eZComponents ',
+  'Server' => 'lighttpd/1.4.18/eZComponents/dev/ezcWebdavTransportMock',
+);
+
+?>
