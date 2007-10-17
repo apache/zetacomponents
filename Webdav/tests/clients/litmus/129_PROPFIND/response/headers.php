67547e45b3744a19164564e1d7ee7cc61ce010d2
ts:ez.no <dev-null@apache.org>
2007-10-17 14:36:15 +0000

    - Temporarely removed Litmus client tests. # Regenerating.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973811 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/tests/clients/litmus/129_PROPFIND/response/headers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  'Warning' => 'eZComponents ',
-  'Server' => 'lighttpd/1.4.18/eZComponents/dev/ezcWebdavTransportMock',
-);
-
-?>
--- Webdav/tests/clients/litmus/149_HEAD/response/headers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array (
-  'Server' => 'lighttpd/1.4.18/eZComponents/dev/ezcWebdavTransportMock',
-  'Content-Type' => 'application/octet-stream; charset="utf-8"',
-  'Content-Length' => 1,
-);
-
-?>
