57781279f3785dd8af6d4b6a5b25ba1b1968d05c
ts:ez.no <dev-null@apache.org>
2007-10-17 13:15:46 +0000

    - Added missing files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@973806 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Webdav/src/transports/property_handlers/nautilus.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * File containing the ezcWebdavPropertyHandler class.
+ *
+ * @package Webdav
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+/**
+ * Description missing
+ *
+ * @package Webdav
+ * @version //autogen//
+ */
+class ezcWebdavNautilusPropertyHandler extends ezcWebdavPropertyHandler
+{
+    /**
+     * Returns the XML representation of a live property.
+     * Returns a DOMElement, representing the content of the given $property.
+     * The newly created element is also appended as a child to the given
+     * $parentElement.
+     * 
+     * @param ezcWebdavLiveProperty $property 
+     * @param DOMElement $parentElement 
+     * @return DOMElement
+     */
+    protected function serializeLiveProperty( ezcWebdavLiveProperty $property, DOMElement $parentElement )
+    {
+        switch ( get_class( $property ) )
+        {
+            case 'ezcWebdavGetContentTypeProperty':
+                $elementName  = 'getcontenttype';
+                $elementValue = ( $property->mime !== null ? $property->mime : null );
+                break;
+            default:
+                return parent::serializeLiveProperty( $property, $parentElement );
+        }
+
+        $propertyElement = $parentElement->appendChild( 
+            $this->xml->createDomElement( $parentElement->ownerDocument, $elementName, $property->namespace )
+        );
+
+        if ( $elementValue instanceof DOMDocument )
+        {
+            $propertyElement->appendChild(
+                $dom->importNode( $elementValue->documentElement, true )
+            );
+        }
+        else if ( is_array( $elementValue ) )
+        {
+            foreach( $elementValue as $subValue )
+            {
+                $propertyElement->appendChild( $subValue );
+            }
+        }
+        else if ( is_scalar( $elementValue ) )
+        {
+            $propertyElement->nodeValue = $elementValue;
+        }
+
+        return $propertyElement;
+    }
+}
+
+?>
