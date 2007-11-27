af465b55e4bb9b30092dd13fa3c14e57b240b569
sb:ez.no <dev-null@apache.org>
2007-11-27 16:56:16 +0000

    - Add tutorial for #12083.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@974155 13f79535-47bb-0310-9956-ffa450edef68

---
+++ WorkflowEventLogTiein/docs/example.php
@@ -0,0 +1,33 @@
+<?php
+// Set up the logfile writer.
+$writer = new ezcLogUnixFileWriter(
+  dirname( __FILE__ ) . '/data', 'actual.log' 
+);
+
+$log = ezcLog::getInstance();
+$mapper = $log->getMapper();
+$filter = new ezcLogFilter;
+$rule = new ezcLogFilterRule( $filter, $writer, true );
+$mapper->appendRule( $rule ); 
+
+// Set up database connection.
+$db = ezcDbFactory::create( 'mysql://test@localhost/test' );
+
+// Set up workflow definition storage (database).
+$definition = new ezcWorkflowDatabaseDefinitionStorage( $db );
+
+// Load latest version of workflow named "Test".
+$workflow = $definition->loadByName( 'Test' );
+
+// Set up database-based workflow executer.
+$execution = new ezcWorkflowDatabaseExecution( $db );
+
+// Pass workflow object to workflow executer.
+$execution->workflow = $workflow;
+
+// Attach logfile writer as a listener.
+$execution->addListener( new ezcWorkflowEventLogListener( $log ) );
+
+// Start workflow execution.
+$id = $execution->start();
+?>
