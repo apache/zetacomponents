acdf8d732caae3917c00f3fc80df574357deefbb
dr:ez.no <dev-null@apache.org>
2008-01-11 09:43:50 +0000

    - Added an optional argument to the ezcBaseValueException constructor to allow   the exception to be used for non-property/setting type violations as well.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@974434 13f79535-47bb-0310-9956-ffa450edef68

--- Base/src/exceptions/value.php
+++ Base/src/exceptions/value.php
@@ -23,15 +23,16 @@ class ezcBaseValueException extends ezcBaseException
      *                wrong with.
      * @param mixed   $value The value that the option was tried to be set too.
      * @param string  $expectedValue A string explaining the allowed type and value range.
+     * @param string  $variableType  What type of variable was tried to be set (setting, argument).
      */
-    function __construct( $settingName, $value, $expectedValue = null )
+    function __construct( $settingName, $value, $expectedValue = null, $variableType = 'setting' )
     {
         $type = gettype( $value );
         if ( in_array( $type, array( 'array', 'object', 'resource' ) ) )
         {
             $value = serialize( $value );
         }
-        $msg = "The value '{$value}' that you were trying to assign to setting '{$settingName}' is invalid.";
+        $msg = "The value '{$value}' that you were trying to assign to $variableType '{$settingName}' is invalid.";
         if ( $expectedValue )
         {
             $msg .= " Allowed values are: " . $expectedValue . '.';
