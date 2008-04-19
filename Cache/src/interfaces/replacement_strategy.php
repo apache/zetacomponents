8c63971d3122944529625dad5470aad29bbfc688
ts:ez.no <dev-null@apache.org>
2008-04-19 11:51:37 +0000

    - Finished documentation.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975021 13f79535-47bb-0310-9956-ffa450edef68

--- Cache/src/interfaces/replacement_strategy.php
+++ Cache/src/interfaces/replacement_strategy.php
@@ -20,6 +20,10 @@
  * storing/restoring/deleting of cache items in the given storage. In addition
  * it must take care about keeping the needed meta data actual and about
  * purging data from the cache storage, if it runs full.
+ *
+ * The {@link ezcCacheStorageMetaData} is used to store information needed by
+ * the replacement process, as well as information about the state of the
+ * storages in the stack and if an item is available in it at all.
  * 
  * @package Cache
  * @version //autogentag//
@@ -44,9 +48,22 @@ interface ezcCacheStackReplacementStrategy
      * the meta data successfully as soon as they are removed from all
      * storages.
      *
+     * The $itemLimit and $freeRate parameters need to be obey the $itemLimit.
+     * If it is reached and the store() method is called, the $freeRate ammount
+     * of $itemLimit needs to be cleaned up in the $storage. To achieve this,
+     * first the {@link ezcCacheStackableStorage->purge()} method should be
+     * used, to remove all outdated content. The method returns an array of
+     * cache item ids, to enable the replacement strategy to remove its data
+     * accordingly. If this does not reach the desired $freeRate in the cache,
+     * items need to be deleted according to the replacement strategy itself,
+     * in addition. Therefore the {@link ezcCacheStackableStorage->delete()}
+     * method also returns an array of item IDs for updating the meta data.
+     *
      * @param ezcCacheStackableStorage $storage
      * @param string $storageId
      * @param ezcCacheStorageMetaData $metaData
+     * @param int $itemLimit
+     * @param float $freeRate Ranging from 0-1, indicating a fraction..
      * @param string $itemId
      * @param mixed $itemData
      * @param array(string=>string) $itemAttributes
@@ -55,20 +72,76 @@ interface ezcCacheStackReplacementStrategy
         ezcCacheStackableStorage $storage,
         $storageId,
         ezcCacheStorageMetaData $metaData,
+        $itemLimit,
+        $freeRate,
         $itemId,
         $itemData,
         $itemAttributes = array()
     );
 
+    /**
+     * Restores the data with the given $dataId from the given $storage.
+     *
+     * This method needs to take care about restoring an item from the given
+     * $storage. The items $itemId and potential $itemAttributes are given for
+     * that purpose. The $storage, which implements {@link
+     * ezcCacheStackableStorage}, must be used to retreive the data from. If no
+     * data is found, boolean false must be returned.
+     *
+     * In addition to these parameters, the method receives the $metaData
+     * struct that should be used to store the meta information needed by the
+     * replacement strategy. To enable this, the ID of the storage in the stack
+     * ($storageId) is given in addition. The meta data should be updated, if
+     * an item is successfully restored, as necessary.
+     *
+     * The $search parameter is to be forwarded to the
+     * {@ezcCacheStackableStorage->restore()} method.
+     *
+     * @param ezcCacheStackableStorage $storage
+     * @param string $storageId
+     * @param ezcCacheStorageMetaData $metaData
+     * @param string $itemId
+     * @param mixed $itemData
+     * @param array(string=>string) $itemAttributes
+     *
+     * @return mixed Restored data or false.
+     */
     public static function restore(
         ezcCacheStackableStorage $storage,
         $storageId,
         ezcCacheStorageMetaData $metaData,
         $itemId,
-        $attributes = array(),
+        $itemAttributes = array(),
         $search = false
     );
 
+    /**
+     * Deletes the data with the given $itemId from the given $storage.
+     *
+     * This method needs to take care about deleting an item from the given
+     * $storage. The items $itemId and potential $itemAttributes are given for
+     * that purpose. The $storage, which implements {@link
+     * ezcCacheStackableStorage}, must be used to delete data from. The deleted
+     * items IDs should be returned in an array.
+     *
+     * In addition to these parameters, the method receives the $metaData
+     * struct that should be used to store the meta information needed by the
+     * replacement strategy. To enable this, the ID of the storage in the stack
+     * ($storageId) is given in addition. The meta data should be updated, if
+     * an item is successfully restored, as necessary.
+     *
+     * The $search parameter is to be forwarded to the
+     * {@ezcCacheStackableStorage->delete()} method.
+     *
+     * @param ezcCacheStackableStorage $storage
+     * @param string $storageId
+     * @param ezcCacheStorageMetaData $metaData
+     * @param string $itemId
+     * @param mixed $itemData
+     * @param array(string=>string) $itemAttributes
+     *
+     * @return array(string) Deleted item IDs.
+     */
     public static function delete(
         ezcCacheStackableStorage $storage,
         $storageId,
