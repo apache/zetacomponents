3a7b076aa754ae94edca6598f12cfecd27830984
dr:ez.no <dev-null@apache.org>
2008-04-23 11:32:48 +0000

    - Clarify example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975074 13f79535-47bb-0310-9956-ffa450edef68

--- Configuration/docs/tutorial_example_02.php
+++ Configuration/docs/tutorial_example_02.php
@@ -4,6 +4,8 @@ require_once 'tutorial_autoload.php';
 $reader = new ezcConfigurationIniReader();
 $reader->init( dirname( __FILE__ ), 'settings' );
 
+// validate the settings file, and loop over all the validation errors and
+// warnings
 $result = $reader->validate();
 foreach ( $result->getResultList() as $resultItem )
 {
@@ -11,5 +13,6 @@ foreach ( $result->getResultList() as $resultItem )
     print " " . $resultItem->details . "\n";
 }
 
+// load the settings into an ezcConfiguration object
 $cfg = $reader->load();
 ?>
