b8f193cad4889de994031d098a371bb87e83fd77
as:ez.no <dev-null@apache.org>
2008-05-05 12:38:53 +0000

    - Fixed package tag.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975212 13f79535-47bb-0310-9956-ffa450edef68

--- Url/src/exceptions/url_invalid_parameter_exception.php
+++ Url/src/exceptions/url_invalid_parameter_exception.php
@@ -2,7 +2,7 @@
 /**
  * File containing the ezcUrlInvalidParameterException class
  *
- * @package Mail
+ * @package Url
  * @version //autogen//
  * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
