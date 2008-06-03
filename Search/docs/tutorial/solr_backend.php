d29acbcc4fc38a47fe41c9320c9f48803ff85c0f
dr:ez.no <dev-null@apache.org>
2008-06-03 11:48:28 +0000

    - s/backend/handler
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975563 13f79535-47bb-0310-9956-ffa450edef68

--- Search/docs/tutorial/solr_backend.php
+++ Search/docs/tutorial/solr_backend.php
@@ -2,8 +2,8 @@
 require_once 'tutorial_autoload.php';
 
 // on localhost with the default port
-$backend = new ezcSearchSolrHandler;
+$handler = new ezcSearchSolrHandler;
 
 // on another host with a different port
-$backend = new ezcSearchSolrHandler( '10.0.2.184', 9123 );
+$handler = new ezcSearchSolrHandler( '10.0.2.184', 9123 );
 ?>
