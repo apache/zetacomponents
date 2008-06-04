289123e37e67c4285ab422bbd6764e80eb1d7b44
kn:ez.no <dev-null@apache.org>
2008-06-04 20:50:00 +0000

    - Added example for custom directive to tutorial - Added example for cutom XHtml visitor to tutorial
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975588 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Document/docs/tutorial/01_custom_directive.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'tutorial_autoload.php';
+
+// Load custom directive
+require 'address_directive.php';
+
+$document = new ezcDocumentRst();
+$document->registerDirective( 'address', 'myAddressDirective' );
+$document->loadString( <<<EORST
+Address example
+===============
+
+.. address:: John Doe
+    :street: Some Lane 42
+EORST
+);
+
+$docbook = $document->getAsDocbook();
+echo $docbook->save();
+
