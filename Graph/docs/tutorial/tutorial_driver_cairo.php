d546b6f31ed273f194d3c13729a908e332a9d85b
kn:ez.no <dev-null@apache.org>
2008-06-16 10:23:47 +0000

    - Added cairo example to the tutorial - Mention glyph support in the tutorial
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975723 13f79535-47bb-0310-9956-ffa450edef68

--- Graph/docs/tutorial/tutorial_driver_cairo.php
+++ Graph/docs/tutorial/tutorial_driver_cairo.php
@@ -3,8 +3,12 @@
 require_once 'tutorial_autoload.php';
 
 $graph = new ezcGraphPieChart();
+$graph->palette = new ezcGraphPaletteEzBlue();
 $graph->title = 'Access statistics';
-$graph->options->label = '%2$d (%3$.1f%%)';
+$graph->legend = false;
+
+// Set the cairo driver
+$graph->driver = new ezcGraphCairoDriver();
 
 $graph->data['Access statistics'] = new ezcGraphArrayDataSet( array(
     'Mozilla' => 19113,
@@ -13,24 +17,23 @@ $graph->data['Access statistics'] = new ezcGraphArrayDataSet( array(
     'Safari' => 652,
     'Konqueror' => 474,
 ) );
-$graph->data['Access statistics']->highlight['Explorer'] = true;
-$graph->data['Access statistics']->symbol = ezcGraph::BULLET;
 
 $graph->renderer = new ezcGraphRenderer3d();
 
+$graph->renderer->options->pieChartGleam = .3;
+$graph->renderer->options->pieChartGleamColor = '#FFFFFF';
 $graph->renderer->options->pieChartShadowSize = 5;
-$graph->renderer->options->pieChartShadowColor = ezcGraphColor::create( '#000000' );
-$graph->renderer->options->pieChartGleam = .5;
-$graph->renderer->options->dataBorder = false;
-$graph->renderer->options->pieChartHeight = 8;
-$graph->renderer->options->pieChartSymbolColor = '#888A8588';
-$graph->renderer->options->pieChartRotation = .8;
+$graph->renderer->options->pieChartShadowColor = '#000000';
+
 $graph->renderer->options->legendSymbolGleam = .5;
 $graph->renderer->options->legendSymbolGleamSize = .9;
 $graph->renderer->options->legendSymbolGleamColor = '#FFFFFF';
 
-$graph->driver = new ezcGraphCairoDriver();
+$graph->renderer->options->pieChartSymbolColor = '#55575388';
 
-$graph->render( 400, 150, 'tutorial_driver_cairo.png' );
+$graph->renderer->options->pieChartHeight = 5;
+$graph->renderer->options->pieChartRotation = .8;
+
+$graph->render( 400, 300, 'tutorial_driver_cairo.png' );
 
 ?>
