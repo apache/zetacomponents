0887b41fffb3236eb8c6c57d85965456eac57fc4
ts:ez.no <dev-null@apache.org>
2008-07-09 09:19:39 +0000

    - Docs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@975872 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/src/transports/konqueror.php
+++ Webdav/src/transports/konqueror.php
@@ -10,11 +10,32 @@
 /**
  * Transport layer for the Konqueror web browser (KDE).
  *
+ * This transport class adjust the behavior of the Webdav component to work
+ * with the KDE browser Konqueror.
+ *
+ * Tested with:
+ *
+ * - Konqueror 3.5.7
+ * - Konqueror 3.5.9 (does not perform PUT requests, bug in client)
+ *
  * @version //autogentag//
  * @package Webdav
  */
 class ezcWebdavKonquerorCompatibleTransport extends ezcWebdavTransport
 {
+    /**
+     * Decodes the URLs in href attributes of PROPFIND responses.
+     *
+     * Konqueror does not use the <displayname> property (which is also URL
+     * encoded), but the <href> tag of the response to determine the displayed
+     * resource names. It expects the content to be un-encoded.
+     *
+     * This method calls the parent method and replaces the content of all
+     * <href> elements in the DOM tree.
+     * 
+     * @param ezcWebdavPropFindResponse $response 
+     * @return ezcWebdavXmlDisplayInformation
+     */
     protected function processPropFindResponse( ezcWebdavPropFindResponse $response )
     {
         $xmlDisplayInfo = parent::processPropFindResponse( $response );
