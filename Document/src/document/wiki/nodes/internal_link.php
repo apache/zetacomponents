4e20e0e38870bc9b3a706663bd14867861fe75ad
kn:ez.no <dev-null@apache.org>
2008-09-29 13:13:16 +0000

    - Added two missing classes and another test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976315 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Document/src/document/wiki/nodes/internal_link.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * File containing the ezcDocumentWikiInternalLinkNode struct
+ *
+ * @package Document
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * Struct for Wiki document inline internal link syntax tree nodes
+ * 
+ * @package Document
+ * @version //autogen//
+ */
+class ezcDocumentWikiInternalLinkNode extends ezcDocumentWikiInlineNode
+{
+    /**
+     * Set state after var_export
+     * 
+     * @param array $properties 
+     * @return void
+     * @ignore
+     */
+    public static function __set_state( $properties )
+    {
+        $nodeClass = __CLASS__;
+        $node = new $nodeClass( $properties['token'] );
+        $node->nodes = $properties['nodes'];
+        return $node;
+    }
+}
+
+?>
