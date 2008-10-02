e398884f27109e66421cf743ba5b4168007b796a
dr:ez.no <dev-null@apache.org>
2008-10-02 14:09:14 +0000

    - Fixed issue #13658: tutorial_autoload.php fails under Windows.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976333 13f79535-47bb-0310-9956-ffa450edef68

--- Graph/docs/tutorial/tutorial_autoload.php
+++ Graph/docs/tutorial/tutorial_autoload.php
@@ -1,11 +1,11 @@
 <?php
-$dir = dirname( dirname( __FILE__ ) ); 
-$dirParts = explode( '/', $dir );
+$dir = dirname( __FILE__ );
+$dirParts = explode( DIRECTORY_SEPARATOR, $dir );
 switch ( $dirParts[count( $dirParts ) - 3] )
 {
     case 'doc': require_once 'ezc/Base/base.php'; break; // pear
-    case 'trunk': require_once "$dir/../../Base/src/base.php"; break; // svn
-    default: require_once "$dir/../../Base/src/base.php"; break; // bundle
+    case 'trunk': require_once "$dir/../../../Base/src/base.php"; break; // svn
+    default: require_once "$dir/../../../Base/src/base.php"; break; // bundle
 }
 
 /**
