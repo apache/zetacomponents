840d74235eebd320a48c88d7ebaac4f8bac463b4
ts:ez.no <dev-null@apache.org>
2008-10-12 20:26:14 +0000

    - Added response class for the WebDAV LOCK method. # These classes will later be moved to the lock plugin directory.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976406 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Webdav/tests/response_lock_test.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Basic test cases for the error response class.
+ *
+ * @package Webdav
+ * @subpackage Tests
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * Reqiuire base test
+ */
+require_once 'test_case.php';
+
+/**
+ * Tests for ezcWebdavLockResponse class.
+ * 
+ * @package Webdav
+ * @subpackage Tests
+ */
+class ezcWebdavLockResponseTest extends ezcWebdavTestCase
+{
+	public static function suite()
+	{
+		return new PHPUnit_Framework_TestSuite( 'ezcWebdavLockResponseTest' );
+	}
+
+    public function testGetUnknownProperty()
+    {
+        $lockDiscovery = new ezcWebdavLockDiscoveryProperty();
+        $response      = new ezcWebdavLockResponse( $lockDiscovery );
+
+        try
+        {
+            // Read access
+            $response->unknownProperty;
+        }
+        catch ( ezcBasePropertyNotFoundException $e )
+        {
+            return true;
+        }
+
+        $this->fail( 'Expected ezcBasePropertyNotFoundException.' );
+    }
+
+    public function testSetUnknownProperty()
+    {
+        $lockDiscovery = new ezcWebdavLockDiscoveryProperty();
+        $response      = new ezcWebdavLockResponse( $lockDiscovery );
+
+        try
+        {
+            $response->unknownProperty = 42;
+        }
+        catch ( ezcBasePropertyNotFoundException $e )
+        {
+            return true;
+        }
+
+        $this->fail( 'Expected ezcBasePropertyNotFoundException.' );
+    }
+
+    public function testLockResponsePropertyLockDiscovery()
+    {
+        $lockDiscovery = new ezcWebdavLockDiscoveryProperty();
+        $response      = new ezcWebdavLockResponse( $lockDiscovery );
+
+        $this->assertSame(
+            $lockDiscovery,
+            $response->lockDiscovery,
+            'Wrong default value for property type in class ezcWebdavLockResponse.'
+        );
+
+        try
+        {
+            $response->lockDiscovery = 34650;
+        }
+        catch ( ezcBaseValueException $e )
+        {
+            return true;
+        }
+
+        $this->fail( 'Expected ezcBaseValueException.' );
+    }
+
+    public function testLockResponseConstructor()
+    {
+        $lockDiscovery = new ezcWebdavLockDiscoveryProperty();
+        $response      = new ezcWebdavLockResponse( $lockDiscovery, 'some lock token' );
+
+        $response->setHeader( 'Server', 'Some server' );
+        $response->validateHeaders();
+
+        $this->assertEquals(
+            'some lock token',
+            $response->getHeader( 'Lock-Token' )
+        );
+    }
+
+    public function testLockResponseToString()
+    {
+        $lockDiscovery = new ezcWebdavLockDiscoveryProperty();
+        $response      = new ezcWebdavLockResponse( $lockDiscovery, 'some lock token' );
+
+        $this->assertSame(
+            'HTTP/1.1 200 OK',
+            (string) $response
+        );
+    }
+}
+?>
