29bdb649a58895cb3ebc3997da471e782776ffe7
ts:ez.no <dev-null@apache.org>
2008-11-02 14:34:00 +0000

    - Added missed file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976515 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Webdav/src/plugins/lock/structs/lock_check_info.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * File containing the ezcWebdavLockCheckInfo struct class.
+ *
+ * @package Webdav
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+/**
+ * Struct containing information on lock checking for a request.
+ *
+ * An array of such structs is given to {@link
+ * ezcWebdavLockPlugin::checkViolations()}. It contains all information
+ * necessary to check violations on locks.
+ * 
+ * @package Webdav
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+class ezcWebdavLockCheckInfo extends ezcBaseStruct
+{
+    /**
+     * Path to check
+     * 
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Depth to check in the $path. 
+     * 
+     * @var ezcWebdavRequest::DEPTH_*
+     */
+    public $depth;
+
+    /**
+     * If header item fitting to that path 
+     * 
+     * @var ezcWebdavLockIfHeaderTaggedList|ezcWebdavLockIfHeaderNoTagList
+     */
+    public $ifHeader;
+
+    /**
+     * Authorization header content. 
+     * 
+     * @var ezcWebdavAuthBasic|ezcWebdavAuthDigest|null
+     */
+    public $authHeader;
+
+    /**
+     * Access type for auth checks. 
+     * 
+     * @var ezcWebdavAuthorizer::ACCESS
+     */
+    public $access;
+
+    /**
+     * Request generator to notify for this $path. 
+     * 
+     * @var ezcWebdavLockRequestGenerator
+     */
+    public $requestGenerator;
+
+    /**
+     * Creates a new lock info struct.
+     *
+     * @param string $path
+     * @param int $depth
+     * @param ezcWebdavAuthBasic|ezcWebdavAuthDigest|null $ifHeader
+     * @param ezcWebdavAuthBasic|ezcWebdavAuthDigest|null $authHeader
+     * @param int $access
+     * @param ezcWebdavLockRequestGenerator $requestGenerator
+     */
+    public function __construct(
+        $path                                           = '',
+        $depth                                          = ezcWebdavRequest::DEPTH_INFINITY,
+        $ifHeader                                       = null,
+        $authHeader                                     = null,
+        $access                                         = ezcWebdavAuthorizer::ACCESS_WRITE,
+        ezcWebdavLockRequestGenerator $requestGenerator = null
+    )
+    {
+        $this->path             = $path;
+        $this->depth            = $depth;
+        $this->ifHeader         = $ifHeader;
+        $this->authHeader       = $authHeader;
+        $this->access           = $access;
+        $this->requestGenerator = $requestGenerator;
+    }
+}
+
+?>
