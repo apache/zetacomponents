22bb0d31e57e6104228a5e4e01c19c0e4fc8a093
ts:ez.no <dev-null@apache.org>
2008-11-02 21:19:11 +0000

    - Docs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976525 13f79535-47bb-0310-9956-ffa450edef68

--- Webdav/src/plugins/lock/interfaces/check_observer.php
+++ Webdav/src/plugins/lock/interfaces/check_observer.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * File containing the ezcWebdavLockCheckObserver interface.
+ *
+ * @package Webdav
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
 /**
  * Interface that needs to be implemented by observers to lock checks.
  *
