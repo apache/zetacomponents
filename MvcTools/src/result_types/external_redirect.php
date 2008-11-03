f4a2194bec7ec32f124ca6d3cf70cd86c3cf9eb0
dr:ez.no <dev-null@apache.org>
2008-11-03 12:06:05 +0000

    - Added missing documentation to those two classes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976537 13f79535-47bb-0310-9956-ffa450edef68

--- MvcTools/src/result_types/external_redirect.php
+++ MvcTools/src/result_types/external_redirect.php
@@ -1,6 +1,33 @@
 <?php
-class ezcMvcExternalRedirect //implements ezcMvcStatus
+/**
+ * File containing the ezcMvcExternalRedirect class.
+ *
+ * @package MvcTools
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * This result type is used to force an external redirect
+ *
+ * @package MvcTools
+ * @version //autogentag//
+ */
+class ezcMvcExternalRedirect extends ezcMvcResult
 {
+    /**
+     * The location where to re-direct to.
+     *
+     * @var string
+     */
+    public $realm;
+
+    /**
+     * Constructs an ezcMvcExternalRedirect object to re-direct to $location
+     *
+     * @param string $location
+     */
 	public function __construct( $location )
 	{
 		$this->location = $location;
--- MvcTools/src/result_types/unauthorized.php
+++ MvcTools/src/result_types/unauthorized.php
@@ -1,8 +1,33 @@
 <?php
+/**
+ * File containing the ezcMvcResultUnauthorized class.
+ *
+ * @package MvcTools
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * This result type is used to signal a HTTP basic auth header
+ *
+ * @package MvcTools
+ * @version //autogentag//
+ */
 class ezcMvcResultUnauthorized extends ezcMvcResult
 {
+    /**
+     * The realm is the unique ID to identify a login area
+     *
+     * @var string
+     */
     public $realm;
 
+    /**
+     * Constructs an ezcMvcResultUnauthorized object for $realm
+     *
+     * @param string $realm
+     */
     public function __construct( $realm )
     {
         $this->realm = $realm;
