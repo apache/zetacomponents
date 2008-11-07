a5b8db737fcbcb7b66ae3d3d99be8da45f927430
kn:ez.no <dev-null@apache.org>
2008-11-07 10:27:16 +0000

    - Documented #13877: PDO by default lowercases column names.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976603 13f79535-47bb-0310-9956-ffa450edef68

--- GraphDatabaseTiein/src/dataset.php
+++ GraphDatabaseTiein/src/dataset.php
@@ -33,6 +33,13 @@ class ezcGraphDatabaseDataSet extends ezcGraphDataSet
      *      ezcGraph::VALUE => 'row name',
      *  );
      *
+     * PDO by default lowercases all column names, see PDO::setAttribute() for
+     * details. If the column names you pass to the dataset definition array
+     * are not lowercase, you either need to change the PDO::ATTR_CASE
+     * attribute of your PDO connection instance, or lowercase the names passed
+     * to the definition array. Otherwise this will throw
+     * ezcGraphDatabaseMissingColumnException exceptions.
+     *
      * @param PDOStatement $statement
      * @param array $definition
      * @return ezcGraphDatabase
@@ -68,60 +75,88 @@ class ezcGraphDatabaseDataSet extends ezcGraphDataSet
      */
     protected function createFromPdo( PDOStatement $statement, array $definition = null ) 
     {
-        $count = 0;
-
         if ( $definition === null )
         {
-            while ( $row = $statement->fetch( PDO::FETCH_NUM ) )
-            {
-                ++$count;
-
-                switch ( count( $row ) )
-                {
-                    case 1:
-                        $this->data[] = $row[0];
-                        break;
-                    case 2:
-                        $this->data[$row[0]] = $row[1];
-                        break;
-                    default:
-                        throw new ezcGraphDatabaseTooManyColumnsException( $row );
-                }
-            }
+            $this->fetchNumeric( $statement );
         }
         else
         {
-            while ( $row = $statement->fetch( PDO::FETCH_NAMED ) )
-            {
-                ++$count;
-
-                if ( !array_key_exists( $definition[ezcGraph::VALUE], $row ) )
-                {
-                    throw new ezcGraphDatabaseMissingColumnException( $definition[ezcGraph::VALUE] );
-                }
-
-                $value = $row[$definition[ezcGraph::VALUE]];
-
-                if ( array_key_exists( ezcGraph::KEY, $definition ) )
-                {
-                    if ( !array_key_exists( $definition[ezcGraph::KEY], $row ) )
-                    {
-                        throw new ezcGraphDatabaseMissingColumnException( $definition[ezcGraph::KEY] );
-                    }
-
-                    $this->data[$row[$definition[ezcGraph::KEY]]] = $value;
-                }
-                else
-                {
-                    $this->data[] = $value;
-                }
-            }
+            $this->fetchByDefinition( $statement, $definition );
         }
 
         // Empty result set
-        if ( $count <= 0 )
+        if ( count( $this->data ) <= 0 )
         {
             throw new ezcGraphDatabaseStatementNotExecutedException( $statement );
+        }
+    }
+
+    /**
+     * Fetch numeric
+     *
+     * If there is only one column returned by the query use the column as
+     * values for the dataset. If there are two columns returned use the first
+     * as key and the second as values for teh dataset.
+     *
+     * If there are more or less columns returned, a
+     * ezcGraphDatabaseTooManyColumnsException is thrown.
+     * 
+     * @param PDOStatement $statement 
+     * @return void
+     */
+    protected function fetchNumeric( PDOStatement $statement )
+    {
+        while ( $row = $statement->fetch( PDO::FETCH_NUM ) )
+        {
+            switch ( count( $row ) )
+            {
+                case 1:
+                    $this->data[] = $row[0];
+                    break;
+                case 2:
+                    $this->data[$row[0]] = $row[1];
+                    break;
+                default:
+                    throw new ezcGraphDatabaseTooManyColumnsException( $row );
+            }
+        }
+    }
+
+    /**
+     * Fecth data by provided definition
+     *
+     * Use the provided definition with column names to fetch specific columns
+     * as key and values. Will throw a ezcGraphDatabaseMissingColumnException,
+     * if the column does not exist in the query result.
+     *
+     * @param PDOStatement $statement 
+     * @param array $definition 
+     * @return void
+     */
+    protected function fetchByDefinition( PDOStatement $statement, array $definition )
+    {
+        while ( $row = $statement->fetch( PDO::FETCH_NAMED ) )
+        {
+            if ( !array_key_exists( $definition[ezcGraph::VALUE], $row ) )
+            {
+                throw new ezcGraphDatabaseMissingColumnException( $definition[ezcGraph::VALUE] );
+            }
+
+            $value = $row[$definition[ezcGraph::VALUE]];
+
+            if ( array_key_exists( ezcGraph::KEY, $definition ) )
+            {
+                if ( !array_key_exists( $definition[ezcGraph::KEY], $row ) )
+                {
+                    throw new ezcGraphDatabaseMissingColumnException( $definition[ezcGraph::KEY] );
+                }
+
+                $this->data[$row[$definition[ezcGraph::KEY]]] = $value;
+            }
+            else
+            {
+                $this->data[] = $value;
+            }
         }
     }
 
