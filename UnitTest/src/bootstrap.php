cccd8d2775d4cf891d3c111da1f03e68832adced
sb:ez.no <dev-null@apache.org>
2008-11-10 16:00:52 +0000

    - Factor out autoload/bootstrap code into separate file   (that can be used with PHPUnit's --bootstrap feature).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976692 13f79535-47bb-0310-9956-ffa450edef68

---
+++ UnitTest/src/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * File contaning the bootstrap script for the eZ Components test runner.
+ *
+ * @package UnitTest
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+require_once 'PHPUnit/Util/Filter.php';
+
+PHPUnit_Util_Filter::addFileToFilter( __FILE__, 'PHPUNIT' );
+
+// All errors must be reported
+$currentErrorLevel = error_reporting();
+if ( $currentErrorLevel != ( E_ALL | E_STRICT ) )
+{
+    echo "Your error reporting setting is not E_ALL | E_STRICT, please change\nthis in your php.ini.\n";
+    die();
+}
+
+ini_set( 'include_path', getcwd(). PATH_SEPARATOR . dirname( __FILE__ ) . '/../..' . PATH_SEPARATOR . ini_get( 'include_path' ) );
+
+require_once 'Base/src/base.php';
+
+function __autoload( $className )
+{
+    if ( strpos( $className, '_' ) !== false )
+    {
+        $file = str_replace( '_', '/', $className ) . '.php';
+        $val = require_once( $file );
+        if ( $val == 0 )
+            return true;
+        return false;
+    }
+    ezcBase::autoload( $className );
+}
+?>
--- UnitTest/src/runtests.php
+++ UnitTest/src/runtests.php
@@ -9,32 +9,9 @@
  */
 require_once 'PHPUnit/Util/Filter.php';
 
-PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
+PHPUnit_Util_Filter::addFileToFilter( __FILE__, 'PHPUNIT' );
 
-// All errors must be reported
-$currentErrorLevel = error_reporting();
-if ( $currentErrorLevel != ( E_ALL | E_STRICT ) )
-{
-    echo "Your error reporting setting is not E_ALL | E_STRICT, please change\nthis in your php.ini.\n";
-    die();
-}
-
-ini_set( 'include_path', getcwd(). PATH_SEPARATOR . dirname( __FILE__ ) . '/../..' . PATH_SEPARATOR . ini_get( 'include_path' ) );
-
-require_once("Base/src/base.php");
-
-function __autoload( $className )
-{
-    if ( strpos( $className, "_" ) !== false )
-    {
-        $file = str_replace( "_", "/", $className ) . ".php";
-        $val = require_once( $file );
-        if ( $val == 0 )
-            return true;
-        return false;
-    }
-    ezcBase::autoload( $className );
-}
+require_once 'bootstrap.php';
 
 ezcTestRunner::main();
 ?>
