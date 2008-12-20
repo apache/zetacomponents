3d944cb096cb5530e3e4511bf34031b133f2e90e
kn:ez.no <dev-null@apache.org>
2008-12-20 14:20:20 +0000

    - Fixed issue #14185: Invalid docbook markup created for table cells in wiki   visitor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@976965 13f79535-47bb-0310-9956-ffa450edef68

--- Document/src/document/wiki/visitor/docbook.php
+++ Document/src/document/wiki/visitor/docbook.php
@@ -64,7 +64,7 @@ class ezcDocumentWikiDocbookVisitor extends ezcDocumentWikiVisitor
         'ezcDocumentWikiPageBreakNode'          => 'beginpage',
 
         'ezcDocumentWikiTableNode'              => 'table',
-        'ezcDocumentWikiTableCellNode'          => 'cell',
+        'ezcDocumentWikiTableCellNode'          => 'entry',
     );
 
     /**
