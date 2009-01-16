c2b20b8002fdfb93fa77714b1ca44f80a117e9e2
ts:ez.no <dev-null@apache.org>
2009-01-16 10:27:25 +0000

    - Added exception to be thrown for existing identities.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977064 13f79535-47bb-0310-9956-ffa450edef68

---
+++ PersistentObject/src/exceptions/identity_already_exists.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * File containing the ezcPersistentIdentityAlreadyExistsException class.
+ *
+ * @package PersistentObject
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2009 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+/**
+ * Exception thrown if the same identity is added twice to the identity map.
+ *
+ * {@link ezcPersistentIdentityMap::addIdentity()} will throw this exception,
+ * if the same identity is added twice.
+ *
+ * @package PersistentObject
+ * @version //autogen//
+ */
+class ezcPersistentIdentityAlreadyExistsException extends ezcPersistentObjectException
+{
+
+    /**
+     * Creates a new ezcPersistentIdentityAlreadyExistsException.
+     *
+     * Creates a new ezcPersistentIdentityAlreadyExistsException for the object
+     * of $class with ID $id.
+     *
+     * @param string $class
+     * @param mixed $id
+     */
+    public function __construct( $class, $id )
+    {
+        parent::__construct(
+            "An identity for the object of {$class} with ID {$id} already exists in the identity map."
+        );
+    }
+}
+?>
