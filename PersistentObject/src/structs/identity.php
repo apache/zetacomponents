048c14600448aead3383c03312b0dcdc8c3d5a80
ts:ez.no <dev-null@apache.org>
2009-01-16 11:32:34 +0000

    - Added storage for named related object sets to ezcPersistentIdentiy.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977068 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/src/structs/identity.php
+++ PersistentObject/src/structs/identity.php
@@ -24,24 +24,75 @@ class ezcPersistentIdentity extends ezcBaseStruct
 
     /**
      * Related objects of $object. 
+     *
+     * Structure:
+     *
+     * <code>
+     * <?php
+     * array(
+     *     '<relatedClassName>' => array(
+     *         '<id1>' => ezcPersistentObject,
+     *         '<id2>' => ezcPersistentObject,
+     *         // ...
+     *     ),
+     *     '<anotherRelatedClassName>' => array(
+     *         '<idA>' => ezcPersistentObject,
+     *         '<idB>' => ezcPersistentObject,
+     *         // ...
+     *     ),
+     *     // ...
+     * );
+     * ?>
+     * </code>
      * 
-     * @var array(string=>array(object))
+     * @var array(string=>array(mixed=>ezcPersistentObject))
      */
     public $relatedObjects;
+
+    /**
+     * Named sets of related objects. 
+     *
+     * Structure:
+     *
+     * <code>
+     * <?php
+     * array(
+     *     '<relatedClassName>' => array(
+     *         '<setName' => array(
+     *             '<id1>' => ezcPersistentObject,
+     *             '<id2>' => ezcPersistentObject,
+     *             // ...
+     *         ),
+     *         '<anotherSetName' => array(
+     *             '<idA>' => ezcPersistentObject,
+     *             '<idB>' => ezcPersistentObject,
+     *             // ...
+     *         ),
+     *     ),
+     *     // ...
+     * );
+     * ?>
+     * </code>
+     * 
+     * @var array(string=>array(mixed=>ezcPersistentObject))
+     */
+    public $namedRelatedObjectSets;
 
     /**
      * Creates a new object identity.
      *
      * Creates an identity struct for $object with references to its
-     * $relatedObjects.
+     * $relatedObjects and $namedRelatedObjectSets.
      * 
      * @param object $object 
      * @param array $relatedObjects 
+     * @param array $namedRelatedObjectSets
      */
-    public function __construct( $object = null, array $relatedObjects = null )
+    public function __construct( $object = null, array $relatedObjects = null, array $namedRelatedObjectSets )
     {
-        $this->object         = $object;
-        $this->relatedObjects = $relatedObjects;
+        $this->object                 = $object;
+        $this->relatedObjects         = $relatedObjects;
+        $this->namedRelatedObjectSets = $namedRelatedObjectSets;
     }
 }
 
