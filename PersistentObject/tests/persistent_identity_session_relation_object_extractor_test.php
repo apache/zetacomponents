0e339eb84543926f119133a56fd1fa3f4a667157
ts:ez.no <dev-null@apache.org>
2009-03-26 09:04:57 +0000

    - Implemented test cases for object extraction.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977332 13f79535-47bb-0310-9956-ffa450edef68

--- PersistentObject/tests/persistent_identity_session_relation_object_extractor_test.php
+++ PersistentObject/tests/persistent_identity_session_relation_object_extractor_test.php
@@ -89,6 +89,207 @@ class ezcPersistentIdentitySessionRelationObjectExtractorTest extends ezcPersist
             current( $this->session->getRelatedObjects( $person, 'RelationTestEmployer' ) )
         );
     }
+
+    public function testOneLevelMultiRelationExtract()
+    {
+        $relations = $this->getOneLevelMultiRelationRelations();
+        $q         = $this->getOneLevelMultiRelationQuery( $relations );
+        
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        // Actual test
+        $this->extractor->extractObjects(
+            $stmt, 'RelationTestPerson', 2, $relations
+        );
+
+        $person = $this->idMap->getIdentity( 'RelationTestPerson', 2 );
+        $this->assertNotNull( $person );
+        $this->assertEquals(
+            $this->session->load( 'RelationTestPerson', 2 ),
+            $person
+        );
+
+        $employers = $this->idMap->getRelatedObjects(
+            $person, 'RelationTestEmployer'
+        );
+        
+        $this->assertNotNull( $employers );
+
+        $this->assertEquals( 1, count( $employers ) );
+
+        $this->assertEquals(
+            current( $employers ),
+            current( $this->session->getRelatedObjects( $person, 'RelationTestEmployer' ) )
+        );
+
+        $addresses = $this->idMap->getRelatedObjects(
+            $person, 'RelationTestAddress'
+        );
+        
+        $this->assertNotNull( $addresses );
+
+        $this->assertEquals( 3, count( $addresses ) );
+
+        $this->assertEquals(
+            current( $addresses ),
+            current( $this->session->getRelatedObjects( $person, 'RelationTestAddress' ) )
+        );
+    }
+
+    public function testMultiLevelSingleRelation()
+    {
+        $relations = $this->getMultiLevelSingleRelationRelations();
+        $q         = $this->getMultiLevelSingleRelationQuery( $relations );
+        
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        // Actual test
+        $this->extractor->extractObjects(
+            $stmt, 'RelationTestPerson', 2, $relations
+        );
+
+        $person = $this->idMap->getIdentity( 'RelationTestPerson', 2 );
+        $this->assertNotNull( $person );
+        $this->assertEquals(
+            $this->session->load( 'RelationTestPerson', 2 ),
+            $person
+        );
+
+        $addresses = $this->idMap->getRelatedObjects(
+            $person, 'RelationTestAddress'
+        );
+        
+        $this->assertNotNull( $addresses );
+
+        $this->assertEquals( 3, count( $addresses ) );
+        
+        $realAddresses = $this->session->getRelatedObjects( $person, 'RelationTestAddress' );
+
+        $this->assertObjectSetsEqual( $realAddresses, $addresses );
+
+        foreach ( $addresses as $address )
+        {
+            $persons = $this->idMap->getRelatedObjects(
+                $address, 'RelationTestPerson'
+            );
+
+            $this->assertNotNull( $persons );
+
+            $realPersons = $this->session->getRelatedObjects(
+                $address, 'RelationTestPerson'
+            );
+
+            $this->assertObjectSetsEqual( $realPersons, $persons );
+        }
+    }
+
+    public function testMultiLevelMultiRelation()
+    {
+        $relations = $this->getMultiLevelMultiRelationRelations();
+        $q         = $this->getMultiLevelMultiRelationQuery( $relations );
+        
+        $stmt = $q->prepare();
+        $stmt->execute();
+
+        // Actual test
+        $this->extractor->extractObjects(
+            $stmt, 'RelationTestPerson', 2, $relations
+        );
+
+        $person = $this->idMap->getIdentity( 'RelationTestPerson', 2 );
+        $this->assertNotNull( $person );
+        $this->assertEquals(
+            $this->session->load( 'RelationTestPerson', 2 ),
+            $person
+        );
+
+        $addresses = $this->idMap->getRelatedObjects(
+            $person, 'RelationTestAddress'
+        );
+        
+        $this->assertNotNull( $addresses );
+
+        $this->assertEquals( 3, count( $addresses ) );
+        
+        $realAddresses = $this->session->getRelatedObjects( $person, 'RelationTestAddress' );
+
+        $this->assertObjectSetsEqual( $realAddresses, $addresses );
+
+        foreach ( $addresses as $address )
+        {
+            $persons = $this->idMap->getRelatedObjects(
+                $address, 'RelationTestPerson'
+            );
+
+            $this->assertNotNull( $persons );
+
+            $realPersons = $this->session->getRelatedObjects(
+                $address, 'RelationTestPerson'
+            );
+
+            $this->assertObjectSetsEqual( $realPersons, $persons );
+
+            foreach ( $persons as $relPerson )
+            {
+                $employers = $this->idMap->getRelatedObjects( $relPerson, 'RelationTestEmployer' );
+                $this->assertNotNull( $employers );
+                $realEmployers = $this->session->getRelatedObjects( $relPerson, 'RelationTestEmployer' );
+                $this->assertObjectSetsEqual( $realEmployers, $employers );
+
+                $birthdays = $this->idMap->getRelatedObjects( $relPerson, 'RelationTestBirthday' );
+
+                if ( $relPerson->id == 3 )
+                {
+                    // Person with ID 3 has no birthday assigned
+                    $this->assertNull( $birthdays );
+                }
+                else
+                {
+                    $this->assertNotNull( $birthdays );
+                    $realBirthdays = $this->session->getRelatedObjects( $relPerson, 'RelationTestBirthday' );
+                    $this->assertObjectSetsEqual( $realBirthdays, $birthdays );
+                }
+            }
+        }
+
+        $employers = $this->idMap->getRelatedObjects( $person, 'RelationTestEmployer' );
+        $this->assertNotNull( $employers );
+        $realEmployers = $this->session->getRelatedObjects( $person, 'RelationTestEmployer' );
+        $this->assertObjectSetsEqual( $realEmployers, $employers );
+
+        $birthdays = $this->idMap->getRelatedObjects( $person, 'RelationTestBirthday' );
+        $this->assertNotNull( $birthdays );
+        $realBirthdays = $this->session->getRelatedObjects( $person, 'RelationTestBirthday' );
+        $this->assertObjectSetsEqual( $realBirthdays, $birthdays );
+    }
+
+    protected function assertObjectSetsEqual( $realObjects, $identityObjects )
+    {
+        reset( $identityObjects );
+        reset( $realObjects );
+
+        do
+        {
+            $this->assertEquals(
+                current( $realObjects ),
+                current( $identityObjects ),
+                'Object set differs.'
+            );
+            next( $identityObjects );
+            next( $realObjects );
+        } while ( current( $realObjects ) !== false && current( $identityObjects ) !== false );
+
+        $this->assertFalse(
+            current( $realObjects ),
+            'Real object set has more elements than identity object set.'
+        );
+        $this->assertFalse(
+            current( $identityObjects ),
+            'Identity object set has more elements than real object set.'
+        );
+    }
 }
 
 ?>
