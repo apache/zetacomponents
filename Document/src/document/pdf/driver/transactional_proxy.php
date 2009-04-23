a1439585c49adf2c8196657ca8927e4be39a3de3
kn:ez.no <dev-null@apache.org>
2009-04-23 07:56:31 +0000

    - Implemented transactional PDF driver proxy # Read class description for reasoning, should make lots of stuff a lot # easier...
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977494 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Document/src/document/pdf/driver/transactional_proxy.php
@@ -0,0 +1,263 @@
+<?php
+/**
+ * File containing the ezcDocumentPdfTransactionalDriverProxy class
+ *
+ * @package Document
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @access private
+ */
+
+/**
+ * PDF driver proxy, which records write calls to proxied driver, wraps them
+ * into transactions to optionally revert or commit them later.
+ *
+ * Since page layouting algorithms are basically always backtracking algorithms
+ * they always try to render something, and need to recursively revert already
+ * "rendered" stuff. This proxy class for all driver classes records calls,
+ * optionally assiciates them with transaction identifies and allows to
+ * selectively revert or commint groups of such calls to the backend. Only when
+ * comitting or calling save() on the driver proxy any modifying (write)
+ * operations are actually issued on the proxied driver.
+ *
+ * @package Document
+ * @access private
+ * @version //autogen//
+ */
+class ezcDocumentPdfTransactionalDriverProxy extends ezcDocumentPdfDriver
+{
+    /**
+     * Wrapper direver instance
+     * 
+     * @var ezcDocumentPdfDriver
+     */
+    protected $driver;
+
+    /**
+     * Recorded transactions
+     * 
+     * @var array
+     */
+    protected $transactions = array();
+
+    /**
+     * Transaction identifier of current transaction
+     * 
+     * @var mixed
+     */
+    protected $currentTransaction = 0;
+
+    /**
+     * Set proxied driver
+     *
+     * Set driver, which should respond to read calls, and to which comitted
+     * write calls should be passed.
+     * 
+     * @param ezcDocumentPdfDriver $driver 
+     * @return void
+     */
+    public function setDriver( ezcDocumentPdfDriver $driver )
+    {
+        $this->driver = $driver;
+    }
+
+    /**
+     * Start a new transaction sequence
+     *
+     * Start a new transaction, which will record all calls, until the next
+     * transaction is started. This methods returns an identifier for this
+     * transaction, which can be used to commit this transaction, or revert
+     * everything since (including) this this transaction.
+     *
+     * @return mixed
+     */
+    public function startTransaction()
+    {
+        $this->transactions[++$this->currentTransaction] = array();
+        return $this->currentTransaction;
+    }
+
+    /**
+     * Commit recorded transactions
+     *
+     * If no transaction identifier is specified every recorded call will be
+     * comitted to the proxied driver. If you specify a transaction identifier,
+     * each transaction up to the current (including the current) transaction
+     * will be comitted.
+     * 
+     * @param mixed $transaction 
+     * @return void
+     */
+    public function commit( $transaction = null )
+    {
+        if ( ( $transaction !== null ) &&
+             !isset( $this->transactions[$transaction] ) )
+        {
+            return false;
+        }
+
+        foreach ( $this->transactions as $id => $calls )
+        {
+            foreach ( $calls as $call )
+            {
+                call_user_func_array( array( $this->driver, $call[0] ), $call[1] );
+            }
+            
+            unset( $this->transactions[$id] );
+            if ( $id === $transaction )
+            {
+                return true;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Revert transaction
+     *
+     * Revert all transactions after the specified (including the specified)
+     * transaction.
+     * 
+     * @param mixed $transaction 
+     * @return void
+     */
+    public function revert( $transaction )
+    {
+        if ( !isset( $this->transactions[$transaction] ) )
+        {
+            return false;
+        }
+
+        $remove = false;
+        foreach ( $this->transactions as $id => $calls )
+        {
+            if ( !$remove &&
+                 ( $id !== $transaction ) )
+            {
+                continue;
+            }
+
+            $remove = true;
+            unset( $this->transactions[$id] );
+        }
+
+        return true;
+    }
+
+    /**
+     * Record call
+     *
+     * Record a write call in the current transaction. A call is specified by
+     * its method name and the parameters.
+     *
+     * @param string $name 
+     * @param array $parameters 
+     * @return void
+     */
+    protected function recordCall( $name, array $parameters )
+    {
+        $this->transactions[$this->currentTransaction][] = array( $name, $parameters );
+    }
+
+    /**
+     * Create a new page
+     *
+     * Create a new page in the PDF document with the given width and height.
+     * 
+     * @param float $width 
+     * @param float $height 
+     * @return void
+     */
+    public function createPage( $width, $height )
+    {
+        // Just record this write call
+        $this->recordCall( __FUNCTION__, array( $width, $height ) );
+    }
+
+    /**
+     * Set text formatting option
+     *
+     * Set a text formatting option. The names of the options are the same used
+     * in the PCSS files and need to be translated by the driver to the proper
+     * backend calls.
+     *
+     *
+     * @param string $type 
+     * @param mixed $value 
+     * @return void
+     */
+    public function setTextFormatting( $type, $value )
+    {
+        // This call can be relevant for the size estimation, so it needs to be
+        // proxied and recorded
+        $this->recordCall( __FUNCTION__, array( $type, $value ) );
+
+        return $this->driver->setTextFormatting( $type, $value );
+    }
+
+    /**
+     * Calculate the rendered width of the current word
+     *
+     * Calculate the width of the passed word, using the currently set text
+     * formatting options.
+     * 
+     * @param string $word 
+     * @return float
+     */
+    public function calculateWordWidth( $word )
+    {
+        // Just required to read rendering properties of the current driver, no
+        // recording required.
+        return $this->driver->calculateWordWidth( $word );
+    }
+
+    /**
+     * Get current line height
+     *
+     * Return the current line height in millimeter based on the current font
+     * and text rendering settings.
+     * 
+     * @return float
+     */
+    public function getCurrentLineHeight()
+    {
+        // Just required to read rendering properties of the current driver, no
+        // recording required.
+        return $this->driver->getCurrentLineHeight();
+    }
+
+    /**
+     * Draw word at given position
+     *
+     * Draw the given word at the given position using the currently set text
+     * formatting options.
+     * 
+     * @param float $x 
+     * @param float $y 
+     * @param string $word 
+     * @return void
+     */
+    public function drawWord( $x, $y, $word )
+    {
+        // Just record this write call
+        $this->recordCall( __FUNCTION__, array( $x, $y, $word ) );
+    }
+
+    /**
+     * Generate and return PDF
+     *
+     * Return the generated binary PDF content as a string.
+     * 
+     * @return string
+     */
+    public function save()
+    {
+        // The ultimate write call. We can try to recommit an then just proxy
+        // the call.
+        $this->commit();
+        return $this->driver->save();
+    }
+}
+?>
---
+++ Document/tests/document_pdf_driver_transactions_tests.php
@@ -0,0 +1,254 @@
+<?php
+/**
+ * ezcDocumentPdfTransactionalDriverProxyTests
+ * 
+ * @package Document
+ * @version //autogen//
+ * @subpackage Tests
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+require_once 'pdf_test.php';
+
+/**
+ * Test suite for class.
+ * 
+ * @package Document
+ * @subpackage Tests
+ */
+class ezcDocumentPdfTransactionalDriverProxyTests extends ezcDocumentPdfTestCase
+{
+    protected $driver;
+
+    protected $mock;
+
+    public static function suite()
+    {
+        return new PHPUnit_Framework_TestSuite( __CLASS__ );
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->driver = new ezcDocumentPdfTransactionalDriverProxy();
+        $this->driver->setDriver(
+            $this->mock = $this->getMock( 'ezcDocumentPdfDriver', array(
+                'createPage',
+                'getCurrentLineHeight',
+                'calculateWordWidth',
+                'setTextFormatting',
+                'drawWord',
+                'save',
+            ) )
+        );
+    }
+
+    public function testNoIssuedWriteCallsToBackend()
+    {
+        $this->mock->expects( $this->never() )->method( 'drawWord' );
+
+        // Cause and record calls
+        $this->driver->drawWord( 0, 0, 'Hello world.' );
+    }
+
+    public function testPassReadCallsToBackend()
+    {
+        $this->mock->expects( $this->never() )->method( 'drawWord' );
+        $this->mock->expects( $this->once() )->method( 'calculateWordWidth' );
+
+        // Cause and record calls
+        $this->driver->calculateWordWidth( 'Hello world.' );
+        $this->driver->drawWord( 0, 0, 'Hello world.' );
+    }
+
+    public function testPassCombinedReadWriteCallToBackend()
+    {
+        $this->mock->expects( $this->once() )->method( 'setTextFormatting' );
+
+        // Cause and record calls
+        $this->driver->setTextFormatting( 'font-size', '12pt' );
+    }
+
+    public function testCommitSingleTransaction()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 0, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'Paragraphs' )
+        );
+        $this->mock->expects( $this->at( 1 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 44, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'are' )
+        );
+
+        // Cause and record calls
+        $transaction = $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->commit( $transaction );
+    }
+
+    public function testImplicitCommitMultipleTransactions()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 0, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'Paragraphs' )
+        );
+        $this->mock->expects( $this->at( 1 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 44, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'are' )
+        );
+        $this->mock->expects( $this->exactly( 2 ) )->method( 'drawWord' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $transaction = $this->driver->startTransaction();
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 60, 0, 'separated' );
+        $this->driver->commit( $transaction );
+    }
+
+    public function testCommitAll()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 0, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'Paragraphs' )
+        );
+        $this->mock->expects( $this->at( 1 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 44, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'are' )
+        );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->commit();
+    }
+
+    public function testCommitUnknownTransaction()
+    {
+        $this->mock->expects( $this->never() )->method( 'drawWord' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->commit( 'unknown id' );
+    }
+
+    public function testRevertTransactions()
+    {
+        $this->mock->expects( $this->never() )->method( 'drawWord' );
+
+        // Cause and record calls
+        $transaction = $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->revert( $transaction );
+        $this->driver->commit();
+    }
+
+    public function testPartialRevertTransactions()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 0, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'Paragraphs' )
+        );
+        $this->mock->expects( $this->exactly( 1 ) )->method( 'drawWord' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $transaction = $this->driver->startTransaction();
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->revert( $transaction );
+        $this->driver->commit();
+    }
+
+    public function testRevertMultipleTransactions()
+    {
+        $this->mock->expects( $this->never() )->method( 'drawWord' );
+
+        // Cause and record calls
+        $transaction = $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 44, 0, 'are' );
+        $this->driver->revert( $transaction );
+        $this->driver->commit();
+    }
+
+    public function testAutoCommitOnSave()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'drawWord' )->with(
+            $this->equalTo( 0, 1. ), $this->equalTo( 0, 1. ), $this->equalTo( 'Paragraphs' )
+        );
+        $this->mock->expects( $this->at( 1 ) )->method( 'save' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->drawWord( 0, 0, 'Paragraphs' );
+        $this->driver->save();
+    }
+
+    public function testCreatePageCall()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'createPage' )->with(
+            $this->equalTo( 100, 1. ), $this->equalTo( 100, 1. )
+        );
+        $this->mock->expects( $this->exactly( 1 ) )->method( 'createPage' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->createPage( 100, 100 );
+        $this->driver->save();
+    }
+
+    public function testSetTextFormattingCall()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'setTextFormatting' )->with(
+            $this->equalTo( 'font-size' ), $this->equalTo( '12pt' )
+        );
+        $this->mock->expects( $this->at( 1 ) )->method( 'setTextFormatting' )->with(
+            $this->equalTo( 'font-size' ), $this->equalTo( '12pt' )
+        );
+        $this->mock->expects( $this->exactly( 2 ) )->method( 'setTextFormatting' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->driver->setTextFormatting( 'font-size', '12pt' );
+        $this->driver->save();
+    }
+
+    public function testCalculateWordWidthCall()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'calculateWordWidth' )->with(
+            $this->equalTo( 'my word' )
+        )->will( $this->returnValue( 12 ) );
+        $this->mock->expects( $this->exactly( 1 ) )->method( 'calculateWordWidth' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->assertEquals(
+            12,
+            $this->driver->calculateWordWidth( 'my word' )
+        );
+        $this->driver->save();
+    }
+
+    public function testGetLineHeightCall()
+    {
+        $this->mock->expects( $this->at( 0 ) )->method( 'getCurrentLineHeight' )
+            ->will( $this->returnValue( 12 ) );
+        $this->mock->expects( $this->exactly( 1 ) )->method( 'getCurrentLineHeight' );
+
+        // Cause and record calls
+        $this->driver->startTransaction();
+        $this->assertEquals(
+            12,
+            $this->driver->getCurrentLineHeight()
+        );
+        $this->driver->save();
+    }
+}
+
+?>
