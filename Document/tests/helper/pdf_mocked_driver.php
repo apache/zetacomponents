2fcf852f7677fdc04b6c4290d5438415484d23f2
kn:ez.no <dev-null@apache.org>
2009-05-07 06:13:25 +0000

    - Adapted mocked driver to base driver interface changes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977539 13f79535-47bb-0310-9956-ffa450edef68

--- Document/tests/helper/pdf_mocked_driver.php
+++ Document/tests/helper/pdf_mocked_driver.php
@@ -144,6 +144,29 @@ class ezcTestDocumentPdfMockDriver extends ezcDocumentPdfDriver
     }
 
     /**
+     * Draw rectangle
+     *
+     * Draw rectangle of specified dimensions in specified color. The last
+     * parameter defined, if only an outline should be renderer, or if a filled
+     * rectangle should be rendered.
+     *
+       @TODO: Requires proper color specification
+       @TODO: Requires border width specification
+     * 
+     * @param float $x 
+     * @param float $y 
+     * @param float $width 
+     * @param float $height 
+     * @param mixed $color 
+     * @param bool $filled 
+     * @return void
+     */
+    public function drawRectangle( $x, $y, $width, $height, $color, $filled = true )
+    {
+        $this->calls[] = array( __FUNCTION__, func_get_args() );
+    }
+
+    /**
      * Generate and return PDF
      *
      * Return the generated binary PDF content as a string.
