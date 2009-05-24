6ece4f8165901c765e360a1c7d253943548e532e
kn:ez.no <dev-null@apache.org>
2009-05-24 20:35:44 +0000

    - Updated parsed cached CSS file
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977618 13f79535-47bb-0310-9956-ffa450edef68

--- Document/src/document/pdf/style/default.php
+++ Document/src/document/pdf/style/default.php
@@ -5,37 +5,78 @@ return array (
   ezcDocumentPdfCssDirective::__set_state(array(
      'address' => 
     array (
-      0 => 'page',
-    ),
-     'formats' => 
-    array (
-      'text-columns' => '1',
-      'text-column-spacing' => '10mm',
-    ),
-     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
-     'line' => 1,
-     'position' => 5,
-     'regularExpression' => NULL,
-  )),
-  1 => 
-  ezcDocumentPdfCssDirective::__set_state(array(
-     'address' => 
-    array (
       0 => 'article',
     ),
      'formats' => 
     array (
       'font-size' => '12pt',
       'font-family' => 'serif',
+      'font-weight' => 'normal',
+      'font-style' => 'normal',
       'line-height' => '1.4',
       'text-align' => 'left',
+      'color' => '#2e3436',
+      'text-columns' => '1',
+      'text-column-spacing' => '10mm',
+      'padding' => '0mm',
+      'margin' => '0mm',
+      'orphans' => '3',
+      'widows' => '3',
     ),
      'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
-     'line' => 6,
-     'position' => 9,
+     'line' => 1,
+     'position' => 8,
+     'regularExpression' => NULL,
+  )),
+  1 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'page',
+    ),
+     'formats' => 
+    array (
+      'page-size' => 'A4',
+      'page-orientation' => 'portrait',
+      'padding' => '22mm 16mm',
+      'margin' => '0mm',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 24,
+     'position' => 6,
      'regularExpression' => NULL,
   )),
   2 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'para',
+    ),
+     'formats' => 
+    array (
+      'margin' => '3mm 0mm 1mm 0mm',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 34,
+     'position' => 6,
+     'regularExpression' => NULL,
+  )),
+  3 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'emphasis',
+    ),
+     'formats' => 
+    array (
+      'font-weight' => 'bold',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 38,
+     'position' => 10,
+     'regularExpression' => NULL,
+  )),
+  4 => 
   ezcDocumentPdfCssDirective::__set_state(array(
      'address' => 
     array (
@@ -43,12 +84,142 @@ return array (
     ),
      'formats' => 
     array (
-      'font-size' => '16pt',
+      'font-family' => 'sans-serif',
       'font-weight' => 'bold',
+      'color' => '#000000',
     ),
      'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
-     'line' => 13,
+     'line' => 42,
      'position' => 7,
+     'regularExpression' => NULL,
+  )),
+  5 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '32pt',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 48,
+     'position' => 7,
+     'regularExpression' => NULL,
+  )),
+  6 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '24pt',
+      'text-columns' => '1',
+      'margin' => '10mm 0mm 4mm 0mm',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 52,
+     'position' => 17,
+     'regularExpression' => NULL,
+  )),
+  7 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> section',
+      2 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '20pt',
+      'text-columns' => '1',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 58,
+     'position' => 27,
+     'regularExpression' => NULL,
+  )),
+  8 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> section',
+      2 => '> section',
+      3 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '18',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 63,
+     'position' => 37,
+     'regularExpression' => NULL,
+  )),
+  9 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> section',
+      2 => '> section',
+      3 => '> section',
+      4 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '16pt',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 67,
+     'position' => 47,
+     'regularExpression' => NULL,
+  )),
+  10 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> section',
+      2 => '> section',
+      3 => '> section',
+      4 => '> section',
+      5 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '14pt',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 71,
+     'position' => 57,
+     'regularExpression' => NULL,
+  )),
+  11 => 
+  ezcDocumentPdfCssDirective::__set_state(array(
+     'address' => 
+    array (
+      0 => 'section',
+      1 => '> section',
+      2 => '> section',
+      3 => '> section',
+      4 => '> section',
+      5 => '> section',
+      6 => '> title',
+    ),
+     'formats' => 
+    array (
+      'font-size' => '12pt',
+    ),
+     'file' => '/home/kore/devel/ezcomponents/trunk/Document/src/document/pdf/style/default.css',
+     'line' => 75,
+     'position' => 67,
      'regularExpression' => NULL,
   )),
 );
