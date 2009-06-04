758b1441247d4c7fb2e9d795a969004849d37969
sb:ez.no <dev-null@apache.org>
2009-06-04 08:00:17 +0000

    - Initial work on the new test runner.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977737 13f79535-47bb-0310-9956-ffa450edef68

--- UnitTest/src/test/new_runner.php
+++ UnitTest/src/test/new_runner.php
@@ -7,249 +7,53 @@
  * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
-require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'PHPUnit/TextUI/Command.php';
 require_once 'PHPUnit/Util/Class.php';
 require_once 'PHPUnit/Util/Filter.php';
 
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
 
 /**
- * Test runner to run eZ Components test suites.
+ * Test runner to run the eZ Components test suite(s).
  *
  * @package UnitTest
  * @version //autogentag//
  */
-class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
+class ezcTestNewRunner extends PHPUnit_TextUI_Command
 {
     const SUITE_FILENAME = 'tests/suite.php';
 
-    public static function main()
+    protected $arguments = array(
+      'backupStaticAttributes'      => false,
+      'convertErrorsToExceptions'   => true,
+      'convertNoticesToExceptions'  => false,
+      'convertWarningsToExceptions' => false,
+      'listGroups'                  => false,
+      'loader'                      => null,
+      'useDefaultConfiguration'     => true
+    );
+
+    protected $release = 'trunk';
+
+    public function __construct()
     {
-        $version = PHPUnit_Runner_Version::id();
-
-        if ( version_compare( $version, '3.1.2' ) == -1 && $version !== '@package_version@' )
-        {
-            echo "You need PHPUnit 3.1.2 (or later) to run this testsuite.\n";
-            die();
-        }
-
-        $tr = new ezcTestRunner();
-        $tr->runFromArguments();
+        $this->longOptions['dsn=']     = 'handleDsn';
+        $this->longOptions['release='] = 'handleRelease';
     }
 
-    /** 
-     * Registers the consoleInput options and arguments.
-     *
-     * The options and arguments are registered in the given $consoleInput object.
-     *
-     * @param ezcConsoleInput $consoleInput
-     * @return void
-     */
-    protected static function registerConsoleArguments( $consoleInput )
+    protected function handleCustomTestSuite()
     {
-        // Help option
-        $help = new ezcConsoleOption( '', 'help', ezcConsoleInput::TYPE_NONE );
-        $help->shorthelp = 'Show this help';
-        $consoleInput->registerOption( $help );
-
-        // Release option
-        $help = new ezcConsoleOption( 'r', 'release', ezcConsoleInput::TYPE_STRING );
-        $help->shorthelp = "The release from the svn. Use either 'trunk' or 'stable'.";
-        $help->default = 'trunk';
-        $consoleInput->registerOption( $help );
-
-        // DSN option
-        $dsn = new ezcConsoleOption( 'D', 'dsn', ezcConsoleInput::TYPE_STRING );
-        $dsn->shorthelp = 'Use the database specified with a DSN: type://user:password@host/database.';
-        $dsn->longhelp   = 'An example to connect with the local MySQL database is:\n';
-        $dsn->longhelp  .= 'mysql://root@mypass@localhost/unittests';
-        $consoleInput->registerOption( $dsn );
-
-        // Code Coverage HTML option
-        $report = new ezcConsoleOption( '', 'coverage-html', ezcConsoleInput::TYPE_STRING );
-        $report->shorthelp = 'Generate code coverage report in HTML format to directory.';
-        $consoleInput->registerOption( $report );
-
-        // Code Coverage XML option
-        $coverage = new ezcConsoleOption( '', 'coverage-xml', ezcConsoleInput::TYPE_STRING );
-        $coverage->shorthelp = 'Write code coverage data in Clover XML format.';
-        $consoleInput->registerOption( $coverage );
-
-        // Filter option
-        $filter = new ezcConsoleOption( '', 'filter', ezcConsoleInput::TYPE_STRING );
-        $filter->shorthelp = 'Filter which tests to run.';
-        $consoleInput->registerOption( $filter );
-
-        // Project Mess Detector (PMD) XML option
-        $pmd = new ezcConsoleOption( '', 'log-pmd', ezcConsoleInput::TYPE_STRING );
-        $pmd->shorthelp = 'Write violations report in PMD XML format.';
-        $consoleInput->registerOption( $pmd );
-
-        // Logfile XML option
-        $xml = new ezcConsoleOption( '', 'log-xml', ezcConsoleInput::TYPE_STRING );
-        $xml->shorthelp = 'Log test execution in XML format to file.';
-        $consoleInput->registerOption( $xml );
-
-        // XML Configuration File option
-        $configuration = new ezcConsoleOption( '', 'configuration', ezcConsoleInput::TYPE_STRING );
-        $configuration->shorthelp = 'Read configuration from XML file.';
-        $consoleInput->registerOption( $configuration );
-
-        // Verbose option
-        $verbose = new ezcConsoleOption( 'v', 'verbose', ezcConsoleInput::TYPE_NONE );
-        $verbose->shorthelp = 'Output more verbose information.';
-        $consoleInput->registerOption( $verbose );
-    }
-
-    protected static function processConsoleArguments( $consoleInput )
-    {
-        try
-        {
-             $consoleInput->process();
-        }
-        catch ( ezcConsoleOptionException $e )
-        {
-            die ( $e->getMessage() );
-        }
-    }
-
-    protected static function displayHelp( $consoleInput )
-    {
-        echo $consoleInput->getHelpText( 'eZ Components Test Runner' );
-    }
-
-    public function runFromArguments()
-    {
-        /* The following hack is needed so that we can also test the
-           console tools in the stable branch */
-        if ( in_array( 'stable', $_SERVER['argv'] ) )
-        {
-            ezcBase::setWorkingDirectory( getcwd() );
-        }
-
-        $consoleInput = new ezcConsoleInput();
-        self::registerConsoleArguments( $consoleInput );
-        self::processConsoleArguments( $consoleInput );
-
-        if ( $consoleInput->getOption( 'help' )->value )
-        {
-            self::displayHelp( $consoleInput );
-            exit();
-        }
-
-        if ( $consoleInput->getOption( 'dsn' )->value )
-        {
-            $dsn = $consoleInput->getOption( 'dsn' )->value;
-
-            try 
-            {
-                $this->initializeDatabase( $dsn );
-            }
-            catch ( Exception $e )
-            {
-                print( "Database initialization error: {$e->getMessage()}\n" );
-                return;
-            }
-        }
-
-        $this->printCredits();
-
-        $params = array(
-          'backupStaticAttributes'      => false,
-          'convertErrorsToExceptions'   => true,
-          'convertNoticesToExceptions'  => false,
-          'convertWarningsToExceptions' => false
-        );
-
-        // Set the release. Default is trunk. 
-        $release = $consoleInput->getOption( 'release' )->value;
-
-        $config    = $consoleInput->getOption( 'configuration' )->value;
-        $reportDir = $consoleInput->getOption( 'coverage-html' )->value;
-        $coverage  = $consoleInput->getOption( 'coverage-xml' )->value;
-        $filter    = $consoleInput->getOption( 'filter' )->value;
-        $pmd       = $consoleInput->getOption( 'log-pmd' )->value;
-        $logfile   = $consoleInput->getOption( 'log-xml' )->value;
-
+        $directory     = getcwd();
         $fillWhitelist = false;
+        $packages      = $this->options[1];
 
-        if ( $filter )
+        $this->arguments['test'] = new PHPUnit_Framework_TestSuite;
+        $this->arguments['test']->setName( 'eZ Components' );
+
+        if ( empty( $packages ) )
         {
-            $params['filter'] = $filter;
-        }
-
-        if ( $config )
-        {
-            $params['configuration'] = $config;
-        }
-
-        if ( $logfile )
-        {
-            $params['xmlLogfile'] = $logfile;
-        }
-
-        if ( $coverage )
-        {
-            $params['coverageXML'] = $coverage;
-            $fillWhitelist = true;
-        }
-
-        if ( $pmd )
-        {
-            $params['pmdXML'] = $pmd;
-            $fillWhitelist = true;
-        }
-
-        if ( $reportDir )
-        {
-            $params['reportDirectory'] = $reportDir;
-            $fillWhitelist = true;
-        }
-
-        if ( $consoleInput->getOption( 'verbose' )->value )
-        {
-            $params['verbose'] = true;
-        }
-        else
-        {
-            $params['verbose'] = false;
-        }
-
-        $this->setPrinter( new ezcTestPrinter( $params['verbose'] ) );
-
-        $allSuites = $this->prepareTests( $consoleInput->getArguments(), $release, $fillWhitelist );
-        $result    = $this->doRun( $allSuites, $params );
-
-        if ( $result->errorCount() > 0 )
-        {
-            exit( PHPUnit_TextUI_TestRunner::EXCEPTION_EXIT );
-        }
-
-        if ( $result->failureCount() > 0 )
-        {
-            exit( PHPUnit_TextUI_TestRunner::FAILURE_EXIT );
-        }
-
-        exit( PHPUnit_TextUI_TestRunner::SUCCESS_EXIT );
-    }
-
-    protected function printCredits()
-    {
-        print( "ezcUnitTest uses the PHPUnit " . PHPUnit_Runner_Version::id() . " framework from Sebastian Bergmann.\n\n" );
-    }
-
-    protected function prepareTests( $packages, $release, $fillWhitelist )
-    {
-        print( '[Preparing tests]:' );
-
-        $directory = getcwd();
-
-        $allSuites = new PHPUnit_Framework_TestSuite;
-        $allSuites->setName( 'eZ Components' );
-
-        if ( sizeof( $packages ) == 0 )
-        {
-            $packages = $this->getPackages( $release, $directory );
+            $packages = $this->getPackages( $directory );
         }
 
         foreach ( $packages as $package )
@@ -257,8 +61,8 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
             $added      = false;
             $slashCount = substr_count( $package, DIRECTORY_SEPARATOR );
 
-            if ( ( $release == 'trunk'  && $slashCount !== 0 ) ||
-                 ( $release == 'stable' && $slashCount > 1 ) )
+            if ( ( $this->release == 'trunk'  && $slashCount !== 0 ) ||
+                 ( $this->release == 'stable' && $slashCount > 1 ) )
             {
                 if ( file_exists( $package ) )
                 {
@@ -266,25 +70,25 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
                     require_once( $package );
                     $class = PHPUnit_Util_Class::collectEnd();
 
-                    if ( count( $class ) > 0 )
+                    if ( !empty( $class ) )
                     {
-                        $allSuites->addTest( call_user_func( array( array_pop( $class ), 'suite' ) ) );
+                        $this->arguments['test']->addTest( call_user_func( array( array_pop( $class ), 'suite' ) ) );
                         $added   = true;
                         $package = substr($package, 0, strpos($package, DIRECTORY_SEPARATOR));
                     }
                     else
                     {
-                        echo "\n Cannot load: $package. \n";
+                        die( "\n Cannot load: $package. \n" );
                     }
                 }
             }
             else 
             {
-                $suite = $this->getTestSuite( $directory, $package, $release );
+                $suite = $this->getTestSuite( $directory, $package );
 
                 if ( !is_null( $suite ) )
                 {
-                    $allSuites->addTest( $suite );
+                    $this->arguments['test']->addTest( $suite );
                     $added = true;
                 }
             }
@@ -305,22 +109,15 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
                 }
             }
         }
-
-        return $allSuites;
     }
 
-    /**
-     * @param  string $release Release branch (stable or trunk)
-     * @param  string $dir     Absolute or relative path to packages directory
-     * @return array           Package names
-     */
-    protected function getPackages( $release, $dir )
+    protected function getPackages( $directory )
     {
         $packages = array();
 
-        if ( is_dir( $dir ) )
+        if ( is_dir( $directory ) )
         {
-            $entries = glob( $release == 'trunk' ? "$dir/*" : "$dir/*/*" );
+            $entries = glob( $this->release == 'trunk' ? "$directory/*" : "$directory/*/*" );
 
             foreach ( $entries as $entry )
             {
@@ -334,9 +131,9 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
         return $packages;
     }
 
-    protected function isPackageDir( $dir )
+    protected function isPackageDir( $directory )
     {
-        if ( !is_dir( $dir ) || !file_exists( $dir . '/tests/suite.php' ) )
+        if ( !is_dir( $directory ) || !file_exists( $directory . '/tests/suite.php' ) )
         {
             return false;
         }
@@ -344,15 +141,15 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
         return true;
     }
 
-    protected function getTestSuite( $dir, $package, $release )
+    protected function getTestSuite( $directory, $package )
     {
-        $suitePath = implode( '/', array( $dir, '..', $release, $package, self::SUITE_FILENAME ) );
+        $suitePath = implode( '/', array( $directory, '..', $this->release, $package, self::SUITE_FILENAME ) );
 
         if ( file_exists( $suitePath ) )
         {
             require_once( $suitePath );
 
-            if ( $release == 'stable' )
+            if ( $this->release == 'stable' )
             {
                 $package = substr( $package, 0, strpos( $package, '/' ) );
             }
@@ -366,24 +163,35 @@ class ezcTestNewRunner extends PHPUnit_TextUI_TestRunner
         return null;
     }
 
-    protected function initializeDatabase( $dsn )
+    protected function handleDsn( $value )
     {
-        $ts = ezcTestSettings::getInstance();
-        $settings = ezcDbFactory::parseDSN( $dsn );
-
-        // Store the settings
-        $ts->db->dsn = $dsn;
-    
         try
         {
-            $ts->setDatabaseSettings( $settings );
-            $db = ezcDbFactory::create( $settings );
-            ezcDbInstance::set( $db );
+            $ts       = ezcTestSettings::getInstance();
+            $settings = ezcDbFactory::parseDSN( $value );
+
+            $ts->db->value = $value;
+        
+            try
+            {
+                $ts->setDatabaseSettings( $settings );
+                $db = ezcDbFactory::create( $settings );
+                ezcDbInstance::set( $db );
+            }
+            catch ( ezcDbException $e )
+            {
+                die( $e->getMessage() );
+            }
         }
-        catch ( ezcDbException $e )
+        catch ( Exception $e )
         {
-            die( $e->getMessage() );
+            die( "Database initialization error: {$e->getMessage()}\n" );
         }
+    }
+
+    protected function handleRelease( $value )
+    {
+        $this->release = $value;
     }
 }
 ?>
