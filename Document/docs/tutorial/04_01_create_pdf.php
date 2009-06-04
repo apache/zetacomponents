220f6fe0fcce8c9adf9f4cc761b2a8d1cf4ad929
kn:ez.no <dev-null@apache.org>
2009-06-04 08:18:16 +0000

    - Started with document tutorial
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@977739 13f79535-47bb-0310-9956-ffa450edef68

--- Document/docs/tutorial/04_01_create_pdf.php
+++ Document/docs/tutorial/04_01_create_pdf.php
@@ -2,12 +2,14 @@
 
 require 'tutorial_autoload.php';
 
+// Convert some input RSTfile to docbook
 $document = new ezcDocumentRst();
 $document->loadFile( './article/introduction.txt' );
 
 $docbook = $document->getAsDocbook();
 file_put_contents( './article/introduction.xml', $docbook );
 
+// Load the docbook document and create a PDF from it
 $docbook->loadFile( './article/introduction.xml' );
 
 $pdf = new ezcDocumentPdf();
