6710c82f1578dba70b78f301c4e0ca3f613ce984
dr:ez.no <dev-null@apache.org>
2009-08-11 14:49:26 +0000

    - Renamed files according to guidelines.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978063 13f79535-47bb-0310-9956-ffa450edef68

--- Archive/src/archive_autoload.php
+++ Archive/src/archive_autoload.php
@@ -2,7 +2,7 @@
 /**
  * Autoloader definition for the Archive component.
  *
- * @copyright Copyright (C) 2005-2009 eZ systems as. All rights reserved.
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @version //autogentag//
  * @filesource
@@ -10,35 +10,35 @@
  */
 
 return array(
-    'ezcArchiveException'                 => 'Archive/exceptions/archive_exception.php',
-    'ezcArchiveBlockSizeException'        => 'Archive/exceptions/archive_block_size.php',
-    'ezcArchiveChecksumException'         => 'Archive/exceptions/archive_checksum.php',
-    'ezcArchiveEmptyException'            => 'Archive/exceptions/archive_empty.php',
-    'ezcArchiveEntryPrefixException'      => 'Archive/exceptions/archive_entry_prefix.php',
-    'ezcArchiveInternalException'         => 'Archive/exceptions/archive_internal_exception.php',
-    'ezcArchiveIoException'               => 'Archive/exceptions/archive_io.php',
-    'ezcArchiveUnknownTypeException'      => 'Archive/exceptions/archive_unknown_type.php',
-    'ezcArchiveValueException'            => 'Archive/exceptions/archive_value.php',
+    'ezcArchiveException'                 => 'Archive/exceptions/exception.php',
+    'ezcArchiveBlockSizeException'        => 'Archive/exceptions/block_size.php',
+    'ezcArchiveChecksumException'         => 'Archive/exceptions/checksum.php',
+    'ezcArchiveEmptyException'            => 'Archive/exceptions/empty.php',
+    'ezcArchiveEntryPrefixException'      => 'Archive/exceptions/entry_prefix.php',
+    'ezcArchiveInternalException'         => 'Archive/exceptions/internal_exception.php',
+    'ezcArchiveIoException'               => 'Archive/exceptions/io.php',
+    'ezcArchiveUnknownTypeException'      => 'Archive/exceptions/unknown_type.php',
+    'ezcArchiveValueException'            => 'Archive/exceptions/value.php',
     'ezcArchive'                          => 'Archive/archive.php',
-    'ezcArchiveV7Header'                  => 'Archive/tar/headers/tar_v7.php',
-    'ezcArchiveV7Tar'                     => 'Archive/tar/v7_tar.php',
+    'ezcArchiveV7Header'                  => 'Archive/tar/headers/v7.php',
+    'ezcArchiveV7Tar'                     => 'Archive/tar/v7.php',
     'ezcArchiveFile'                      => 'Archive/file/file.php',
-    'ezcArchiveLocalFileHeader'           => 'Archive/zip/headers/zip_local_file.php',
-    'ezcArchiveUstarHeader'               => 'Archive/tar/headers/tar_ustar.php',
-    'ezcArchiveUstarTar'                  => 'Archive/tar/ustar_tar.php',
+    'ezcArchiveLocalFileHeader'           => 'Archive/zip/headers/local_file.php',
+    'ezcArchiveUstarHeader'               => 'Archive/tar/headers/ustar.php',
+    'ezcArchiveUstarTar'                  => 'Archive/tar/ustar.php',
     'ezcArchiveBlockFile'                 => 'Archive/file/block_file.php',
-    'ezcArchiveCentralDirectoryEndHeader' => 'Archive/zip/headers/zip_central_directory_end.php',
-    'ezcArchiveCentralDirectoryHeader'    => 'Archive/zip/headers/zip_central_directory.php',
+    'ezcArchiveCentralDirectoryEndHeader' => 'Archive/zip/headers/central_directory_end.php',
+    'ezcArchiveCentralDirectoryHeader'    => 'Archive/zip/headers/central_directory.php',
     'ezcArchiveCharacterFile'             => 'Archive/file/character_file.php',
     'ezcArchiveChecksums'                 => 'Archive/utils/checksums.php',
     'ezcArchiveEntry'                     => 'Archive/entry.php',
     'ezcArchiveFileStructure'             => 'Archive/structs/file.php',
     'ezcArchiveFileType'                  => 'Archive/utils/file_type.php',
-    'ezcArchiveGnuHeader'                 => 'Archive/tar/headers/tar_gnu.php',
-    'ezcArchiveGnuTar'                    => 'Archive/tar/gnu_tar.php',
+    'ezcArchiveGnuHeader'                 => 'Archive/tar/headers/gnu.php',
+    'ezcArchiveGnuTar'                    => 'Archive/tar/gnu.php',
     'ezcArchiveOptions'                   => 'Archive/options/archive.php',
-    'ezcArchivePaxHeader'                 => 'Archive/tar/headers/tar_pax.php',
-    'ezcArchivePaxTar'                    => 'Archive/tar/pax_tar.php',
+    'ezcArchivePaxHeader'                 => 'Archive/tar/headers/pax.php',
+    'ezcArchivePaxTar'                    => 'Archive/tar/pax.php',
     'ezcArchiveStatMode'                  => 'Archive/utils/stat_mode.php',
     'ezcArchiveZip'                       => 'Archive/zip/zip.php',
 );
