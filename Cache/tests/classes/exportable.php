ab15deb696e4b1e30df63e6490be2347def68a58
ts:ez.no <dev-null@apache.org>
2009-08-17 10:13:37 +0000

    - Added missing test class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978077 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Cache/tests/classes/exportable.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ezcCacheMemcacheBackendTest 
+ * 
+ * @package Cache
+ * @subpackage Tests
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+/**
+ * Test class implementing {@link ezcBaseExportable}.
+ * 
+ * @package Cache
+ * @subpackage Tests
+ */
+class ezcCacheTestExportable implements ezcBaseExportable
+{
+    public $string;
+
+    protected $int;
+
+    private $float;
+
+    public function __construct($string, $int, $float)
+    {
+        $this->string = $string;
+        $this->int    = $int;
+        $this->float  = $float;
+    }
+
+    public static function __set_state( array $state )
+    {
+        return new ezcCacheTestExportable(
+            $state['string'],
+            $state['int'],
+            $state['float']
+        );
+    }
+}
+
+?>
