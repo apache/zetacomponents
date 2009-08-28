2b2d584837a19f2270106a0f9fd8a016dba4bc28
sb:ez.no <dev-null@apache.org>
2009-08-28 10:01:52 +0000

    - Increase compatibility with stricter MySQL server modes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978136 13f79535-47bb-0310-9956-ffa450edef68

--- WorkflowDatabaseTiein/design/schema_mysql.sql
+++ WorkflowDatabaseTiein/design/schema_mysql.sql
@@ -14,7 +14,7 @@ CREATE TABLE node (
   workflow_id        INTEGER      UNSIGNED NOT NULL REFERENCES workflow.workflow_id,
   node_id            INTEGER      UNSIGNED NOT NULL AUTO_INCREMENT,
   node_class         VARCHAR(255)          NOT NULL,
-  node_configuration TEXT                  NOT NULL,
+  node_configuration BLOB                      NULL,
 
   PRIMARY KEY             (node_id),
           KEY workflow_id (workflow_id)
@@ -41,11 +41,11 @@ DROP TABLE IF EXISTS execution;
 CREATE TABLE execution (
   workflow_id              INTEGER UNSIGNED NOT NULL REFERENCES workflow.workflow_id,
   execution_id             INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  execution_parent         INTEGER UNSIGNED NOT NULL REFERENCES execution.execution_id,
+  execution_parent         INTEGER UNSIGNED     NULL REFERENCES execution.execution_id,
   execution_started        INTEGER          NOT NULL,
-  execution_variables      TEXT             NOT NULL,
-  execution_waiting_for    TEXT             NOT NULL,
-  execution_threads        TEXT             NOT NULL,
+  execution_variables      BLOB                 NULL,
+  execution_waiting_for    BLOB                 NULL,
+  execution_threads        BLOB                 NULL,
   execution_next_thread_id INTEGER UNSIGNED NOT NULL,
 
   PRIMARY KEY                  (execution_id, workflow_id),
@@ -56,8 +56,8 @@ DROP TABLE IF EXISTS execution_state;
 CREATE TABLE execution_state (
   execution_id          INTEGER UNSIGNED NOT NULL REFERENCES execution.execution_id,
   node_id               INTEGER UNSIGNED NOT NULL REFERENCES node.node_id,
-  node_state            TEXT             NOT NULL,
-  node_activated_from   TEXT             NOT NULL,
+  node_state            BLOB                 NULL,
+  node_activated_from   BLOB                 NULL,
   node_thread_id        INTEGER UNSIGNED NOT NULL,
 
   PRIMARY KEY (execution_id, node_id)
