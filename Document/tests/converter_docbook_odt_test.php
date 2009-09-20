b7572a640eeb5ae1029fcb80cef8ada249e3e360
ts:ez.no <dev-null@apache.org>
2009-09-20 10:16:01 +0000

    - Added initial test class for DocBook to ODT conversion.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Document/tests/converter_docbook_odt_test.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * ezcDocumentConverterEzp3TpEzp4Tests
+ * 
+ * @package Document
+ * @version //autogen//
+ * @subpackage Tests
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * Test suite for class.
+ * 
+ * @package Document
+ * @subpackage Tests
+ */
+class ezcDocumentConverterDocbookToOdtTests extends ezcTestCase
+{
+    protected static $testDocuments = null;
+
+    public static function suite()
+    {
+        return new PHPUnit_Framework_TestSuite( __CLASS__ );
+    }
+
+    /*
+
+    public function testOdtConverterOptionsFormatOutput()
+    {
+        $options = new ezcDocumentOdtConverterOptions();
+        $options->formatOutput = false;
+
+        try
+        {
+            $options->formatOutput = 0;
+            $this->fail( 'Expected ezcBaseValueException.' );
+        }
+        catch ( ezcBaseValueException $e )
+        {}
+    }
+
+    public function testOdtConverterOptionsDublinCoreMetadata()
+    {
+        $options = new ezcDocumentOdtConverterOptions();
+        $options->dublinCoreMetadata = false;
+
+        try
+        {
+            $options->dublinCoreMetadata = 0;
+            $this->fail( 'Expected ezcBaseValueException.' );
+        }
+        catch ( ezcBaseValueException $e )
+        {}
+    }
+
+    public function testOdtConverterOptionsStyleSheets()
+    {
+        $options = new ezcDocumentOdtConverterOptions();
+        $options->styleSheets = array( 'url' );
+        $options->styleSheets = null;
+
+        try
+        {
+            $options->styleSheets = 0;
+            $this->fail( 'Expected ezcBaseValueException.' );
+        }
+        catch ( ezcBaseValueException $e )
+        {}
+    }
+
+    public function testOdtConverterOptionsUnknownOption()
+    {
+        $options = new ezcDocumentOdtConverterOptions();
+
+        try
+        {
+            $options->notExistingOption = 0;
+            $this->fail( 'Expected ezcBasePropertyNotFoundException.' );
+        }
+        catch ( ezcBasePropertyNotFoundException $e )
+        {}
+    }
+
+    */
+
+    public static function getTestDocuments()
+    {
+        if ( self::$testDocuments === null )
+        {
+            // Get a list of all test files from the respektive folder
+            $testFiles = glob( dirname( __FILE__ ) . '/files/docbook/odt/s_*.xml' );
+
+            // Create array with the test file and the expected result file
+            foreach ( $testFiles as $file )
+            {
+                self::$testDocuments[] = array(
+                    $file,
+                    substr( $file, 0, -3 ) . 'fodt'
+                );
+            }
+        }
+
+        var_dump( self::$testDocuments );
+
+        return self::$testDocuments;
+    }
+
+    /**
+     * @dataProvider getTestDocuments
+     */
+    public function testLoadXmlDocumentFromFile( $from, $to )
+    {
+        if ( !is_file( $to ) )
+        {
+            $this->markTestSkipped( "Comparision file '$to' not yet defined." );
+        }
+
+        $doc = new ezcDocumentDocbook();
+        $doc->loadFile( $from );
+
+        $converter = new ezcDocumentDocbookToOdtConverter();
+        $created = $converter->convert( $doc );
+
+        $this->assertTrue(
+            $created instanceof ezcDocumentOdt
+        );
+
+        // Store test file, to have something to compare on failure
+        $tempDir = $this->createTempDir( 'docbook_odt_custom_' ) . '/';
+        file_put_contents( $tempDir . basename( $to ), $xml = $created->save() );
+
+        /*
+        $this->assertTrue(
+            ( $errors = $created->validateString( $xml ) ) === true,
+            ( is_array( $errors ) ? implode( PHP_EOL, $errors ) : 'Expected true' )
+        );
+        */
+
+        $this->assertEquals(
+            file_get_contents( $to ),
+            $xml
+        );
+
+        // Remove tempdir, when nothing failed.
+        $this->removeTempDir();
+    }
+}
+
+?>
