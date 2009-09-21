2cb696b34da9cda2f395e5af68cea345242401a8
ts:ez.no <dev-null@apache.org>
2009-09-21 10:50:17 +0000

    - Added handling for footnotes and links in DocBook → ODT conversion.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978253 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Document/src/converters/element_visitor/docbook/odt/ulink.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * File containing the ezcDocumentDocbookToOdtUlinkHandler class.
+ *
+ * @package Document
+ * @version //autogen//
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+/**
+ * Visit ulinks.
+ *
+ * Visit docbook ulinks and transform them into ODT a elements.
+ *
+ * @package Document
+ * @version //autogen//
+ */
+class ezcDocumentDocbookToOdtUlinkHandler extends ezcDocumentDocbookToOdtBaseHandler
+{
+    /**
+     * Handle a node
+     *
+     * Handle / transform a given node, and return the result of the
+     * conversion.
+     *
+     * @param ezcDocumentElementVisitorConverter $converter
+     * @param DOMElement $node
+     * @param mixed $root
+     * @return mixed
+     */
+    public function handle( ezcDocumentElementVisitorConverter $converter, DOMElement $node, $root )
+    {
+        $a = $root->ownerDocument->createElementNS(
+            ezcDocumentOdt::NS_ODT_TEXT,
+            'a'
+        );
+        $a->setAttributeNS(
+            ezcDocumentOdt::NS_XLINK,
+            'xlink:type',
+            'simple'
+        );
+        $a->setAttributeNS(
+            ezcDocumentOdt::NS_XLINK,
+            'xlink:href',
+            $node->getAttribute( 'url' )
+        );
+
+        $root->appendChild( $a );
+
+        $converter->visitChildren( $node, $a );
+        return $root;
+    }
+}
+
+?>
