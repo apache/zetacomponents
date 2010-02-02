36234d48e538ca59d6fa2fdfea91b6ea59560f94
ts:ez.no <dev-null@apache.org>
2010-02-02 11:22:10 +0000

    - Added simple script to covert client test dumps from the visual easily   inspectable to the SVN storable format.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@978674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ Webdav/tests/scripts/convert_client_test.php
@@ -0,0 +1,72 @@
+<?php
+
+if ( count( $argv ) < 2 )
+{
+    die( "Missing input directory.\n" );
+}
+
+if ( count( $argv ) < 3 )
+{
+    die( "Missing output file.\n" );
+}
+
+$inDir = $argv[1];
+$outFile = $argv[2];
+
+if ( !is_dir( $inDir ) || !is_readable( $inDir ) )
+{
+    die( "Could not open '$inDir'." );
+}
+
+$fileMap = array(
+    '_request_body.xml'     => array( 'request', 'body' ),
+    '_request_server.php'   => array( 'request', 'server' ),
+    '_response_status.txt'  => array( 'response', 'status' ),
+    '_response_headers.php' => array( 'response', 'headers' ),
+    '_response_body.xml'    => array( 'response', 'body' ),
+);
+
+$data = array();
+
+foreach ( glob( "$inDir/*_request_body.xml" ) as $file )
+{
+    $fileName = basename( $file );
+    if ( !preg_match( '(^(([0-9]+)_[A-Z]+)_.*$)', $fileName, $matches ) )
+    {
+        die( "Could not parse file name '$fileName'.\n" );
+    }
+
+    $prefix = $matches[1];
+    $no     = (int) $matches[2];
+
+    $data[$no] = array();
+    
+    foreach ( $fileMap as $fileTemplate => $keys )
+    {
+        if ( !file_exists( ( $dataFile = "{$inDir}/{$prefix}{$fileTemplate}" ) ) )
+        {
+            die( "Missing data file '$dataFile'\n" );
+        }
+        switch ( pathinfo( $dataFile, PATHINFO_EXTENSION ) )
+        {
+            case 'php':
+                $inData = require $dataFile;
+                break;
+            case 'xml':
+            case 'txt':
+                $inData = file_get_contents( $dataFile );
+                break;
+            default:
+                die( "Unknwon file extension for file '$dataFile'.\n" );
+        }
+        
+        $data[$no][$keys[0]][$keys[1]] = $inData;
+    }
+}
+
+file_put_contents(
+    $outFile,
+    "<?php\n\nreturn " . var_export( $data, true ) . ";\n\n?>"
+);
+
+?>
