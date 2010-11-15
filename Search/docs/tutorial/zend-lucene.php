689fd8c4f967aa3f8fc2b177fbd50ccd8ee7e76f
Fredericus G. Rethans <derick@apache.org>
2010-11-15 12:35:32 +0000

    - Fixed syntax in file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/zetacomponents/trunk@1035247 13f79535-47bb-0310-9956-ffa450edef68

--- Search/docs/tutorial/zend-lucene.php
+++ Search/docs/tutorial/zend-lucene.php
@@ -3,7 +3,7 @@
 require_once 'tutorial_autoload.php';
 
 // add the location of the zend framework to the include path, this can of
-course also be done in php.ini
+// course also be done in php.ini
 ini_set( 'include_path', ini_get( 'include_path' ) . ':/home/derick/dev/ZendFramework-1.7.4-minimal/library' );
 
 // define the autoload function that can load Zend framework classes
